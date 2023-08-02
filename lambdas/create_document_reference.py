052a179c59fc8d3dc3cbf21fa25b43061d153452
NogaNHS <noga.sasson1@nhs.net>
2023-08-02 17:24:32 +0100

    [PRMDR-18] added document reference class and functions

--- lambdas/create_document_reference.py
+++ lambdas/create_document_reference.py
@@ -3,15 +3,28 @@ import uuid
 import boto3
 from botocore.exceptions import ClientError
 
-def create_document_reference_handler(event, context):
+from lambdas.nhs_document_reference import NHSDocumentReference
+
+
+def lambda_handler(event, context):
     print("API Gateway event received - processing starts")
     s3_bucket_name = os.environ['DOCUMENT_STORE_BUCKET_NAME']
+    s3_object_key = str(uuid.uuid4())
+    try:
+        create_document_reference_object(s3_bucket_name, s3_object_key, event["body"])
+        create_document_presigned_url_handler(s3_bucket_name, s3_object_key)
+    except Exception as e:
+        print(e)
+        #create error response generator
+        return e
+
+def create_document_presigned_url_handler(s3_bucket_name, s3_object_key):
     # Generate a presigned S3 POST URL
     s3_client = boto3.client('s3', region_name='eu-west-2')
 
     try:
         response = s3_client.generate_presigned_post(s3_bucket_name,
-                                                     str(uuid.uuid4()),
+                                                     s3_object_key,
                                                      Fields=None,
                                                      Conditions=None,
                                                      ExpiresIn=1800)
@@ -21,3 +34,18 @@ def create_document_reference_handler(event, context):
 
     # The response contains the presigned URL and required fields
     return response
+
+def create_document_reference_object(s3_bucket_name, s3_object_key, document_request_body):
+    s3_file_location = "s3://" + s3_bucket_name + "/" + s3_object_key
+    new_document = NHSDocumentReference(file_location=s3_file_location,reference_id=s3_object_key, **document_request_body)
+    print("Input document reference filename: ", new_document.file_name)
+    create_document_reference_in_dynamo_db(new_document)
+
+def create_document_reference_in_dynamo_db(new_document):
+    dynamodb = boto3.resource('dynamodb')
+    dynamodb_name = os.environ['DOCUMENT_STORE_DYNAMODB_NAME']
+    table = dynamodb.Table(dynamodb_name)
+    table.put_item(
+        Item=new_document.to_dict()
+    )
+
---
+++ lambdas/nhs_document_reference.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+
+class NHSDocumentReference:
+    def __init__(self, reference_id, file_location, data) -> None:
+        self.id = reference_id
+        self.nhs_number = data['subject']['identifier']['value']
+        self.content_type =  data['content'][0]['attachment']['contentType']
+        self.file_name = data['description']
+        self.created = str(datetime.now())
+        self.deleted = None
+        self.uploaded = None
+        self.virus_scanner_result = "Not Scanned"
+        self.file_location = file_location
+
+    def set_uploaded(self) -> None:
+        self.uploaded = datetime.now()
+
+    def set_deleted(self) -> None:
+        self.deleted = datetime.now()
+
+    def set_virus_scanner_result(self, updated_virus_scanner_result) -> None:
+        self.virus_scanner_result = updated_virus_scanner_result
+
+    def update_location(self, updated_file_location):
+        self.file_location = updated_file_location
+
+    def is_uploaded(self) -> bool:
+        return bool(self.uploaded)
+
+    def to_dict(self):
+        document_metadata = {
+            'ID' : str(self.id),
+            'nhs_number' : self.nhs_number,
+            'file_name' : self.file_name,
+            'file_location' : self.file_location,
+            'created' : self.created,
+            'content_type' : self.content_type,
+            'virus_scanner_result' : self.virus_scanner_result
+        }
+        return document_metadata
