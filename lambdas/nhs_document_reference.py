cbc2e5f7ca7e8ddbdf5519d3b750bbd02e1e81f5
NogaNHS <127490765+NogaNHS@users.noreply.github.com>
2023-08-09 14:01:08 +0100

    PRMDR18 Create upload document url lambda (#1)

---
+++ lambdas/nhs_document_reference.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timezone
+
+
+class NHSDocumentReference:
+    def __init__(self, reference_id, file_location, data) -> None:
+        self.id = reference_id
+        self.nhs_number = data["subject"]["identifier"]["value"]
+        self.content_type = data["content"][0]["attachment"]["contentType"]
+        self.file_name = data["description"]
+        self.created = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        self.deleted = None
+        self.uploaded = None
+        self.virus_scanner_result = "Not Scanned"
+        self.file_location = file_location
+
+    def set_uploaded(self) -> None:
+        self.uploaded = datetime.now()
+
+    def set_deleted(self) -> None:
+        self.deleted = datetime.now()
+
+    def set_virus_scanner_result(self, updated_virus_scanner_result) -> None:
+        self.virus_scanner_result = updated_virus_scanner_result
+
+    def update_location(self, updated_file_location):
+        self.file_location = updated_file_location
+
+    def is_uploaded(self) -> bool:
+        return bool(self.uploaded)
+
+    def to_dict(self):
+        document_metadata = {
+            "ID": str(self.id),
+            "NhsNumber": self.nhs_number,
+            "FileName": self.file_name,
+            "FileLocation": self.file_location,
+            "Created": self.created,
+            "ContentType": self.content_type,
+            "VirusScanResult": self.virus_scanner_result,
+        }
+        return document_metadata
---
+++ lambdas/upload_document_lambda/create_document_reference.py
@@ -0,0 +1,69 @@
+import os
+import uuid
+import boto3
+from botocore.exceptions import ClientError
+import logging
+from lambdas.nhs_document_reference import NHSDocumentReference
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def lambda_handler(event, context):
+    logger.info("API Gateway event received - processing starts")
+    s3_bucket_name = os.environ["DOCUMENT_STORE_BUCKET_NAME"]
+    logger.info("S3 bucket in use: ", s3_bucket_name)
+    s3_object_key = str(uuid.uuid4())
+    try:
+        document_object = create_document_reference_object(
+            s3_bucket_name, s3_object_key, event["body"]
+        )
+        save_document_reference_in_dynamo_db(document_object)
+        response = create_document_presigned_url_handler(s3_bucket_name, s3_object_key)
+    except Exception as e:
+        logger.error(e)
+        # create error response generator
+        return e
+    return response
+
+
+def create_document_presigned_url_handler(s3_bucket_name, s3_object_key):
+    # Generate a presigned S3 POST URL
+    s3_client = boto3.client("s3", region_name="eu-west-2")
+
+    try:
+        response = s3_client.generate_presigned_post(
+            s3_bucket_name, s3_object_key, Fields=None, Conditions=None, ExpiresIn=1800
+        )
+    except ClientError as e:
+        logger.error(e)
+        return None
+
+    # The response contains the presigned URL and required fields
+    return response
+
+
+def create_document_reference_object(
+    s3_bucket_name, s3_object_key, document_request_body
+):
+    s3_file_location = "s3://" + s3_bucket_name + "/" + s3_object_key
+    logger.info("Input document reference location: ", s3_file_location)
+    new_document = NHSDocumentReference(
+        file_location=s3_file_location,
+        reference_id=s3_object_key,
+        data=document_request_body,
+    )
+    logger.info("Input document reference filename: ", new_document.file_name)
+    return new_document
+
+
+def save_document_reference_in_dynamo_db(new_document):
+    try:
+        dynamodb = boto3.resource("dynamodb")
+        dynamodb_name = os.environ["DOCUMENT_STORE_DYNAMODB_NAME"]
+        logger.info("Saving DocumentReference to DynamoDB: ", dynamodb_name)
+        table = dynamodb.Table(dynamodb_name)
+        table.put_item(Item=new_document.to_dict())
+    except ClientError as e:
+        logger.error("Unable to connect to DB")
+        logger.error(e)
