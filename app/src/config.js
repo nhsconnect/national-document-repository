a3fe0dc3c487e23712bac6720faf6de15dbe84e7
Joe Fong <joe.fong1@nhs.net>
2023-08-29 14:16:41 +0100

    [PRMDR-112] Change endpoint in config.js to sandbox b

--- app/src/config.js
+++ app/src/config.js
@@ -7,7 +7,13 @@ const config = {
         endpoints: [
             {
                 name: "doc-store-api",
+<<<<<<< HEAD
                 endpoint: "https://wfsosmq04m.execute-api.eu-west-2.amazonaws.com/dev"
+||||||| parent of 6f58e3f ([PRMDR-112] Change endpoint in config.js to sandbox b)
+                endpoint: "https://y98819ugm5.execute-api.eu-west-2.amazonaws.com/prod"
+=======
+                endpoint: "https://y98819ugm5.execute-api.eu-west-2.amazonaws.com/dev"
+>>>>>>> 6f58e3f ([PRMDR-112] Change endpoint in config.js to sandbox b)
             },
         ],
     },
