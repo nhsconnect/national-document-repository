8660f70cd08f0734b0730870972bd523d69bd6e6
Scott Alexander <scott.alexander@madetech.com>
2023-08-30 19:25:35 +0100

    correction on env script

--- app/react-environment-config.sh
+++ app/react-environment-config.sh
@@ -6,7 +6,7 @@ if test -f ".env"; then
 fi
 
 cp .env.template .env
-sed -i "" "s/%DOC_STORE_API_ENDPOINT%/${ENDPOINT_DOC_STORE}/" .env
+sed -i "" "s/%DOC_STORE_API_ENDPOINT%/${ENDPOINT_DOC_STORE_API}/" .env
 sed -i "" "s/%AWS_REGION%/${AWS_REGION}/" .env
 sed -i "" "s/%OIDC_PROVIDER_ID%/${OIDC_PROVIDER_ID}/" .env
 sed -i "" "s/%BUILD_ENV%/${BUILD_ENV}/" .env
