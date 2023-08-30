bc8f66a56fa9dddc9514a967fc2cb9f2fedbce38
Scott Alexander <scott.alexander@madetech.com>
2023-08-30 19:56:55 +0100

    darwin bash configuration to run on mac and non mac machines

--- app/react-environment-config.sh
+++ app/react-environment-config.sh
@@ -6,11 +6,17 @@ if test -f ".env"; then
 fi
 
 cp .env.template .env
-sed -i "" "s/%DOC_STORE_API_ENDPOINT%/${ENDPOINT_DOC_STORE_API}/" .env
-sed -i "" "s/%AWS_REGION%/${AWS_REGION}/" .env
-sed -i "" "s/%OIDC_PROVIDER_ID%/${OIDC_PROVIDER_ID}/" .env
-sed -i "" "s/%BUILD_ENV%/${BUILD_ENV}/" .env
-sed -i "" "s/%IMAGE_VERSION%/${IMAGE_VERSION}/" .env
+
+SEDOPTION='-i ' 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  SEDOPTION='-i '' '
+fi
+
+sed -i '' "s/%DOC_STORE_API_ENDPOINT%/${ENDPOINT_DOC_STORE_API}/" .env
+sed -i '' "s/%AWS_REGION%/${AWS_REGION}/" .env
+sed -i '' "s/%OIDC_PROVIDER_ID%/${OIDC_PROVIDER_ID}/" .env
+sed -i '' "s/%BUILD_ENV%/${BUILD_ENV}/" .env
+sed -i '' "s/%IMAGE_VERSION%/${IMAGE_VERSION}/" .env
 
 
 
