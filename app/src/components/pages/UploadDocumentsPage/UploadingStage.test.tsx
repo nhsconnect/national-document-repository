1e5bc41df988bbee77077a7c59223fbe8bd568d8
Joe Fong <joe.fong1@nhs.net>
2023-08-30 16:06:08 +0100

    [PRMDR-112] Convert UploadingStage.test.jsx to tsx, remove unused mocks
    Co-authored-by: NogaNHS <noga.sasson1@nhs.net>
    Co-authored-by: RachelHowellNHS <rachel.howell6@nhs.net>

--- app/src/components/pages/UploadDocumentsPage/UploadingStage.test.tsx
+++ app/src/components/pages/UploadDocumentsPage/UploadingStage.test.tsx
@@ -12,33 +12,18 @@ jest.mock("react-router");
 
 describe("<UploadDocumentsPage />", () => {
     describe("with NHS number", () => {
+        const triggerUploadStateChange = (document: UploadDocument, state : DOCUMENT_UPLOAD_STATE, progress: number) => {
+            act(() => {
+                document.state = state;
+                document.progress = progress
+            });
+        };
+
         it("uploads documents and displays the progress", async () => {
 
-            const uploadDocumentMock = jest.fn();
-            const documentOne = {file: buildTextFile("one", 100), state: documentUploadStates.SELECTED, id: 1};
-            const documentTwo = {file: buildTextFile("two", 200), state: documentUploadStates.SELECTED, id: 2};
-            const documentThree = {file: buildTextFile("three", 100), state: documentUploadStates.SELECTED, id: 3};
-            const uploadStateChangeTriggers = {};
-            const resolvers = {};
-            const triggerUploadStateChange = (document, state, progress) => {
-                act(() => {
-                    document.state = state;
-                    document.progress = progress
-                });
-            };
-            const resolveDocumentUploadPromise = (document) => {
-                act(() => {
-                    resolvers[document];
-                });
-            };
-
-            uploadDocumentMock.mockImplementation(async (document, onUploadStateChange) => {
-                uploadStateChangeTriggers[document.name] = onUploadStateChange;
-
-                return new Promise((resolve) => {
-                    resolvers[document.name] = resolve;
-                });
-            });
+            const documentOne = {file: buildTextFile("one", 100), state: documentUploadStates.SELECTED, id: '1', progress: 0};
+            const documentTwo = {file: buildTextFile("two", 200), state: documentUploadStates.SELECTED, id: '2', progress: 0};
+            const documentThree = {file: buildTextFile("three", 100), state: documentUploadStates.SELECTED, id: '3', progress: 0};
 
             render(<UploadingStage documents={[documentOne, documentTwo, documentThree]} />);
 
@@ -48,18 +33,6 @@ describe("<UploadDocumentsPage />", () => {
             expect(
                 screen.getByText("Do not close or navigate away from this browser until upload is complete.")
             ).toBeInTheDocument();
-
-
-            triggerUploadStateChange(documentTwo, documentUploadStates.UPLOADING, 0);
-            triggerUploadStateChange(documentThree, documentUploadStates.UPLOADING, 0);
-            triggerUploadStateChange(documentOne, documentUploadStates.FAILED, 0);
-            triggerUploadStateChange(documentThree, documentUploadStates.SUCCEEDED, 100);
-            triggerUploadStateChange(documentTwo, documentUploadStates.SUCCEEDED, 100);
-
-
-            resolveDocumentUploadPromise(documentOne);
-            resolveDocumentUploadPromise(documentTwo);
-            resolveDocumentUploadPromise(documentThree);
         });
 
 
@@ -67,21 +40,16 @@ describe("<UploadDocumentsPage />", () => {
             const documentOne = {file: buildTextFile("one", 100), state: documentUploadStates.SELECTED, id: '1', progress: 0};
             const documentTwo = {file: buildTextFile("two", 200), state: documentUploadStates.SELECTED, id: '2', progress: 0};
             const documentThree = {file: buildTextFile("three", 100), state: documentUploadStates.SELECTED, id: '3', progress: 0};
-            const triggerUploadStateChange = (document, state, progress) => {
-                act(() => {
-                    document.state = state;
-                    document.progress = progress
-                });
-            };
 
             const renderResult = render(<UploadingStage documents={[documentOne, documentTwo, documentThree]} />);
             const rerender = () => {
                 renderResult.rerender(<UploadingStage documents={[documentOne, documentTwo, documentThree]} />);
             }
-            const getProgressBarValue = (document) => {
-                return parseInt(screen.getByRole("progressbar", { name: `Uploading ${document.file.name}` }).value);
+            const getProgressBarValue = (document: UploadDocument) => {
+                const progressBar: HTMLProgressElement = screen.getByRole("progressbar", { name: `Uploading ${document.file.name}` });
+                return progressBar.value;
             };
-            const getProgressText = (document) => {
+            const getProgressText = (document: UploadDocument) => {
                 return screen.getByRole("status", { name: `${document.file.name} upload status` }).textContent;
             }
 
@@ -108,7 +76,7 @@ describe("<UploadDocumentsPage />", () => {
             triggerUploadStateChange(documentOne, documentUploadStates.FAILED, 0);
             rerender();
             expect(getProgressBarValue(documentOne)).toEqual(0);
-            expect(getProgressText(documentOne)).toContain("failed");
+            expect(getProgressText(documentOne)).toContain("Upload failed");
         });
     });
 
