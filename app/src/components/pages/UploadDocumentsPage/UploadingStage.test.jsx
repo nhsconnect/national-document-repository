2b0e9aa1fe2dc947767c30cf452bb2d2fed8ecdd
Joe Fong <joe.fong1@nhs.net>
2023-08-30 14:05:29 +0100

    [PRMDR-112] Migrate unit test for progress bar in UploadingStage

--- app/src/components/pages/UploadDocumentsPage/UploadingStage.test.jsx
+++ app/src/components/pages/UploadDocumentsPage/UploadingStage.test.jsx
@@ -1,6 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import { act } from "react-dom/test-utils";
-import {DOCUMENT_UPLOAD_STATE as documentUploadStates} from "../../../types/pages/UploadDocumentsPage/types";
+import {
+    DOCUMENT_UPLOAD_STATE,
+    DOCUMENT_UPLOAD_STATE as documentUploadStates, UploadDocument
+} from "../../../types/pages/UploadDocumentsPage/types";
 import { buildTextFile } from "../../../helpers/test/testBuilders";
 import UploadingStage from "./UploadingStage";
 
@@ -57,6 +60,55 @@ describe("<UploadDocumentsPage />", () => {
             resolveDocumentUploadPromise(documentOne);
             resolveDocumentUploadPromise(documentTwo);
             resolveDocumentUploadPromise(documentThree);
+        });
+
+
+        it("progress bar reflect the upload progress", async () => {
+            const documentOne = {file: buildTextFile("one", 100), state: documentUploadStates.SELECTED, id: '1', progress: 0};
+            const documentTwo = {file: buildTextFile("two", 200), state: documentUploadStates.SELECTED, id: '2', progress: 0};
+            const documentThree = {file: buildTextFile("three", 100), state: documentUploadStates.SELECTED, id: '3', progress: 0};
+            const triggerUploadStateChange = (document, state, progress) => {
+                act(() => {
+                    document.state = state;
+                    document.progress = progress
+                });
+            };
+
+            const renderResult = render(<UploadingStage documents={[documentOne, documentTwo, documentThree]} />);
+            const rerender = () => {
+                renderResult.rerender(<UploadingStage documents={[documentOne, documentTwo, documentThree]} />);
+            }
+            const getProgressBarValue = (document) => {
+                return parseInt(screen.getByRole("progressbar", { name: `Uploading ${document.file.name}` }).value);
+            };
+            const getProgressText = (document) => {
+                return screen.getByRole("status", { name: `${document.file.name} upload status` }).textContent;
+            }
+
+            triggerUploadStateChange(documentOne, documentUploadStates.UPLOADING, 10);
+            rerender();
+            expect(getProgressBarValue(documentOne)).toEqual(10);
+            expect(getProgressText(documentOne)).toContain("10% uploaded...");
+
+            triggerUploadStateChange(documentOne, documentUploadStates.UPLOADING, 70);
+            rerender();
+            expect(getProgressBarValue(documentOne)).toEqual(70);
+            expect(getProgressText(documentOne)).toContain("70% uploaded...");
+
+            triggerUploadStateChange(documentTwo, documentUploadStates.UPLOADING, 20);
+            rerender();
+            expect(getProgressBarValue(documentTwo)).toEqual(20);
+            expect(getProgressText(documentTwo)).toContain("20% uploaded...");
+
+            triggerUploadStateChange(documentTwo, documentUploadStates.SUCCEEDED, 100);
+            rerender();
+            expect(getProgressBarValue(documentTwo)).toEqual(100);
+            expect(getProgressText(documentTwo)).toContain("Uploaded");
+
+            triggerUploadStateChange(documentOne, documentUploadStates.FAILED, 0);
+            rerender();
+            expect(getProgressBarValue(documentOne)).toEqual(0);
+            expect(getProgressText(documentOne)).toContain("failed");
         });
     });
 
