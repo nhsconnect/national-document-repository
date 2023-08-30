600dd277819f96cc4df753e2c5198829f4f54903
Scott Alexander <scott.alexander@madetech.com>
2023-08-30 19:15:22 +0100

    actually added the environment configuration script

---
+++ app/react-environment-config.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -eu
+
+function generateConfigJs(){
+    rm .env
+    cp .env.template .env
+    sed -i "" "s/%DOC_STORE_API_ENDPOINT%/${ENDPOINT_DOC_STORE}/" .env
+    sed -i "" "s/%AWS_REGION%/${AWS_REGION}/" .env
+    sed -i "" "s/%OIDC_PROVIDER_ID%/${OIDC_PROVIDER_ID}/" .env
+    sed -i "" "s/%BUILD_ENV%/${BUILD_ENV}/" .env
+    sed -i "" "s/%IMAGE_VERSION%/${IMAGE_VERSION}/" .env
+}
+
+generateConfigJs 
