a1575fed45c555483d2c14bc7dc35d343f8c0198
Scott Alexander <scott.alexander@madetech.com>
2023-08-31 15:06:49 +0100

    changed sed seperate in bash script react-environement-config for urls

--- app/react-environment-config.sh
+++ app/react-environment-config.sh
@@ -10,11 +10,11 @@ cp .env.template .env
 SEDOPTION='-i ' 
 
 # Enable if you want to use on a mac based machine (OSTYPE does not exist on github actions)
-# # if [ -z "$OSTYPE" -a "$OSTYPE" == "darwin"* ]; then
-#   SEDOPTION='-i "" '
-# # fi
+# if [ -z "$OSTYPE" -a "$OSTYPE" == "darwin"* ]; then
+#   SEDOPTION='-i '' '
+# fi
 
-sed $SEDOPTION "s/%DOC_STORE_API_ENDPOINT%/${ENDPOINT_DOC_STORE_API}/" .env
+sed $SEDOPTION "s@%DOC_STORE_API_ENDPOINT%@${ENDPOINT_DOC_STORE_API}@" .env
 sed $SEDOPTION "s/%AWS_REGION%/${AWS_REGION}/" .env
 sed $SEDOPTION "s/%OIDC_PROVIDER_ID%/${OIDC_PROVIDER_ID}/" .env
 sed $SEDOPTION "s/%BUILD_ENV%/${BUILD_ENV}/" .env
