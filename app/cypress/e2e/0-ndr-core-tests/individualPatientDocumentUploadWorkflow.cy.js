c52304720572afc89f3d773eb633c944ed95e3e7
NogaNHS <noga.sasson1@nhs.net>
2023-09-01 16:49:17 +0100

    [PRMDR-138] added Multiple files cypress test for upload screen
    Co-authored-by: RachelHowellNHS <rachel.howell6@nhs.net>

--- app/cypress/e2e/0-ndr-core-tests/individualPatientDocumentUploadWorkflow.cy.js
+++ app/cypress/e2e/0-ndr-core-tests/individualPatientDocumentUploadWorkflow.cy.js
@@ -47,17 +47,6 @@ describe('Uploads docs and tests it looks OK', () => {
 
     })
 
-    // it('On Upload button click, renders Uploading Stage', () => {
-    //
-    //     cy.get('.nhsuk-button').click()
-    //     cy.wait(20)
-    //     cy.get("input[type=file]").selectFile(uploadedFilePathNames[0])
-    //     cy.get("#upload-button").click()
-    //     cy.get('#uploading-stage-warning').should('be.visible')
-    //
-    // })
-
-
     it('Single file - On Upload button click, renders Upload Summary for successful upload', () => {
 
         cy.get('.nhsuk-button').click()
@@ -68,7 +57,7 @@ describe('Uploads docs and tests it looks OK', () => {
         cy.get("#upload-summary-confirmation").should('be.visible')
         cy.get("#upload-summary-header").should('be.visible')
         cy.get('#successful-uploads').should('be.visible')
-        cy.get("#successful-uploads").should('have.length', 1)
+        cy.get("#successful-uploads tbody tr").should('have.length', 1)
         cy.get("#successful-uploads tbody tr").first().get('td').first().should('contain', 'test_patient_record.pdf')
         cy.get('#close-page-warning').should('be.visible')
         cy.get('#start-again-button').should('have.text', 'Start Again')
@@ -141,7 +130,6 @@ describe('Uploads docs and tests it looks OK', () => {
         cy.get('#start-again-button').should('have.text', 'Start Again')
         cy.get('#start-again-button').click()
         cy.url().should('eq', localhost)
-
     })
 
     it('Multiple files - On Choose files button click, file selection is visible', () => {
@@ -154,6 +142,100 @@ describe('Uploads docs and tests it looks OK', () => {
         cy.get("#selected-documents-table tbody tr").should('have.length', 2)
         cy.get("#selected-documents-table tbody tr").first().get('td').first().should('have.text', 'test_patient_record.pdf')
         cy.get("#selected-documents-table tbody tr").next().contains('td', 'test_patient_record_two.pdf')
+    })
+
+    it('Multiple files - On Upload button click, renders Upload Summary for successful upload', () => {
+
+        cy.get('.nhsuk-button').click()
+        cy.wait(20)
+        cy.get("input[type=file]").selectFile(uploadedImagesPathNames)
+        cy.get("#upload-button").click()
+        cy.wait(40)
+        cy.get("#upload-summary-confirmation").should('be.visible')
+        cy.get("#upload-summary-header").should('be.visible')
+        cy.get('#successful-uploads').should('be.visible')
+        cy.get("#successful-uploads tbody tr").should('have.length', 11)
+        cy.get("#successful-uploads tbody tr").first().get('td').first().should('contain', 'test_image.jpg')
+        cy.get('#close-page-warning').should('be.visible')
+        cy.get('#start-again-button').should('have.text', 'Start Again')
+        cy.get('#start-again-button').click()
+        cy.url().should('eq', localhost)
+    })
+
+    it('Multiple files - On Upload button click, renders Uploading Stage', () => {
+
+        cy.get('.nhsuk-button').click()
+        cy.wait(20)
+        cy.get("input[type=file]").selectFile(uploadedImagesPathNames)
+        cy.get("#upload-button").click()
+        cy.get('#uploading-stage-warning').should('be.visible')
 
     })
+
+     it('Multiple files - On Upload button click, renders Upload Summary with error box when DocumentReference returns a 500', () => {
+        // intercept this response and return an error
+        cy.intercept('POST', '*/DocumentReference*', {
+            statusCode: 500,
+        })
+        cy.get('.nhsuk-button').click()
+        cy.wait(20)
+        cy.get("input[type=file]").selectFile(uploadedImagesPathNames)
+        cy.get("#upload-button").click()
+        cy.wait(20)
+        cy.get('#failed-document-uploads-summary-title').should('be.visible')
+        cy.get('#failed-uploads').should('be.visible')
+        cy.get("#failed-uploads tbody tr").should('have.length', 11)
+        cy.get("#failed-uploads tbody tr").first().get('td').first().should('contain', 'test_image.jpg')
+        cy.get('#failed-uploads').should('contain', '11 of 11 files failed to upload')
+        cy.get('#close-page-warning').should('be.visible')
+        cy.get('#start-again-button').should('have.text', 'Start Again')
+        cy.get('#start-again-button').click()
+        cy.url().should('eq', localhost)
+
+    })
+
+    it('Multiple files - On Upload button click, renders Upload Summary with error box when DocumentReference returns a 404', () => {
+        // intercept this response and return an error
+        cy.intercept('POST', '*/DocumentReference*', {
+            statusCode: 404,
+        })
+        cy.get('.nhsuk-button').click()
+        cy.wait(20)
+        cy.get("input[type=file]").selectFile(uploadedImagesPathNames)
+        cy.get("#upload-button").click()
+        cy.wait(20)
+        cy.get('#failed-document-uploads-summary-title').should('be.visible')
+        cy.get('#failed-uploads').should('be.visible')
+        cy.get("#failed-uploads tbody tr").should('have.length', 11)
+        cy.get("#failed-uploads tbody tr").first().get('td').first().should('contain', 'test_image.jpg')
+        cy.get('#failed-uploads').should('contain', '11 of 11 files failed to upload')
+        cy.get('#close-page-warning').should('be.visible')
+        cy.get('#start-again-button').should('have.text', 'Start Again')
+        cy.get('#start-again-button').click()
+        cy.url().should('eq', localhost)
+
+    })
+
+    // once authorisation functionality has been added we can include a test like below for s3 bucket POST failure with 403 status
+    // it('Multiple file - On Upload button click, renders Upload Summary with error box when the s3 bucket POST request fails', () => {
+    //     // intercept this response and return an error
+    //     cy.intercept('POST', 'https://ndra-document-store.s3.amazonaws.com/', (req) => {
+    //          req.reply({statusCode: 500,
+    //                     // forceNetworkError: true,
+    //                     delay: 30})})
+    //     cy.get('.nhsuk-button').click()
+    //     cy.wait(20)
+    //     cy.get("input[type=file]").selectFile(uploadedImagesPathNames)
+    //     cy.get("#upload-button").click()
+    //     cy.wait(20)
+    //     cy.get('#failed-document-uploads-summary-title').should('be.visible')
+    //     cy.get('#failed-uploads').should('be.visible')
+    //     cy.get("#failed-uploads tbody tr").should('have.length', 11)
+    //     cy.get("#failed-uploads tbody tr").first().get('td').first().should('contain', 'test_image.jpg')
+    //     cy.get('#failed-uploads').should('contain', '11 of 11 files failed to upload')
+    //     cy.get('#close-page-warning').should('be.visible')
+    //     cy.get('#start-again-button').should('have.text', 'Start Again')
+    //     cy.get('#start-again-button').click()
+    //     cy.url().should('eq', localhost)
+    // })
 })
