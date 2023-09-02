f0cb2c172fc089d9de35e9a3be197a4a99ac6873
Scott Alexander <scott.alexander@madetech.com>
2023-09-02 11:26:58 +0100

    ci action: tidy up and correct configuration put in place

--- app/react-environment-config.sh
+++ app/react-environment-config.sh
@@ -1,17 +1,14 @@
 #!/bin/sh -eu
 
 echo "script started"
-
 if test -f ".env"; then
     rm .env
 fi
 
-echo "any existing .env file remove"
-
+echo "any existing .env file removed"
 cp .env.template .env
 
 echo "created new .env file from template"
-
 SEDOPTION='-i ' 
 
 # Enable if you want to use on a mac based machine (OSTYPE does not exist on github actions)
@@ -20,11 +17,11 @@ SEDOPTION='-i '
 # fi
 
 echo "filling in vars"
-sed -i "s@%DOC_STORE_API_ENDPOINT%@${ENDPOINT_DOC_STORE_API}@" .env
-sed -i "s/%AWS_REGION%/${AWS_REGION}/" .env
-sed -i "s/%OIDC_PROVIDER_ID%/${OIDC_PROVIDER_ID}/" .env
-sed -i "s/%BUILD_ENV%/${BUILD_ENV}/" .env
-sed -i "s/%IMAGE_VERSION%/${IMAGE_VERSION}/" .env
+sed SEDOPTION='-i '  "s@%DOC_STORE_API_ENDPOINT%@${ENDPOINT_DOC_STORE_API}@" .env
+sed SEDOPTION='-i '  "s/%AWS_REGION%/${AWS_REGION}/" .env
+sed SEDOPTION='-i '  "s/%OIDC_PROVIDER_ID%/${OIDC_PROVIDER_ID}/" .env
+sed SEDOPTION='-i '  "s/%BUILD_ENV%/${BUILD_ENV}/" .env
+sed SEDOPTION='-i '  "s/%IMAGE_VERSION%/${IMAGE_VERSION}/" .env
 
 echo "var transformation completed"
 
