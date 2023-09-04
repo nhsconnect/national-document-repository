ce29c01853ed549436b9585d4ae6d736c59f8b61
Rio Knightley <rio.knightley2@nhs.net>
2023-09-04 12:05:37 +0100

    Add upload and download result tests

--- app/src/pages/patientResultPage/PatientResultPage.test.tsx
+++ app/src/pages/patientResultPage/PatientResultPage.test.tsx
@@ -13,22 +13,99 @@ describe('PatientResultPage', () => {
     });
 
     it('renders component', () => {
-        const mockDetails = buildPatientDetails({
-            familyName: 'Jones',
-            nhsNumber: '0000222000',
+        renderPatientResultPage();
+
+        expect(screen.getByText('Verify patient details')).toBeInTheDocument();
+    });
+
+    it('renders the patient details page when patient data is found', async () => {
+        const nhsNumber = '9000000000';
+        const familyName = 'Smith';
+        const patientDetails = buildPatientDetails({ familyName, nhsNumber });
+
+        renderPatientResultPage(patientDetails);
+
+        expect(screen.getByRole('heading', { name: 'Verify patient details' })).toBeInTheDocument();
+        expect(screen.getByText(familyName)).toBeInTheDocument();
+        expect(
+            screen.getByRole('button', { name: 'Accept details are correct' }),
+        ).toBeInTheDocument();
+        expect(screen.getByText(/If patient details are incorrect/)).toBeInTheDocument();
+
+        const nationalServiceDeskLink = screen.getByRole('link', {
+            name: /National Service Desk/,
         });
 
-        renderPatientResultPage(mockDetails);
+        expect(nationalServiceDeskLink).toHaveAttribute(
+            'href',
+            'https://digital.nhs.uk/about-nhs-digital/contact-us#nhs-digital-service-desks',
+        );
+        expect(nationalServiceDeskLink).toHaveAttribute('target', '_blank');
+    });
 
-        expect(screen.getByText(mockDetails.nhsNumber)).toBeInTheDocument();
-        expect(screen.getByText(mockDetails.familyName)).toBeInTheDocument();
-        //expect(screen.getByText(mockDetails.givenName)).toBeInTheDocument()
+    it('displays text specific to upload path if user has selected upload', async () => {
+        const nhsNumber = '9000000000';
+        const patientDetails = buildPatientDetails({ nhsNumber });
+        const uploadRole = USER_ROLE.GP;
+
+        renderPatientResultPage(patientDetails, uploadRole);
+
+        expect(screen.getByRole('heading', { name: 'Verify patient details' })).toBeInTheDocument();
+        expect(
+            screen.getByText(
+                'Ensure these patient details match the electronic health records and attachments you are about to upload.',
+            ),
+        ).toBeInTheDocument();
+    });
+
+    it("doesn't display text specific to upload path if user has selected download", async () => {
+        const nhsNumber = '9000000000';
+        const patientDetails = buildPatientDetails({ nhsNumber });
+        const downloadRole = USER_ROLE.PCSE;
+        renderPatientResultPage(patientDetails, downloadRole);
+
+        expect(screen.getByRole('heading', { name: 'Verify patient details' })).toBeInTheDocument();
+        expect(
+            screen.queryByText(
+                'Ensure these patient details match the electronic health records and attachments you are about to upload.',
+            ),
+        ).not.toBeInTheDocument();
+    });
+
+    it('displays a message when NHS number is superseded', async () => {
+        const nhsNumber = '9000000012';
+        const patientDetails = buildPatientDetails({ superseded: true, nhsNumber });
+
+        renderPatientResultPage(patientDetails);
+
+        expect(screen.getByRole('heading', { name: 'Verify patient details' })).toBeInTheDocument();
+        expect(
+            screen.getByText('The NHS number for this patient has changed.'),
+        ).toBeInTheDocument();
+    });
+
+    it('displays a message when patient is sensitive', async () => {
+        const nhsNumber = '9124038456';
+        const patientDetails = buildPatientDetails({
+            nhsNumber,
+            postalCode: null,
+            restricted: true,
+        });
+
+        renderPatientResultPage(patientDetails);
+
+        expect(screen.getByRole('heading', { name: 'Verify patient details' })).toBeInTheDocument();
+        expect(
+            screen.getByText(
+                'Certain details about this patient cannot be displayed without the necessary access.',
+            ),
+        ).toBeInTheDocument();
     });
 });
 
 const renderPatientResultPage = (
     patientOverride: Partial<PatientDetails> = {},
-    role: USER_ROLE = USER_ROLE.GP,
+    role: USER_ROLE = USER_ROLE.PCSE,
 ) => {
     const patient: PatientDetails = {
         ...buildPatientDetails(),
