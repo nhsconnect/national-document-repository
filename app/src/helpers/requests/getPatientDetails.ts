9c0376ad72579558b809ffc41b92cf692484caf4
Rio Knightley <rio.knightley2@nhs.net>
2023-09-06 17:07:52 +0100

    Add local env toggle for disable lambda search

--- app/src/helpers/requests/getPatientDetails.ts
+++ app/src/helpers/requests/getPatientDetails.ts
@@ -1,6 +1,7 @@
 import { PatientDetails } from '../../types/generic/patientDetails';
 import { SetSearchErrorCode } from '../../types/pages/patientSearchPage';
 import axios, { AxiosError } from 'axios';
+import { buildPatientDetails } from '../test/testBuilders';
 
 type Args = {
     setStatusCode: SetSearchErrorCode;
@@ -26,7 +27,13 @@ const getPatientDetails = async ({ nhsNumber, baseUrl }: Args) => {
         return data;
     } catch (e) {
         const error = e as AxiosError;
-        throw error;
+        const isLocal =
+            !process.env.REACT_APP_ENVIRONMENT || process.env.REACT_APP_ENVIRONMENT === 'local';
+        if (isLocal) {
+            return buildPatientDetails({ nhsNumber });
+        } else {
+            throw error;
+        }
     }
 };
 
