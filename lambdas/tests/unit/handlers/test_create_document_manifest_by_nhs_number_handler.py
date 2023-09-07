73aa697788c051142cbfffc90642e96a03f35a2a
AlexHerbertNHS <alexandra.herbert1@nhs.net>
2023-09-07 13:53:33 +0100

    [PRMDR-110]
    Adding comment instructions so Rachel can progress while I'm in a meeting

--- lambdas/tests/unit/handlers/test_create_document_manifest_by_nhs_number_handler.py
+++ lambdas/tests/unit/handlers/test_create_document_manifest_by_nhs_number_handler.py
@@ -7,3 +7,18 @@ def test_find_docs_retrieves_something():
     actual = find_document_locations(NHS_NUMBER)
     assert len(actual) > 0
     assert actual[0].startswith("s3://")
+
+# Mock the DynamoQueryService for this unit test
+# Example below
+# with patch.object(DynamoQueryService, "__call__", return_value=MOCK_RESPONSE):
+# Refer to test_document_reference_search_handler for examples of patch.object
+# We'd like the output to be list of strings, example: <-- might change later, but ok for now
+# [
+#   's3://rachel-is-cool/abcde-3i421394792837-sdkbcskjdnf',
+#   's3://alex-is-cool/abcde-3i421394792837-sdkbcskjdnf',
+# ]
+
+# Test it has the s3:// at the start and contains the bucket ID as expected
+# Test for empty response (i.e. response with empty Items)
+# Write a test to see if it responds correctly whern an exception is thrown from the Dynamo service
+
