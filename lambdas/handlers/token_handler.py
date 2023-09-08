70a89f9e71ab1f4f3e85620e2c518347fe786fba
NogaNHS <noga.sasson1@nhs.net>
2023-09-08 16:17:11 +0100

    [PRMDR-95] adding expiry time for token Co-authored-by: Joe Fong <joe.fong1@nhs.net>

--- lambdas/handlers/token_handler.py
+++ lambdas/handlers/token_handler.py
@@ -4,7 +4,7 @@ import logging
 import boto3
 import botocore
 import jwt
-
+import time
 from utils.lambda_response import ApiGatewayResponse
 
 logger = logging.getLogger()
@@ -15,12 +15,17 @@ def lambda_handler(event, context):
     print(f"incoming event: {event}")
     try:
         client = boto3.client('ssm')
+        logger.info("starting ssm request")
         ssm_response = client.get_parameter(
         Name='jwt_token_private_key',
         WithDecryption=True
         )
+        logger.info("ending ssm request")
         cis2_user_info = event['body']['user']
+        cis2_user_info['exp'] = time.time() + 60*15
+        cis2_user_info['iss'] = "nhs repo"
         private_key = ssm_response['Parameter']['Value']
+        logger.info("starting encoding request")
         token = jwt.encode(cis2_user_info, private_key, algorithm="RS256")
         logger.info(f"encoded JWT: {token}")
     except botocore.exceptions.ClientError as e:
@@ -35,8 +40,8 @@ def lambda_handler(event, context):
 
     response = {
     "access_token": token,
-    "token_type":"Bearer",
-    "expires_in":3600,
+    "token_type": "Bearer",
+    "expires_in": 3600,
   }
 
     return ApiGatewayResponse(200, json.dumps(response), "GET").create_api_gateway_response()
