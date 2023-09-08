8642ebd33837ad6dc6a8912e09140f1f30d67c1a
Joe Fong <joe.fong1@nhs.net>
2023-09-08 16:36:18 +0100

    [PRMDR-163] Add unit test for authoriser lambda, run formatter, removed unused 'context' and 'principalId'
    Co-authored-by: NogaNHS <noga.sasson1@nhs.net>

--- lambdas/handlers/authoriser_handler.py
+++ lambdas/handlers/authoriser_handler.py
@@ -1,14 +1,14 @@
 """
+This code has been modified from AWS blueprint. Below is the original license:
+
 Copyright 2015-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-This code has been modified from AWS blueprint.
 Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
      http://aws.amazon.com/apache2.0/
 or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 """
 
-from __future__ import print_function
-
 import logging
+import os
 import re
 
 import boto3
@@ -18,31 +18,37 @@ import jwt
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
 def lambda_handler(event, context):
-    """Do not print the auth token unless absolutely necessary """
-    print("Client token: " + event['authorizationToken'])
-    print(f"incoming event: {event}")
-    user = 'Unauthorised'
+    user = "Unauthorised"
+    ssm_public_key_parameter_name = os.environ["SSM_PARAM_JWT_TOKEN_PUBLIC_KEY"]
+
     try:
-        client = boto3.client('ssm')
+        client = boto3.client("ssm")
         ssm_response = client.get_parameter(
-        Name='jwt_token_public_key',
-        WithDecryption=True
+            Name=ssm_public_key_parameter_name, WithDecryption=True
         )
-        public_key = ssm_response['Parameter']['Value']
-        decoded = jwt.decode(event['authorizationToken'], public_key, algorithms=["RS256"])
-        user = decoded['user_role']
+        public_key = ssm_response["Parameter"]["Value"]
+
+        # For now we just assume the user authenticate with a signed token in this form:
+        # { "user_role": "Unauthorised"  or  "GP"  or  "PCSE" }
+        # The token is going to be issued by the lambda of ticket PRMDR-167
+
+        decoded = jwt.decode(
+            event["authorizationToken"], public_key, algorithms=["RS256"]
+        )
+        user = decoded["user_role"]
         logger.info(f"decoded JWT: {decoded}")
     except botocore.exceptions.ClientError as e:
         logger.error(e)
     except jwt.PyJWTError as e:
-        logger.info(f"error while decoding JWT: {e}")
+        logger.error(f"error while decoding JWT: {e}")
     except KeyError as e:
         logger.error(e)
 
-    principalId = "user|a1b2c3d4"
-    tmp = event['methodArn'].split(':')
-    apiGatewayArnTmp = tmp[5].split('/')
+    principalId = ""
+    tmp = event["methodArn"].split(":")
+    apiGatewayArnTmp = tmp[5].split("/")
     awsAccountId = tmp[4]
 
     policy = AuthPolicy(principalId, awsAccountId)
@@ -54,15 +60,15 @@ def lambda_handler(event, context):
         policy.allowAllMethods()
     elif user == "PCSE":
         policy.allowMethod(HttpVerb.GET, "/SearchDocumentReferences")
+    elif user == "SuperUser":
+        policy.allowAllMethods()
     else:
         policy.denyAllMethods()
 
-
     authResponse = policy.build()
 
-    authResponse['context'] = context
-
     return authResponse
+
 
 class HttpVerb:
     GET = "GET"
@@ -74,17 +80,18 @@ class HttpVerb:
     OPTIONS = "OPTIONS"
     ALL = "*"
 
+
 class AuthPolicy(object):
     awsAccountId = ""
     principalId = ""
     version = "2012-10-17"
-    pathRegex = "^[/.a-zA-Z0-9-\*]+$"
+    pathRegex = r"^[/.a-zA-Z0-9-*]+$"
     allowMethods = []
     denyMethods = []
 
-    restApiId = "<<restApiId>>"
-    region = "<<region>>"
-    stage = "<<stage>>"
+    restApiId = "<<restApiId>"
+    region = "eu-west-2"
+    stage = "dev"
 
     def __init__(self, principal, awsAccountId):
         self.awsAccountId = awsAccountId
@@ -97,40 +104,52 @@ class AuthPolicy(object):
         the internal list contains a resource ARN and a condition statement. The condition
         statement can be null."""
         if verb != "*" and not hasattr(HttpVerb, verb):
-            raise NameError("Invalid HTTP verb " + verb + ". Allowed verbs in HttpVerb class")
+            raise NameError(
+                "Invalid HTTP verb " + verb + ". Allowed verbs in HttpVerb class"
+            )
         resourcePattern = re.compile(self.pathRegex)
         if not resourcePattern.match(resource):
-            raise NameError("Invalid resource path: " + resource + ". Path should match " + self.pathRegex)
+            raise NameError(
+                "Invalid resource path: "
+                + resource
+                + ". Path should match "
+                + self.pathRegex
+            )
 
         if resource[:1] == "/":
             resource = resource[1:]
 
-        resourceArn = ("arn:aws:execute-api:" +
-                       self.region + ":" +
-                       self.awsAccountId + ":" +
-                       self.restApiId + "/" +
-                       self.stage + "/" +
-                       verb + "/" +
-                       resource)
+        resourceArn = (
+            "arn:aws:execute-api:"
+            + self.region
+            + ":"
+            + self.awsAccountId
+            + ":"
+            + self.restApiId
+            + "/"
+            + self.stage
+            + "/"
+            + verb
+            + "/"
+            + resource
+        )
 
         if effect.lower() == "allow":
-            self.allowMethods.append({
-                'resourceArn': resourceArn,
-                'conditions': conditions
-            })
+            self.allowMethods.append(
+                {"resourceArn": resourceArn, "conditions": conditions}
+            )
         elif effect.lower() == "deny":
-            self.denyMethods.append({
-                'resourceArn': resourceArn,
-                'conditions': conditions
-            })
+            self.denyMethods.append(
+                {"resourceArn": resourceArn, "conditions": conditions}
+            )
 
     def _getEmptyStatement(self, effect):
         """Returns an empty statement object prepopulated with the correct action and the
         desired effect."""
         statement = {
-            'Action': 'execute-api:Invoke',
-            'Effect': effect[:1].upper() + effect[1:].lower(),
-            'Resource': []
+            "Action": "execute-api:Invoke",
+            "Effect": effect[:1].upper() + effect[1:].lower(),
+            "Resource": [],
         }
 
         return statement
@@ -144,12 +163,12 @@ class AuthPolicy(object):
             statement = self._getEmptyStatement(effect)
 
             for curMethod in methods:
-                if curMethod['conditions'] is None or len(curMethod['conditions']) == 0:
-                    statement['Resource'].append(curMethod['resourceArn'])
+                if curMethod["conditions"] is None or len(curMethod["conditions"]) == 0:
+                    statement["Resource"].append(curMethod["resourceArn"])
                 else:
                     conditionalStatement = self._getEmptyStatement(effect)
-                    conditionalStatement['Resource'].append(curMethod['resourceArn'])
-                    conditionalStatement['Condition'] = curMethod['conditions']
+                    conditionalStatement["Resource"].append(curMethod["resourceArn"])
+                    conditionalStatement["Condition"] = curMethod["conditions"]
                     statements.append(conditionalStatement)
 
             statements.append(statement)
@@ -179,19 +198,21 @@ class AuthPolicy(object):
         conditions. This will generate a policy with two main statements for the effect:
         one statement for Allow and one statement for Deny.
         Methods that includes conditions will have their own statement in the policy."""
-        if ((self.allowMethods is None or len(self.allowMethods) == 0) and
-                (self.denyMethods is None or len(self.denyMethods) == 0)):
+        if (self.allowMethods is None or len(self.allowMethods) == 0) and (
+            self.denyMethods is None or len(self.denyMethods) == 0
+        ):
             raise NameError("No statements defined for the policy")
 
         policy = {
-            'principalId': self.principalId,
-            'policyDocument': {
-                'Version': self.version,
-                'Statement': []
-            }
+            "principalId": self.principalId,
+            "policyDocument": {"Version": self.version, "Statement": []},
         }
 
-        policy['policyDocument']['Statement'].extend(self._getStatementForEffect("Allow", self.allowMethods))
-        policy['policyDocument']['Statement'].extend(self._getStatementForEffect("Deny", self.denyMethods))
+        policy["policyDocument"]["Statement"].extend(
+            self._getStatementForEffect("Allow", self.allowMethods)
+        )
+        policy["policyDocument"]["Statement"].extend(
+            self._getStatementForEffect("Deny", self.denyMethods)
+        )
 
         return policy
--- lambdas/tests/unit/handlers/test_authoriser_handler.py
+++ lambdas/tests/unit/handlers/test_authoriser_handler.py
@@ -1,44 +1,147 @@
 from unittest.mock import patch
+
+import jwt
 import pytest
 import boto3
 from moto import mock_ssm
 from dataclasses import dataclass
 
-from handlers.authoriser_handler import lambda_handler as auth_lambda
+from handlers.authoriser_handler import lambda_handler
+
+
+MOCK_METHOD_ARN_PREFIX = "arn:aws:execute-api:eu-west-2:fake_arn:fake_api_endpoint/dev"
+TEST_PUBLIC_KEY = "test_public_key"
+TEST_PUBLIC_KEY_SSM_PARAM_NAME = "jwt_token_public_key"
+
 
 @pytest.fixture
 def context():
     @dataclass
     class LambdaContext:
         pass
+
     return LambdaContext()
 
-# def test_token():
-#     private_key = b"testtsdfushd"
-#     token = jwt.encode(test_json, private_key, algorithm="RS256")
-#     event = {'authorizationToken': token}
-#     auth_lambda.lambda_handler(event, context)
 
-@mock_ssm
-def test_valid_gp_token_return_allow_policy(context):
-    with patch("handler.jwt.decode") as decode_mock:
-        ssm = boto3.client('ssm')
-        ssm.put_parameter(
-            Name="jwt_token_public_key",
-            Value="test_public_key",
-            Type="SecureString",
-        )
-        event = {'authorizationToken': "valid_token"}
-        decode_mock.return_value = {'user_role' : 'GP'}
-        response = auth_lambda.lambda_handler(event, context)
-        print(response)
+@pytest.fixture()
+def mock_ssm_public_key():
+    with patch.dict("os.environ", {"SSM_PARAM_JWT_TOKEN_PUBLIC_KEY": TEST_PUBLIC_KEY_SSM_PARAM_NAME}):
+        with mock_ssm():
+            ssm = boto3.client("ssm")
+            ssm.put_parameter(
+                Name=TEST_PUBLIC_KEY_SSM_PARAM_NAME,
+                Value=TEST_PUBLIC_KEY,
+                Type="SecureString",
+            )
+            yield
 
 
-def test_valid_pcse_token_return_allow_policy():
-    pass
+def test_valid_gp_token_return_allow_policy(mocker, mock_ssm_public_key, context):
+    decoded_token = {"user_role": "GP"}
+    decode_mock = mocker.patch("jwt.decode", return_value=decoded_token)
+    expected_allow_policy = {
+        "Statement": [
+            {
+                "Action": "execute-api:Invoke",
+                "Effect": "Allow",
+                "Resource": [f"{MOCK_METHOD_ARN_PREFIX}/*/*"],
+            }
+        ],
+        "Version": "2012-10-17",
+    }
 
-def test_invalid_token_return_deny_policy():
-    pass
+    test_event = {
+        "authorizationToken": "valid_gp_token",
+        "methodArn": f"{MOCK_METHOD_ARN_PREFIX}/GET/SearchPatient",
+    }
 
-def test_invalid_signature_return_deny_policy():
-    pass
+    response = lambda_handler(test_event, context)
+
+    decode_mock.assert_called_with(
+        "valid_gp_token", TEST_PUBLIC_KEY, algorithms=["RS256"]
+    )
+    assert response["policyDocument"] == expected_allow_policy
+
+
+def test_valid_pcse_token_return_allow_policy(mocker, mock_ssm_public_key, context):
+    decoded_token = {"user_role": "PCSE"}
+    decode_mock = mocker.patch("jwt.decode", return_value=decoded_token)
+    expected_allow_policy = {
+        "Statement": [
+            {
+                "Action": "execute-api:Invoke",
+                "Effect": "Allow",
+                "Resource": [f"{MOCK_METHOD_ARN_PREFIX}/GET/SearchDocumentReferences"],
+            }
+        ],
+        "Version": "2012-10-17",
+    }
+
+    test_event = {
+        "authorizationToken": "valid_pcse_token",
+        "methodArn": f"{MOCK_METHOD_ARN_PREFIX}/GET/SearchPatient",
+    }
+
+    response = lambda_handler(test_event, context)
+
+    decode_mock.assert_called_with(
+        "valid_pcse_token", TEST_PUBLIC_KEY, algorithms=["RS256"]
+    )
+    assert response["policyDocument"] == expected_allow_policy
+
+
+def test_invalid_token_return_deny_policy(mocker, mock_ssm_public_key, context):
+    decode_mock = mocker.patch(
+        "jwt.decode", side_effect=jwt.exceptions.InvalidTokenError
+    )
+
+    expected_deny_policy = {
+        "Statement": [
+            {
+                "Action": "execute-api:Invoke",
+                "Effect": "Deny",
+                "Resource": [f"{MOCK_METHOD_ARN_PREFIX}/*/*"],
+            }
+        ],
+        "Version": "2012-10-17",
+    }
+
+    test_event = {
+        "authorizationToken": "invalid_token",
+        "methodArn": f"{MOCK_METHOD_ARN_PREFIX}/GET/SearchPatient",
+    }
+    response = lambda_handler(test_event, context)
+
+    decode_mock.assert_called_with(
+        "invalid_token", TEST_PUBLIC_KEY, algorithms=["RS256"]
+    )
+    assert response["policyDocument"] == expected_deny_policy
+
+
+def test_invalid_signature_return_deny_policy(mocker, mock_ssm_public_key, context):
+    decode_mock = mocker.patch(
+        "jwt.decode", side_effect=jwt.exceptions.InvalidSignatureError
+    )
+
+    expected_deny_policy = {
+        "Statement": [
+            {
+                "Action": "execute-api:Invoke",
+                "Effect": "Deny",
+                "Resource": [f"{MOCK_METHOD_ARN_PREFIX}/*/*"],
+            }
+        ],
+        "Version": "2012-10-17",
+    }
+
+    test_event = {
+        "authorizationToken": "token_with_invalid_signature",
+        "methodArn": f"{MOCK_METHOD_ARN_PREFIX}/GET/SearchPatient",
+    }
+    response = lambda_handler(test_event, context)
+
+    decode_mock.assert_called_with(
+        "token_with_invalid_signature", TEST_PUBLIC_KEY, algorithms=["RS256"]
+    )
+
+    assert response["policyDocument"] == expected_deny_policy
