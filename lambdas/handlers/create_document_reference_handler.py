8148268b40a20b9309a31fa7842808fca5cb5f76
Scott Alexander <scott.alexander@madetech.com>
2023-09-13 16:45:47 +0100

    create docuement reference modifications inline with ticket

--- lambdas/handlers/create_document_reference_handler.py
+++ lambdas/handlers/create_document_reference_handler.py
@@ -3,10 +3,12 @@ import logging
 import os
 import sys
 import uuid
+import string
 
 from services.dynamo_reference_service import DynamoReferenceService
 from services.s3_upload_service import S3UploadService
 from utils.lambda_response import ApiGatewayResponse
+from enums.supported_document_types import SupportedDocumentTypes
 
 sys.path.append(os.path.join(os.path.dirname(__file__)))
 
@@ -15,36 +17,51 @@ logger.setLevel(logging.INFO)
 
 
 def lambda_handler(event, context):
+    
     logger.info("API Gateway event received - processing starts")
 
+    # process passed in parameters
+    try:
+        document_type = (event["queryStringParameters"]["documentType"]).upper()
+        if document_type in SupportedDocumentTypes.list_names():
+            logger.info("Provided document is supported")
+        else:
+            raise Exception("Provided document type is not supported")                 
+    except Exception as e:
+        logger.error(e)
+        response = ApiGatewayResponse(400, "An error occured processing the required document type", "POST").create_api_gateway_response()
+        return response
+
+    # We cannot get here unless a valid document type has been provided, therefore we can default to ARF
     s3_bucket_name = os.environ["DOCUMENT_STORE_BUCKET_NAME"]
     dynamo_table = os.environ["DOCUMENT_STORE_DYNAMODB_NAME"]
 
+    if (document_type == SupportedDocumentTypes.LG.name):
+        s3_bucket_name = os.environ["LLOYD_GEORGE_BUCKET_NAME"]
+        dynamo_table = os.environ["LLOYD_GEORGE_DYNAMODB_NAME"]
+        
     logger.info(f"S3 bucket in use: {s3_bucket_name}")
     logger.info(f"Dynamo table in use: {dynamo_table}")
 
     body = json.loads(event["body"])
-    dynamo_reference_service = DynamoReferenceService(dynamo_table)
-    s3_upload_service = S3UploadService(s3_bucket_name)
+    logging.info(event["body"])
+    logging.info(body)
+    
+    dynamo_reference_service = DynamoReferenceService()
+    s3_upload_service = S3UploadService()
 
     try:
         s3_object_key = str(uuid.uuid4())
-        document_object = (
-            dynamo_reference_service.create_document_dynamo_reference_object(
-                s3_bucket_name, s3_object_key, body
-            )
-        )
-        dynamo_reference_service.save_document_reference_in_dynamo_db(document_object)
-
-        s3_response = s3_upload_service.create_document_presigned_url_handler(
-            s3_object_key
-        )
+        document_object = dynamo_reference_service.create_document_dynamo_reference_object(s3_bucket_name,
+                                                                                           s3_object_key, body)
+        dynamo_reference_service.save_document_reference_in_dynamo_db(dynamo_table, document_object)
+        s3_response = s3_upload_service.create_document_presigned_url_handler(s3_bucket_name, s3_object_key)
+        
     except Exception as e:
         logger.error(e)
-        response = ApiGatewayResponse(
-            400, "An error occurred when getting ready to upload", "POST"
-        ).create_api_gateway_response()
+        response = ApiGatewayResponse(400, "An error occurred when getting ready to upload", "POST").create_api_gateway_response()
         return response
+    
     return ApiGatewayResponse(
         200, json.dumps(s3_response), "POST"
     ).create_api_gateway_response()
--- lambdas/services/dynamo_reference_service.py
+++ lambdas/services/dynamo_reference_service.py
@@ -12,9 +12,6 @@ logger.setLevel(logging.INFO)
 class DynamoReferenceService:
     s3_object_key = str(uuid.uuid4())
 
-    def __init__(self, dynamo_table_name):
-        self.dynamo_table_name = dynamo_table_name
-
     # Previously Create Document Reference Object
     # Creates the necessary data to upload to Dynamo DocumentReferenceMetadata table
     def create_document_dynamo_reference_object(
@@ -22,6 +19,12 @@ class DynamoReferenceService:
     ) -> NHSDocumentReference:
         s3_file_location = f"s3://{s3_bucket_name}/{s3_object_key}"
         logger.info(f"Input document reference location: {s3_file_location}")
+
+        logger.info("request body:")
+        logger.info(document_request_body)
+        logger.info(document_request_body[["subject"]])
+        logger.info(document_request_body[["content"]])
+        logger.info(document_request_body[["description"]])
 
         new_document = NHSDocumentReference(
             file_location=s3_file_location,
@@ -32,13 +35,13 @@ class DynamoReferenceService:
         logger.info(f"Input document reference filename: {new_document.file_name}")
         return new_document
 
-    def save_document_reference_in_dynamo_db(self, new_document: NHSDocumentReference):
+    def save_document_reference_in_dynamo_db(self, dynamo_table_name, new_document: NHSDocumentReference):
         try:
             dynamodb = boto3.resource("dynamodb")
             logger.info(
-                f"Saving DocumentReference to DynamoDB: {self.dynamo_table_name}"
+                f"Saving DocumentReference to DynamoDB: {dynamo_table_name}"
             )
-            table = dynamodb.Table(self.dynamo_table_name)
+            table = dynamodb.Table(dynamo_table_name)
             table.put_item(Item=new_document.to_dict())
         except ClientError as e:
             logger.error("Unable to connect to DB")
