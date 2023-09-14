9f559801d8b055fcc0a64e1f5308b3175a65bedb
Joe Fong <joe.fong1@nhs.net>
2023-09-14 13:39:34 +0100

    [PRMDR-167] Add unit test for OIDC service validate_and_decode_token

--- lambdas/tests/unit/services/test_oidc_service.py
+++ lambdas/tests/unit/services/test_oidc_service.py
@@ -1,8 +1,9 @@
-from unittest.mock import patch, ANY
+import time
+from unittest.mock import patch
 
 import jwt
+from cryptography.hazmat.primitives.asymmetric import rsa
 import pytest
-import pytest_mock
 
 from models.oidc_models import IdTokenClaimSet
 from services.oidc_service import OidcService
@@ -21,7 +22,9 @@ MOCK_PARAMETERS = {
 
 @pytest.fixture
 def oidc_service(mocker):
-    with patch.object(OidcService, "fetch_oidc_parameters", return_value=MOCK_PARAMETERS):
+    with patch.object(
+        OidcService, "fetch_oidc_parameters", return_value=MOCK_PARAMETERS
+    ):
         oidc_service = OidcService()
         yield oidc_service
 
@@ -41,7 +44,6 @@ class MockResponse:
 def test_oidc_service_fetch_tokens_successfully(mocker, oidc_service):
     mock_access_token = "mock_access_token"
     mock_id_token = "mock_id_token"
-    mock_cis2_public_key = "mock_cis2_public_key"
     mock_cis2_response = MockResponse(
         status_code=200,
         json_data={
@@ -55,29 +57,20 @@ def test_oidc_service_fetch_tokens_successfully(mocker, oidc_service):
     mock_decoded_claim_set = {
         "sid": "fake_cis2_session_id",
         "sub": "fake_cis2_login_id",
-        "exp": 1234567890
+        "exp": 1234567890,
     }
-    mock_cis2_signing_key = mocker.Mock()
-    mock_cis2_signing_key.key = mock_cis2_public_key
 
-    mocker.patch.object(jwt.PyJWKClient, "get_signing_key_from_jwt", return_value=mock_cis2_signing_key)
-    mocked_jwt_decode = mocker.patch("jwt.decode", return_value=mock_decoded_claim_set)
     mocker.patch("requests.post", return_value=mock_cis2_response)
+    mocked_id_token_validation = mocker.patch.object(
+        oidc_service, "validate_and_decode_token", return_value=mock_decoded_claim_set
+    )
 
-    expected = (mock_access_token,  IdTokenClaimSet(**mock_decoded_claim_set))
+    expected = (mock_access_token, IdTokenClaimSet(**mock_decoded_claim_set))
 
     actual = oidc_service.fetch_tokens("fake_auth_code")
 
     assert actual == expected
-
-    mocked_jwt_decode.assert_called_with(
-        jwt=mock_id_token,
-        key=mock_cis2_public_key,
-        algorithms=["RS256"],
-        audience=MOCK_PARAMETERS["OIDC_CLIENT_ID"],
-        issuer=MOCK_PARAMETERS["OIDC_ISSUER_URL"],
-        options=OidcService.VERIFY_ALL
-    )
+    mocked_id_token_validation.assert_called_with(mock_id_token)
 
 
 def test_oidc_service_fetch_tokens_raises_AuthorisationException_for_invalid_auth_code(
@@ -96,6 +89,30 @@ def test_oidc_service_fetch_tokens_raises_AuthorisationException_for_invalid_aut
 
     with pytest.raises(AuthorisationException):
         oidc_service.fetch_tokens("invalid_auth_code")
+
+
+def test_oidc_service_fetch_tokens_raises_AuthorisationException_for_invalid_id_token(
+    mocker, oidc_service
+):
+    mock_access_token = "mock_access_token"
+    invalid_id_token = "invalid_id_token"
+    mock_cis2_response = MockResponse(
+        status_code=200,
+        json_data={
+            "access_token": mock_access_token,
+            "scope": "openid",
+            "id_token": invalid_id_token,
+            "token_type": "Bearer",
+            "expires_in": 3599,
+        },
+    )
+    mocker.patch("requests.post", return_value=mock_cis2_response)
+    mocker.patch.object(
+        oidc_service, "validate_and_decode_token", side_effect=AuthorisationException
+    )
+
+    with pytest.raises(AuthorisationException):
+        oidc_service.fetch_tokens("fake_auth_code")
 
 
 def test_oidc_service_fetch_user_org_codes(mocker, oidc_service):
@@ -139,7 +156,7 @@ def test_oidc_service_fetch_user_org_codes(mocker, oidc_service):
     assert actual == expected
 
 
-def test_oidc_service_fetch_user_org_codes_raise_AuthorisationException(
+def test_oidc_service_fetch_user_org_codes_raise_AuthorisationException_for_invalid_access_token(
     mocker, oidc_service
 ):
     mock_token = "fake_access_token"
@@ -155,3 +172,78 @@ def test_oidc_service_fetch_user_org_codes_raise_AuthorisationException(
 
     with pytest.raises(AuthorisationException):
         oidc_service.fetch_user_org_codes(mock_token)
+
+
+# Below are tests around the validate_and_decode_token method. Involve mocking the CIS2 signing key pairs
+@pytest.fixture(name="mock_id_tokens", scope="session")
+def mock_cis2_public_key_and_id_tokens():
+    mock_cis2_private_key = rsa.generate_private_key(
+        public_exponent=65537, key_size=4096
+    )
+    mock_cis2_public_key = mock_cis2_private_key.public_key()
+
+    claim_set = {
+        "name": "fake_id_token",
+        "iss": MOCK_PARAMETERS["OIDC_ISSUER_URL"],
+        "aud": MOCK_PARAMETERS["OIDC_CLIENT_ID"],
+        "exp": time.time() + 3600,
+    }
+    valid_id_token = jwt.encode(claim_set, key=mock_cis2_private_key, algorithm="RS256")
+
+    fake_key = rsa.generate_private_key(public_exponent=65537, key_size=4096)
+    id_token_of_fake_key = jwt.encode(claim_set, key=fake_key, algorithm="RS256")
+
+    one_minute_ago = time.time() - 60
+    valid_id_token_but_expired = jwt.encode(
+        {**claim_set, "exp": one_minute_ago},
+        key=mock_cis2_private_key,
+        algorithm="RS256",
+    )
+
+    yield {
+        "valid_id_token": valid_id_token,
+        "counterfeit_id_token": id_token_of_fake_key,
+        "expected_claim_set": claim_set,
+        "expired_id_token": valid_id_token_but_expired,
+        "mock_cis2_public_key": mock_cis2_public_key,
+    }
+
+
+@pytest.fixture()
+def mock_jwk_client(mocker, mock_id_tokens):
+    # jwk client normally will fetch the public key from cis2 server. mock it with our fake public key instead.
+    mock_cis2_signing_key = mocker.Mock()
+    mock_cis2_signing_key.key = mock_id_tokens["mock_cis2_public_key"]
+    mocker.patch.object(
+        jwt.PyJWKClient, "get_signing_key_from_jwt", return_value=mock_cis2_signing_key
+    )
+    yield
+
+
+def test_oidc_service_validate_and_decode_token__validate_token_with_proper_keys(
+    mock_id_tokens, oidc_service, mock_jwk_client
+):
+    id_token = mock_id_tokens["valid_id_token"]
+
+    actual = oidc_service.validate_and_decode_token(id_token)
+    expect = mock_id_tokens["expected_claim_set"]
+
+    assert actual == expect
+
+
+def test_oidc_service_validate_and_decode_token__raise_AuthorisationException_for_fake_id_token(
+    mock_id_tokens, oidc_service, mock_jwk_client
+):
+    counterfeit_id_token = mock_id_tokens["counterfeit_id_token"]
+
+    with pytest.raises(AuthorisationException):
+        oidc_service.validate_and_decode_token(counterfeit_id_token)
+
+
+def test_oidc_service_validate_and_decode_token__raise_AuthorisationException_for_expired_id_token(
+    mock_id_tokens, oidc_service, mock_jwk_client
+):
+    expired_id_token = mock_id_tokens["expired_id_token"]
+
+    with pytest.raises(AuthorisationException):
+        oidc_service.validate_and_decode_token(expired_id_token)
