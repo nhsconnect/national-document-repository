5e5299e4a84af7d3a9a5e465c9cd8a9ce4842f31
NogaNHS <noga.sasson1@nhs.net>
2023-09-15 12:41:19 +0100

    [PRMDR-165] added unit test for lambda

--- lambdas/handlers/login_redirect_handler.py
+++ lambdas/handlers/login_redirect_handler.py
@@ -1,3 +1,4 @@
+import abc
 import logging
 import os
 import time
@@ -13,14 +14,15 @@ logger.setLevel(logging.INFO)
 
 
 def lambda_handler(event, context):
+    return prepare_redirect_response(WebApplicationClient)
+
+def prepare_redirect_response(web_application_client_class):
     try:
-        ssm_parameters_names = ["OIDC_AUTHORISE_URL", "OIDC_CLIENT_ID"]
-        ssm_client = boto3.client("ssm", region_name="eu-west-2")
-        ssm_response = ssm_client.get_parameters(Names=ssm_parameters_names)
+        ssm_response = get_ssm_parameters()
 
         oidc_parameters = {parameter["Name"] : parameter["Value"] for parameter in ssm_response["Parameters"]}
 
-        oidc_client = WebApplicationClient(
+        oidc_client = web_application_client_class(
             client_id=oidc_parameters["OIDC_CLIENT_ID"],
         )
 
@@ -29,15 +31,17 @@ def lambda_handler(event, context):
             redirect_url=os.environ["OIDC_CALLBACK_URL"],
             scope=["openid", "profile", "nationalrbacaccess", "associatedorgs"],
         )
+
         save_state_in_dynamo_db(oidc_client.state)
 
         location_header = {"Location": url}
+
     except ClientError as e:
         logger.error(f"Error getting using aws client: {e}")
-        return ApiGatewayResponse(500, e, "GET").create_api_gateway_response()
+        return ApiGatewayResponse(500, "Server error", "GET").create_api_gateway_response()
     except InsecureTransportError as e:
         logger.error(f"Error preparing auth request: {e}")
-        return ApiGatewayResponse(500, e, "GET").create_api_gateway_response()
+        return ApiGatewayResponse(500, "Server error", "GET").create_api_gateway_response()
     return ApiGatewayResponse(302, "", "GET").create_api_gateway_response(
         headers=location_header
     )
@@ -48,3 +52,8 @@ def save_state_in_dynamo_db(state):
     ttl = round(time.time()) + 60 * 10
     item = {"State": state, "TimeToExist": ttl}
     dynamodb_service.post_item_service(item=item)
+
+def get_ssm_parameters():
+    ssm_parameters_names = ["OIDC_AUTHORISE_URL", "OIDC_CLIENT_ID"]
+    ssm_client = boto3.client("ssm", region_name="eu-west-2")
+    return ssm_client.get_parameters(Names=ssm_parameters_names)
--- lambdas/tests/unit/handlers/test_redirect_login_handler.py
+++ lambdas/tests/unit/handlers/test_redirect_login_handler.py
@@ -1,55 +1,94 @@
-from unittest.mock import MagicMock
-
+from botocore.exceptions import ClientError
 from handlers import login_redirect_handler
+from oauthlib.oauth2 import InsecureTransportError
 from tests.unit.helpers.ssm_responses import MOCK_MULTI_STRING_PARAMETERS_RESPONSE
 
 from lambdas.utils.lambda_response import ApiGatewayResponse
 
+RETURN_URL = "https://www.string_value_1.com?" \
+              "response_type=code&" \
+              "client_id=1122121212&" \
+              "redirect_uri=https%3A%2F%2Fwww.testexample.com%&" \
+              "state=test1state&" \
+              "scope=openid+profile+nationalrbacaccess+associatedorgs"
 
-def test_return_302_with_correct_headers(mocker, monkeypatch):
-    mock_ssm_client = mocker.Mock()
-    mock_oauth_client = mocker.Mock()
-    mock_oauth_client2 = mocker.Mock()
+class FakeWebAppClient:
+    def __init__(self, *arg, **kwargs):
+        self.state = "test1state"
 
+    def prepare_authorization_request(self, *args, **kwargs):
+        return RETURN_URL, "", ""
+
+class FakeDynamoDBService:
+    def __init__(self, dynamodb_name, *arg, **kwargs):
+        self.dynamodb_name = dynamodb_name
+
+    def post_item_service(self, *args, **kwargs):
+        return None
+def test_prepare_redirect_response_return_302_with_correct_headers(mocker, monkeypatch):
     monkeypatch.setenv("OIDC_CALLBACK_URL", "https://www.testexample.com")
+    mock_dynamo_service = mocker.patch("handlers.login_redirect_handler.save_state_in_dynamo_db")
+    mock_ssm_service = mocker.patch("handlers.login_redirect_handler.get_ssm_parameters", return_value=MOCK_MULTI_STRING_PARAMETERS_RESPONSE)
 
-    mocker.patch("boto3.client", return_value=mock_ssm_client)
-    # mocker.patch("oauthlib.oauth2.Client.state", return_value="test1state", new_callable=mocker.PropertyMock)
-    mocker.patch("oauthlib.oauth2.Client", return_value=mock_oauth_client2)
-    mocker.patch("oauthlib.oauth2.WebApplicationClient", return_value=mock_oauth_client)
-    mock_dynamo_client = mocker.patch("handlers.login_redirect_handler.save_state_in_dynamo_db")
+    response = login_redirect_handler.prepare_redirect_response(FakeWebAppClient)
+    location_header = {"Location": RETURN_URL}
 
-    mock_ssm_client.get_parameters.return_value=MOCK_MULTI_STRING_PARAMETERS_RESPONSE
-
-    return_url = "https://www.string_value_1.com?" \
-                 "response_type=code&" \
-                 "client_id=1122121212&" \
-                 "redirect_uri=https%3A%2F%2Fwww.testexample.com%&" \
-                 "state=test1state&" \
-                 "scope=openid+profile+nationalrbacaccess+associatedorgs"
-    mocker.patch("oauthlib.oauth2.Client.prepare_authorization_request", return_value=(return_url, "", ""))
-
-    response = login_redirect_handler.lambda_handler(event=None, context=None)
-    location_header = {"Location": return_url}
     expected = ApiGatewayResponse(
         302, "", "GET"
     ).create_api_gateway_response(headers=location_header)
 
     assert response == expected
-    mock_dynamo_client.assert_called_with("test1state")
+    mock_dynamo_service.assert_called_with("test1state")
+    mock_ssm_service.assert_called_once()
 
-def test_return_500_when_boto3_client_failing():
-    pass
+def test_prepare_redirect_response_return_500_when_boto3_client_failing(mocker, monkeypatch):
+    monkeypatch.setenv("OIDC_CALLBACK_URL", "https://www.testexample.com")
+    mock_dynamo_service = mocker.patch("handlers.login_redirect_handler.save_state_in_dynamo_db")
+    mock_ssm_service = mocker.patch("handlers.login_redirect_handler.get_ssm_parameters",
+                                    side_effect=ClientError({'Error': {'code' : 500, 'message' : 'mocked error'}}, 'test'))
+    response = login_redirect_handler.prepare_redirect_response(FakeWebAppClient)
 
-def test_return_500_when_auth_client_failing():
-    pass
+    expected = ApiGatewayResponse(
+        500, "Server error", "GET"
+    ).create_api_gateway_response()
 
-def test_save_to_dynamo():
-    pass
+    assert response == expected
+    mock_dynamo_service.assert_not_called()
+    mock_ssm_service.assert_called_once()
 
-# monkeypatch.setenv("AUTH_DYNAMODB_NAME", "test_table")
-# item = {"State": "test1state", "TimeToExist": 1834}
-# mock_dynamo_client.post_item_service.return_value = ""
-# mocker.patch("services.dynamo_services.DynamoDBService", return_value=mock_dynamo_client)
-# mocker.patch("time.time", return_value=1234)
-# mocker.patch("services.dynamo_services.DynamoDBService.post_item_service", return_value="")
+def test_prepare_redirect_response_return_500_when_auth_client_failing(mocker, monkeypatch):
+    monkeypatch.setenv("OIDC_CALLBACK_URL", "https://www.testexample.com")
+    mock_dynamo_service = mocker.patch("handlers.login_redirect_handler.save_state_in_dynamo_db")
+    mock_ssm_service = mocker.patch("handlers.login_redirect_handler.get_ssm_parameters",
+                                    side_effect=InsecureTransportError())
+    response = login_redirect_handler.prepare_redirect_response(FakeWebAppClient)
+
+    expected = ApiGatewayResponse(
+        500, "Server error", "GET"
+    ).create_api_gateway_response()
+
+    assert response == expected
+    mock_dynamo_service.assert_not_called()
+    mock_ssm_service.assert_called_once()
+
+# def test_save_to_dynamo(mocker, monkeypatch):
+#     monkeypatch.setenv("AUTH_DYNAMODB_NAME", "test_table")
+#     mock_dynamo_service = mocker.patch("DynamoDBService")
+#     mock_dynamo_service.return_value = FakeDynamoDBService
+#     mocker.patch("time.time", return_value=1238)
+#     expected_item = {"State": 'test', "TimeToExist": 1838}
+#
+#     login_redirect_handler.save_state_in_dynamo_db('test')
+#
+#     mock_dynamo_service.assert_called_with("test_table")
+#     mock_dynamo_service.post_item_service.assert_called_once()
+#     mock_dynamo_service.post_item_service.assert_called_with(expected_item)
+def test_get_ssm_parameters(mocker):
+    mock_ssm_client = mocker.Mock()
+    mocker.patch("boto3.client", return_value=mock_ssm_client)
+    mock_ssm_client.get_parameters.return_value=MOCK_MULTI_STRING_PARAMETERS_RESPONSE
+    response = login_redirect_handler.get_ssm_parameters()
+
+    expected = MOCK_MULTI_STRING_PARAMETERS_RESPONSE
+
+    assert response == expected
