3dd1e168f0626aeca8807d08904a7447608a0775
Joe Fong <127404525+joefong-nhs@users.noreply.github.com>
2023-09-20 11:34:13 +0100

    Prmdr 167 migrate token handler (#48)
    * [PRMDR-163] Add authoriser lambda from AWS's example
    
    Co-authored-by: NogaNHS <noga.sasson1@nhs.net>
    
    * [PRMDR-95] added jwt to decode token
    
    Co-authored-by: Joe Fong <joe.fong1@nhs.net>
    
    * [PRMDR-163] Change makefile to zip lambda with --platform manylinux2014_x86_64
    
    Co-authored-by: NogaNHS <noga.sasson1@nhs.net>
    
    * [PRMDR-95] clean up and editing ssm response
    
    Co-authored-by: Joe Fong <joe.fong1@nhs.net>
    
    * [PRMDR-95] added test cases
    Co-authored-by: Joe Fong <joe.fong1@nhs.net>
    
    * [PRMDR-163] Add unit test for authoriser lambda, run formatter, removed unused 'context' and 'principalId'
    
    Co-authored-by: NogaNHS <noga.sasson1@nhs.net>
    
    * [PRMDR-163] Run `make format`, edit the linebreaks in amazon Apache license to satisfy linter
    
    Co-authored-by: NogaNHS <noga.sasson1@nhs.net>
    
    * [PRMDR-163] Add region_name to mock ssm client
    
    Co-authored-by: NogaNHS <noga.sasson1@nhs.net>
    
    * [PRMDR-163] Add region_name to ssm client
    
    * [PRMDR-163] Improve variable names in authoriser handler
    
    * [PRMDR-95] initial token_handler.py encode incoming event body
    Co-authored-by: Joe Fong <joe.fong1@nhs.net>
    
    * [PRMDR-95] adding expiry time for token
    Co-authored-by: Joe Fong <joe.fong1@nhs.net>
    
    * [PRMDR-95] test cases
    Co-authored-by: Joe Fong <joe.fong1@nhs.net>
    
    * [PRMDR-167] Add OIDC Service to interact with CIS2
    
    * [PRMDR-167] Add OIDC service to token handler to fetch user token and org codes
    
    * [PRMDR-167] Correct how token handler get auth code, fix format warnings
    
    * [PRMDR-167] Change to fetch OIDC configs from ssm instead of read from env var
    
    * [PRMDR-167] Add ods api service and unit test
    
    Co-authored-by: Richard Smith <richard.smith33@nhs.net>
    
    * [PRMDR-167] Rename folder pds_data to test_data, add test data for ods api response
    
    * [PRMDR-167] Add ID token verification to OIDC service
    
    * [PRMDR-167] Parse response from ODS Api and extract permitted role
    
    * [PRMDR-167] Mock the id token validation in unit test of fetch_tokens
    
    * [PRMDR-167] Add unit test for OIDC service validate_and_decode_token
    
    * Prmdr 163 migrate authorisation handler (#42)
    
    * [PRMDR-163] Add authoriser lambda from AWS's example, added jwt to decode token
    * [PRMDR-163] Add unit test for authoriser lambda
    ---------
    Co-authored-by: NogaNHS <noga.sasson1@nhs.net>
    
    * [PRMDR-167] Add state verification, issuing signed authorisation token, session creation in token lambda
    
    * [PRMDR-167] Amend ods_api_service to return organisation info in dict form, for better readability
    
    * [PRMDR-167] Use a temp dynamo resource instance in token handler. to be switch to DynamoService after merge with other branch
    
    * [PRMDR-167] Add several unit test to lambda
    
    * [PRMDR-167] Add more unit tests to token_handler, improve logging in related code
    
    * [PRMDR-167] Change location of test data file to match other branch, put MockResponse class as a separate helper file
    
    * [PRMDR-167] Amend authoriser handler to check login session and match the new NDR token structure
    
    * [PRMDR-167] Run formatter
    
    * [PRMDR-167] Refactor token handler lambda
    
    * [PRMDR-167] undo unintentional formatter change in .tsx file
    
    * [PRMDR-167] Remove a completed todo comment
    
    * [PRMDR-167] Minor refactor of authoriser_handler lambda
    
    * [PRMDR-167] remove unused comment, undo unintentional formatter change
    
    * [PRMDR-167] Update yml for deploying lambda
    
    * [PRMDR-167] Amend token request lambda to be able to catch the error when both code and state are missing
    
    * [PRMDR-167] Address PR comments
    
    * [PRMDR-167] Add explanatory comments to OIDC IDTokenClaimSet model
    
    * [PRMDR-167] Minor amendment around NDR token expiry time for clarity
    
    ---------
    
    Co-authored-by: NogaNHS <noga.sasson1@nhs.net>
    Co-authored-by: Richard Smith <richard.smith33@nhs.net>

--- lambdas/tests/unit/handlers/test_authoriser_handler.py
+++ lambdas/tests/unit/handlers/test_authoriser_handler.py
@@ -1,44 +1,92 @@
-from dataclasses import dataclass
+import os
+import time
 from unittest.mock import patch
 
-import boto3
 import jwt
 import pytest
+from enums.permitted_role import PermittedRole
 from handlers.authoriser_handler import lambda_handler
-from moto import mock_ssm
 
 MOCK_METHOD_ARN_PREFIX = "arn:aws:execute-api:eu-west-2:fake_arn:fake_api_endpoint/dev"
 TEST_PUBLIC_KEY = "test_public_key"
-TEST_PUBLIC_KEY_SSM_PARAM_NAME = "jwt_token_public_key"
+DENY_ALL_POLICY = {
+    "Statement": [
+        {
+            "Action": "execute-api:Invoke",
+            "Effect": "Deny",
+            "Resource": [f"{MOCK_METHOD_ARN_PREFIX}/*/*"],
+        }
+    ],
+    "Version": "2012-10-17",
+}
 
 
 @pytest.fixture
-def context():
-    @dataclass
-    class LambdaContext:
-        pass
-
-    return LambdaContext()
+def patch_env_vars():
+    env_vars = {
+        "SSM_PARAM_JWT_TOKEN_PUBLIC_KEY": "test_jwt_token_public_key",
+        "AUTH_SESSION_TABLE_NAME": "test_session_table",
+    }
+    with patch.dict(os.environ, env_vars):
+        yield env_vars
 
 
 @pytest.fixture()
-def mock_ssm_public_key():
-    with patch.dict(
-        "os.environ", {"SSM_PARAM_JWT_TOKEN_PUBLIC_KEY": TEST_PUBLIC_KEY_SSM_PARAM_NAME}
-    ):
-        with mock_ssm():
-            ssm = boto3.client("ssm", region_name="eu-west-2")
-            ssm.put_parameter(
-                Name=TEST_PUBLIC_KEY_SSM_PARAM_NAME,
-                Value=TEST_PUBLIC_KEY,
-                Type="SecureString",
-            )
-            yield
+def mock_ssm(patch_env_vars, mocker):
+    mock_ssm_client = mocker.patch("boto3.client")
+    mock_ssm_client.return_value.get_parameter.return_value = {
+        "Parameter": {"Value": TEST_PUBLIC_KEY}
+    }
 
 
-def test_valid_gp_token_return_allow_policy(mocker, mock_ssm_public_key, context):
-    decoded_token = {"user_role": "GP"}
-    decode_mock = mocker.patch("jwt.decode", return_value=decoded_token)
+@pytest.fixture()
+def mock_session_table(mocker):
+    mock_table = mocker.MagicMock()
+    mock_dynamo = mocker.patch("boto3.resource")
+    mock_dynamo.return_value.Table.return_value = mock_table
+
+    valid_session_record = {
+        "Count": 1,
+        "Items": [
+            {
+                "NDRSessionId": "test_session_id",
+                "sid": "test_cis2_session_id",
+                "Subject": "test_cis2_user_id",
+                "TimeToExist": time.time() + 60,
+            }
+        ],
+    }
+    mock_table.query.return_value = valid_session_record
+    yield mock_table
+
+
+def build_decoded_token_for_role(role: str) -> dict:
+    return {
+        "exp": time.time() + 60,
+        "iss": "nhs repo",
+        "ndr_session_id": "test_session_id",
+        "organisations": [
+            {"ods_code": "A9A5A", "org_name": "PORTWAY LIFESTYLE CENTRE", "role": role}
+        ],
+    }
+
+
+@pytest.fixture
+def mock_jwt_decode(mocker):
+    def mocked_decode_method(token: str, *_args, **_kwargs):
+        if token == "valid_gp_token":
+            return build_decoded_token_for_role(PermittedRole.GP.name)
+        elif token == "valid_pcse_token":
+            return build_decoded_token_for_role(PermittedRole.PCSE.name)
+        else:
+            raise jwt.exceptions.InvalidTokenError
+
+    yield mocker.patch("jwt.decode", side_effect=mocked_decode_method)
+
+
+def test_valid_gp_token_return_allow_policy(
+    mock_ssm, mock_session_table, mock_jwt_decode
+):
     expected_allow_policy = {
         "Statement": [
             {
@@ -55,17 +103,17 @@ def test_valid_gp_token_return_allow_policy(mocker, mock_ssm_public_key, context
         "methodArn": f"{MOCK_METHOD_ARN_PREFIX}/GET/SearchPatient",
     }
 
-    response = lambda_handler(test_event, context)
+    response = lambda_handler(event=test_event, context=None)
 
-    decode_mock.assert_called_with(
+    mock_jwt_decode.assert_called_with(
         "valid_gp_token", TEST_PUBLIC_KEY, algorithms=["RS256"]
     )
     assert response["policyDocument"] == expected_allow_policy
 
 
-def test_valid_pcse_token_return_allow_policy(mocker, mock_ssm_public_key, context):
-    decoded_token = {"user_role": "PCSE"}
-    decode_mock = mocker.patch("jwt.decode", return_value=decoded_token)
+def test_valid_pcse_token_return_allow_policy(
+    mock_ssm, mock_session_table, mock_jwt_decode
+):
     expected_allow_policy = {
         "Statement": [
             {
@@ -82,66 +130,86 @@ def test_valid_pcse_token_return_allow_policy(mocker, mock_ssm_public_key, conte
         "methodArn": f"{MOCK_METHOD_ARN_PREFIX}/GET/SearchPatient",
     }
 
-    response = lambda_handler(test_event, context)
+    response = lambda_handler(test_event, context=None)
 
-    decode_mock.assert_called_with(
+    mock_jwt_decode.assert_called_with(
         "valid_pcse_token", TEST_PUBLIC_KEY, algorithms=["RS256"]
     )
     assert response["policyDocument"] == expected_allow_policy
 
 
-def test_invalid_token_return_deny_policy(mocker, mock_ssm_public_key, context):
+def test_return_deny_policy_when_no_session_found(
+    mock_ssm, mock_session_table, mock_jwt_decode
+):
+    mock_session_table.query.return_value = {"Count": 0, "Items": []}
+
+    test_event = {
+        "authorizationToken": "valid_pcse_token",
+        "methodArn": f"{MOCK_METHOD_ARN_PREFIX}/GET/SearchPatient",
+    }
+
+    response = lambda_handler(test_event, context=None)
+
+    assert response["policyDocument"] == DENY_ALL_POLICY
+
+
+def test_return_deny_policy_when_user_session_is_expired(
+    mock_ssm, mock_session_table, mock_jwt_decode
+):
+    one_minute_ago = time.time() - 60
+    expired_session = {
+        "Count": 1,
+        "Items": [
+            {
+                "NDRSessionId": "test_session_id",
+                "sid": "test_cis2_session_id",
+                "Subject": "test_cis2_user_id",
+                "TimeToExist": one_minute_ago,
+            }
+        ],
+    }
+    mock_session_table.query.return_value = expired_session
+
+    test_event = {
+        "authorizationToken": "valid_pcse_token",
+        "methodArn": f"{MOCK_METHOD_ARN_PREFIX}/GET/SearchPatient",
+    }
+
+    response = lambda_handler(test_event, context=None)
+
+    assert response["policyDocument"] == DENY_ALL_POLICY
+
+
+def test_invalid_token_return_deny_policy(mocker, mock_ssm, mock_session_table):
     decode_mock = mocker.patch(
         "jwt.decode", side_effect=jwt.exceptions.InvalidTokenError
     )
-
-    expected_deny_policy = {
-        "Statement": [
-            {
-                "Action": "execute-api:Invoke",
-                "Effect": "Deny",
-                "Resource": [f"{MOCK_METHOD_ARN_PREFIX}/*/*"],
-            }
-        ],
-        "Version": "2012-10-17",
-    }
 
     test_event = {
         "authorizationToken": "invalid_token",
         "methodArn": f"{MOCK_METHOD_ARN_PREFIX}/GET/SearchPatient",
     }
-    response = lambda_handler(test_event, context)
+    response = lambda_handler(test_event, context=None)
 
     decode_mock.assert_called_with(
         "invalid_token", TEST_PUBLIC_KEY, algorithms=["RS256"]
     )
-    assert response["policyDocument"] == expected_deny_policy
+    assert response["policyDocument"] == DENY_ALL_POLICY
 
 
-def test_invalid_signature_return_deny_policy(mocker, mock_ssm_public_key, context):
+def test_invalid_signature_return_deny_policy(mocker, mock_ssm, mock_session_table):
     decode_mock = mocker.patch(
         "jwt.decode", side_effect=jwt.exceptions.InvalidSignatureError
     )
-
-    expected_deny_policy = {
-        "Statement": [
-            {
-                "Action": "execute-api:Invoke",
-                "Effect": "Deny",
-                "Resource": [f"{MOCK_METHOD_ARN_PREFIX}/*/*"],
-            }
-        ],
-        "Version": "2012-10-17",
-    }
 
     test_event = {
         "authorizationToken": "token_with_invalid_signature",
         "methodArn": f"{MOCK_METHOD_ARN_PREFIX}/GET/SearchPatient",
     }
-    response = lambda_handler(test_event, context)
+    response = lambda_handler(test_event, context=None)
 
     decode_mock.assert_called_with(
         "token_with_invalid_signature", TEST_PUBLIC_KEY, algorithms=["RS256"]
     )
 
-    assert response["policyDocument"] == expected_deny_policy
+    assert response["policyDocument"] == DENY_ALL_POLICY
