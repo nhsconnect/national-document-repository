25f3a873033f3b4dfac55f223541bf77e43d12ba
Rio Knightley <rio.knightley2@nhs.net>
2023-09-21 15:54:43 +0100

    Add test lambda env code

--- lambdas/handlers/test_reference_search_handler.py
+++ lambdas/handlers/test_reference_search_handler.py
@@ -2,6 +2,8 @@ import json
 import logging
 import os
 
+from py_dotenv import read_dotenv
+from pathlib import Path
 from botocore.exceptions import ClientError
 from enums.metadata_field_names import DynamoDocumentMetadataTableFields
 from services.dynamo_query_service import DynamoQueryService
@@ -14,9 +16,11 @@ logger.setLevel(logging.INFO)
 
 
 def lambda_handler(event, context):
-    arr = []
-    for name, value in os.environ.items():
-        arr.append("[{0}, {1}]".format(name, value))
+    dotenv_path = os.path.join(os.path.dirname(__file__), '../.env')
+    read_dotenv(dotenv_path)
+
+    list_of_table_names = json.loads(os.environ["DYNAMODB_TABLE_LIST"])
+
     return ApiGatewayResponse(
-    200, json.dumps(arr), "GET"
+    200, json.dumps(list_of_table_names), "GET"
     ).create_api_gateway_response()
