83c020721b41e7fca1ee1f8bea54baeab7212b94
Joe Fong <joe.fong1@nhs.net>
2023-09-22 12:35:12 +0100

    [PRMDR-166] Fix an issue causing 502 error at logout_handler, change response to 400 Bad Request when user logout with an invalid token

--- lambdas/handlers/logout_handler.py
+++ lambdas/handlers/logout_handler.py
@@ -12,14 +12,16 @@ import jwt
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
 def lambda_handler(event, context):
-    token = event['headers']['authorizationToken']
-    logout_handler(token)
+    token = event["headers"]["authorizationToken"]
+    return logout_handler(token)
+
 
 def logout_handler(token):
     try:
         ssm_public_key_parameter_name = os.environ["SSM_PARAM_JWT_TOKEN_PUBLIC_KEY"]
-        ssm_response = get_ssm_parameter(key = ssm_public_key_parameter_name)
+        ssm_response = get_ssm_parameter(key=ssm_public_key_parameter_name)
         jwt_class = jwt
         public_key = ssm_response["Parameter"]["Value"]
         logger.info("decoding token")
@@ -29,30 +31,29 @@ def logout_handler(token):
 
     except ClientError as e:
         logger.error(f"Error logging out user: {e}")
-        return ApiGatewayResponse(500, "Error logging user out", "GET").create_api_gateway_response()
-    except jwt.PyJWTError as e:
+        return ApiGatewayResponse(
+            500, "Error logging user out", "GET"
+        ).create_api_gateway_response()
+    except (jwt.PyJWTError, KeyError) as e:
         logger.error(f"error while decoding JWT: {e}")
-        return ApiGatewayResponse(500, "Error logging user out", "GET").create_api_gateway_response()
+        return ApiGatewayResponse(
+            400, "Invalid authorizationToken", "GET"
+        ).create_api_gateway_response()
     return ApiGatewayResponse(200, "", "GET").create_api_gateway_response()
+
 
 def get_ssm_parameter(key):
     client = boto3.client("ssm", region_name="eu-west-2")
-    ssm_response = client.get_parameter(
-        Name=key, WithDecryption=True
-    )
+    ssm_response = client.get_parameter(Name=key, WithDecryption=True)
     return ssm_response
 
+
 def decode_token(jwt_class, token, key):
-    return jwt_class.decode(
-        token, key, algorithms=["RS256"]
-    )
+    return jwt_class.decode(token, key, algorithms=["RS256"])
+
 
 def remove_session_from_dynamo_db(session_id):
     logger.info(f"Session to be removed: {session_id}")
     dynamodb_name = os.environ["AUTH_DYNAMODB_NAME"]
     dynamodb_service = DynamoDBService(dynamodb_name)
-    dynamodb_service.delete_item_service(
-        key={
-            'NDRSessionId' : session_id
-        }
-    )
+    dynamodb_service.delete_item_service(key={"NDRSessionId": session_id})
