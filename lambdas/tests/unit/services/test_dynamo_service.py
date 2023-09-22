5c86544e9746992376a2c2f0d799485e907deda6
Joe Fong <joe.fong1@nhs.net>
2023-09-22 13:43:41 +0100

    [PRMDR-166] Add unit test for dynamo service delete_item method

--- lambdas/tests/unit/services/test_dynamo_service.py
+++ lambdas/tests/unit/services/test_dynamo_service.py
@@ -117,6 +117,14 @@ def test_post_item_to_dynamo(mock_dynamo_table, mock_boto3_dynamo):
         mock_dynamo_table.put_item.assert_called_once()
 
 
+def test_delete_item(mock_dynamo_table, mock_boto3_dynamo):
+    with patch.object(boto3, "resource", return_value=mock_boto3_dynamo):
+        mock_boto3_dynamo.Table.return_value = mock_dynamo_table
+        db_service = DynamoDBService()
+        db_service.delete_item_service("test_table", {"NhsNumber": "0123456789"})
+        mock_dynamo_table.delete_item.assert_called_with(Key={"NhsNumber": "0123456789"})
+
+
 def test_DynamoDbException_raised_when_results_are_invalid(
     mock_dynamo_table, mock_boto3_dynamo
 ):
