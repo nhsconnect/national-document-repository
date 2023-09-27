53cc7cffe98b8aa5bf7f1ddee2e1e86595c748f0
AlexHerbertNHS <alexandra.herbert1@nhs.net>
2023-09-27 09:44:06 +0100

    [PRMDR-270]
    WIP adding tests

--- lambdas/tests/unit/handlers/test_lloyd_george_record_stitch_handler.py
+++ lambdas/tests/unit/handlers/test_lloyd_george_record_stitch_handler.py
@@ -1,12 +1,26 @@
 from handlers.lloyd_george_record_stitch_handler import lambda_handler
 from utils.lambda_response import ApiGatewayResponse
 
-MISSING_NHS_NUMBER_KEY_ERROR = ApiGatewayResponse(
-    400, f"An error occurred due to missing key: 'patientId'", "GET"
-).create_api_gateway_response()
-
 
 def test_throws_error_when_no_nhs_number_supplied(missing_id_event, context):
     actual = lambda_handler(missing_id_event, context)
-    expected = MISSING_NHS_NUMBER_KEY_ERROR
+    expected = ApiGatewayResponse(
+        400, f"An error occurred due to missing key: 'patientId'", "GET"
+    ).create_api_gateway_response()
+    assert actual == expected
+
+
+def test_throws_error_when_environment_variables_not_set(valid_id_event, context):
+    actual = lambda_handler(valid_id_event, context)
+    expected = ApiGatewayResponse(
+        400, f"An error occurred due to missing key: 'LLOYD_GEORGE_DYNAMODB_NAME'", "GET"
+    ).create_api_gateway_response()
+    assert actual == expected
+
+
+def test_throws_error_when_nhs_number_not_valid(invalid_id_event, context):
+    actual = lambda_handler(invalid_id_event, context)
+    expected = ApiGatewayResponse(
+        400, f"Invalid NHS number", "GET"
+    ).create_api_gateway_response()
     assert actual == expected
