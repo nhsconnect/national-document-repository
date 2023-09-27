c074e3583c3de2952243d3188828d43ecb41cd3e
RachelHowellNHS <rachel.howell6@nhs.net>
2023-09-27 15:20:30 +0100

    [PRMDR-268] add search function to LG page and LG record request to helpers

--- app/src/pages/lloydGeorgeRecordPage/LloydGeorgeRecordPage.tsx
+++ app/src/pages/lloydGeorgeRecordPage/LloydGeorgeRecordPage.tsx
@@ -1,13 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { usePatientDetailsContext } from '../../providers/patientProvider/PatientProvider';
 import { getFormattedDate } from '../../helpers/utils/formatDate';
 import { useNavigate } from 'react-router';
 import { routes } from '../../types/generic/routes';
 import { Card } from 'nhsuk-react-components';
+import { useBaseAPIUrl } from '../../providers/configProvider/ConfigProvider';
+import getLloydGeorgeRecord from '../../helpers/requests/lloydGeorgeSearchResult';
 
 function LloydGeorgeRecordPage() {
     const [patientDetails] = usePatientDetailsContext();
+    const [lloydGeorgeRecord, setLloydGeorgeRecord] = useState();
     const navigate = useNavigate();
+    const baseUrl = useBaseAPIUrl();
 
     const dob = patientDetails?.birthDate
         ? getFormattedDate(new Date(patientDetails.birthDate))
@@ -15,9 +19,9 @@ function LloydGeorgeRecordPage() {
 
     const patientInfo = (
         <>
-            <p
-                style={{ fontSize: '24px', marginBottom: 5, fontWeight: '600' }}
-            >{`${patientDetails?.givenName} ${patientDetails?.familyName}`}</p>
+            <p style={{ fontSize: '24px', marginBottom: 5, fontWeight: '600' }}>
+                {`${patientDetails?.givenName} ${patientDetails?.familyName}`}
+            </p>
             <p style={{ fontSize: '20px', marginBottom: 5 }}>
                 NHS number: {patientDetails?.nhsNumber}
             </p>
@@ -29,6 +33,18 @@ function LloydGeorgeRecordPage() {
         if (!patientDetails) {
             navigate(routes.HOME);
         }
+        // search function that sets LG state based on request(to be created based on documentSearchResults request)
+        // request will trigger lambda and either return pdf or null?
+        // submission state from DocumentSearchResultsPage not needed?
+        const search = async () => {
+            const nhsNumber: string = patientDetails?.nhsNumber || '';
+
+            const result = await getLloydGeorgeRecord({ nhsNumber, baseUrl });
+
+            if (result.length > 0) {
+                setLloydGeorgeRecord(result);
+            }
+        };
     }, [patientDetails, navigate]);
 
     return (
@@ -37,7 +53,9 @@ function LloydGeorgeRecordPage() {
             <Card>
                 <Card.Content>
                     <Card.Heading>Lloyd George Record</Card.Heading>
-                    <Card.Description>No documents are available</Card.Description>
+                    <Card.Description>
+                        {lloydGeorgeRecord ? 'display LG' : 'No documents are available'}
+                    </Card.Description>
                 </Card.Content>
             </Card>
         </>
