9dff76b880e8e644605ee9358661b0f5840d25c7
Rio Knightley <rio.knightley2@nhs.net>
2023-09-27 12:11:16 +0100

    Add endpoints type

---
+++ app/src/types/generic/endpoints.ts
@@ -0,0 +1,7 @@
+export enum endpoints {
+    LOGIN = '/Login',
+    AUTH = '/TokenRequest',
+    PATIENT_SEARCH = '/SearchPatient',
+    DOCUMENT_SEARCH = '/DocumentReference',
+    DOCUMENT_UPLOAD = '/DocumentReference',
+}
