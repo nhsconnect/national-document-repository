310838366d3e114b4d093a0f556edf5f5d3711b6
AlexHerbertNHS <alexandra.herbert1@nhs.net>
2023-09-27 09:41:51 +0100

    [PRMDR-270]
    WIP adding tests

--- lambdas/handlers/lloyd_george_record_stitch_handler.py
+++ lambdas/handlers/lloyd_george_record_stitch_handler.py
@@ -48,6 +48,7 @@ def lambda_handler(event, context):
 
 
 
+
     # Get the patient's list of docs from the NDR LG Dynamo table
     # Download them all in order, their filenames should impose an order
     # File names are stored in Dynamo which is why we need it first
