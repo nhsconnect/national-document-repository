1f41debbc665561e83ec0c8980efecd0d354b5b1
abbas-khan10 <127417949+abbas-khan10@users.noreply.github.com>
2023-09-28 15:02:12 +0100

    [PRMDR-99] - Fix error when parsing list of dynamo tables from environment variables

--- lambdas/handlers/document_reference_search_handler.py
+++ lambdas/handlers/document_reference_search_handler.py
@@ -20,7 +20,10 @@ def lambda_handler(event, context):
         nhs_number = event["queryStringParameters"]["patientId"]
         validate_id(nhs_number)
 
-        list_of_table_names = json.loads(os.environ["DYNAMODB_TABLE_LIST"])
+        list_of_table_names = [
+            table_name.strip()
+            for table_name in os.environ["DYNAMODB_TABLE_LIST"].strip("[]").split(",")
+        ]
 
     except InvalidResourceIdException:
         return ApiGatewayResponse(
