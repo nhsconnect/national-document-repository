9f3dc06440280f017b83730e8dd92b58a175eccf
abbas-khan10 <abbas.khan10@nhs.net>
2023-09-28 14:34:34 +0100

    Fix error when parsing list of dynamo tables from environment variables

--- lambdas/tests/unit/conftest.py
+++ lambdas/tests/unit/conftest.py
@@ -1,5 +1,4 @@
 import pytest
-import json
 
 REGION_NAME = "eu-west-2"
 
@@ -28,6 +27,8 @@ def set_env(monkeypatch):
     monkeypatch.setenv("DOCUMENT_STORE_BUCKET_NAME", MOCK_ARF_BUCKET)
     monkeypatch.setenv("LLOYD_GEORGE_BUCKET_NAME", MOCK_LG_BUCKET)
     monkeypatch.setenv("LLOYD_GEORGE_DYNAMODB_NAME", MOCK_LG_TABLE_NAME)
-    monkeypatch.setenv("DYNAMODB_TABLE_LIST", json.dumps([MOCK_ARF_TABLE_NAME, MOCK_LG_TABLE_NAME]))
+    monkeypatch.setenv(
+        "DYNAMODB_TABLE_LIST", f"[{MOCK_ARF_TABLE_NAME}, {MOCK_LG_TABLE_NAME}]"
+    )
     monkeypatch.setenv("ZIPPED_STORE_BUCKET_NAME", MOCK_ZIP_OUTPUT_BUCKET)
     monkeypatch.setenv("ZIPPED_STORE_DYNAMODB_NAME", MOCK_ZIP_TRACE_TABLE)
