944a3a60cbfb2395b8ebbcb2c61b0603e3d2ee5b
Scott Alexander <scott.alexander@madetech.com>
2023-09-29 16:52:07 +0100

    cypress tests have been fixed for the upload page following LG file name validation implementation

--- app/cypress/e2e/0-ndr-core-tests/gp_upload_workflow_step2_individual_patient_document_upload.cy.js
+++ app/cypress/e2e/0-ndr-core-tests/gp_upload_workflow_step2_individual_patient_document_upload.cy.js
@@ -61,7 +61,10 @@ const testStartAgainButton = () => {
 const uploadedFilePathNames = {
     ARF: [
         'cypress/fixtures/test_patient_record.pdf',
-        'cypress/fixtures/test_patient_record_two.pdf',
+        [
+            'cypress/fixtures/test_patient_record.pdf',
+            'cypress/fixtures/test_patient_record_two.pdf',
+        ],
     ],
     LG: [
         'cypress/fixtures/lg-files/1of1_Lloyd_George_Record_[Testy Test]_[0123456789]_[01-01-2011].pdf',
@@ -73,10 +76,9 @@ const uploadedFilePathNames = {
 };
 
 const uploadedFileNames = {
-    ARF: ['test_patient_record.pdf', 'test_patient_record_two.pdf'],
+    ARF: ['test_patient_record.pdf', ['test_patient_record.pdf', 'test_patient_record_two.pdf']],
     LG: [
         '1of1_Lloyd_George_Record_[Testy Test]_[0123456789]_[01-01-2011].pdf',
-
         [
             '1of2_Lloyd_George_Record_[Testy Test]_[0123456789]_[01-01-2011].pdf',
             '2of2_Lloyd_George_Record_[Testy Test]_[0123456789]_[01-01-2011].pdf',
@@ -84,24 +86,11 @@ const uploadedFileNames = {
     ],
 };
 
-const uploadedImagesPathNames = [
-    'cypress/fixtures/test-images/test_image.jpg',
-    'cypress/fixtures/test-images/test_image_two.jpg',
-    'cypress/fixtures/test-images/test_image_three.jpg',
-    'cypress/fixtures/test-images/test_image_four.jpg',
-    'cypress/fixtures/test-images/test_image_five.jpg',
-    'cypress/fixtures/test-images/test_image_six.jpg',
-    'cypress/fixtures/test-images/test_image_seven.jpg',
-    'cypress/fixtures/test-images/test_image_eight.jpg',
-    'cypress/fixtures/test-images/test_image_nine.jpg',
-    'cypress/fixtures/test-images/test_image_ten.jpg',
-    'cypress/fixtures/test-images/test_image_eleven.jpg',
-];
-
 beforeEach(() => {
     cy.visit(baseUrl);
     navigateToUploadPage();
 });
+
 describe('[ALL] GP Upload Workflow Step 2: Uploads docs and tests it looks OK', () => {
     it('(Smoke test) On Start now button click, redirect to uploads is successful', () => {
         cy.url().should('include', 'upload');
@@ -160,7 +149,7 @@ Object.values(formTypes).forEach((type) => {
                 .first()
                 .get('td')
                 .first()
-                .should('have.text', uploadedFileNames[type][0]);
+                .should('contain.text', uploadedFileNames[type][0]);
         });
 
         it(`Single file - On Upload button click, renders Upload Summary with error box when DocumentReference returns a 500 for ${type} input`, () => {
@@ -193,7 +182,7 @@ Object.values(formTypes).forEach((type) => {
                 statusCode: 404,
             });
 
-            selectForm(type).selectFile(uploadedFilePathNames[type][1]);
+            selectForm(type).selectFile(uploadedFilePathNames[type][0]);
 
             clickUploadButton();
 
@@ -202,7 +191,7 @@ Object.values(formTypes).forEach((type) => {
             cy.get('#failed-uploads tbody tr').should('have.length', 1);
             cy.get('#failed-uploads tbody tr')
                 .first()
-                .get('td')
+                .children()
                 .first()
                 .should('contain', uploadedFileNames[type][0]);
             cy.get('#failed-uploads').should('contain', '1 of 1 files failed to upload');
@@ -219,7 +208,7 @@ Object.values(formTypes).forEach((type) => {
                 statusCode: 500,
             });
 
-            selectForm(type).selectFile(uploadedImagesPathNames[0]);
+            selectForm(type).selectFile(uploadedFilePathNames[type][0]);
 
             clickUploadButton();
 
@@ -228,9 +217,9 @@ Object.values(formTypes).forEach((type) => {
             cy.get('#failed-uploads tbody tr').should('have.length', 1);
             cy.get('#failed-uploads tbody tr')
                 .first()
-                .get('td')
+                .children()
                 .first()
-                .should('contain', 'test_image.jpg');
+                .should('contain.text', uploadedFileNames[type][0]);
             cy.get('#failed-uploads').should('contain', '1 of 1 files failed to upload');
             cy.get('#close-page-warning').should('be.visible');
 
@@ -239,17 +228,20 @@ Object.values(formTypes).forEach((type) => {
 
         it(`(Smoke test) Multiple files - On Choose files button click, file selection is visible for ${type} input`, () => {
             cy.get('#selected-documents-table').should('not.exist');
-            selectForm(type).selectFile(uploadedFilePathNames[type]);
+            selectForm(type).selectFile(uploadedFilePathNames[type][1]);
             cy.get('#selected-documents-table').should('be.visible');
             cy.get('#selected-documents-table tbody tr').should('have.length', 2);
+
             cy.get('#selected-documents-table tbody tr')
                 .first()
-                .get('td')
+                .children()
                 .first()
-                .should('have.text', uploadedFileNames[type][0]);
+                .should('have.text', uploadedFileNames[type][1][0]);
             cy.get('#selected-documents-table tbody tr')
-                .next()
-                .contains('td', uploadedFileNames[type][1]);
+                .eq(1)
+                .children()
+                .first()
+                .should('have.text', uploadedFileNames[type][1][1]);
         });
 
         it(`(Smoke test) Multiple files - On Upload button click, renders Upload Summary for successful upload for ${type} input`, () => {
@@ -274,7 +266,7 @@ Object.values(formTypes).forEach((type) => {
                 });
             }
 
-            selectForm(type).selectFile(uploadedFilePathNames[type]);
+            selectForm(type).selectFile(uploadedFilePathNames[type][1]);
 
             clickUploadButton();
 
@@ -284,9 +276,9 @@ Object.values(formTypes).forEach((type) => {
             cy.get('#successful-uploads tbody tr').should('have.length', 2);
             cy.get('#successful-uploads tbody tr')
                 .first()
-                .get('td')
+                .children()
                 .first()
-                .should('contain', uploadedFileNames[type][0]);
+                .should('contain', uploadedFileNames[type][1][0]);
             cy.get('#close-page-warning').should('be.visible');
 
             testStartAgainButton();
@@ -296,30 +288,24 @@ Object.values(formTypes).forEach((type) => {
             cy.intercept('POST', '**/DocumentReference*', (req) => {
                 req.reply({
                     statusCode: 200,
+                    delay: 1500,
                     body: {
                         url: bucketUrlIdentifer,
                         fields: {
-                            key: '382d3db4-9625-4bd9-ad46-c2e31451efd2',
+                            key: 'xxxxxxxxxxxx2',
                             'x-amz-algorithm': 'AWS4-HMAC-SHA256',
                             'x-amz-credential':
-                                'ASIAXYSUA44VYD2FFBHK/20230904/eu-west-2/s3/aws4_request',
+                                'xxxxxxxxxxxxxxxxxx/20230904/eu-west-2/s3/aws4_request',
                             'x-amz-date': '20230904T125954Z',
-                            'x-amz-security-token':
-                                'IQoJb3JpZ2luX2VjEG0aCWV1LXdlc3QtMiJHMEUCIQC1Mx4VTrF2alA9s8P5HJ+j9y2XJmAF5ODdZGEui/QMGwIgRdGiuXVqq8Uq6l5dJZpAx39b1kro7x1Q0FQn6rWRUvUqggMIRhABGgw1MzM4MjU5MDY0NzUiDOeBIQxpj1BmAtTeVSrfAsugZpo7cQzj+R0uwE9o3bxMyR7lqdVCkaAb/ZVF4iBY/Cn7FYCB+pO3nvS6CWLClcIbNhhZkqgJOZvBJ+fua5QvchxX4LO+sq0Or+s29Ym8mSNHpB4mLi3iyQZ08Iw1p9c52Mfo6B5mh5IGKu8gvKf1y9gOWKAJTTuLbuzKKkbII4tRr+1PSAeFwLgSSzeDInv9QnTbKAPSbVMAWBic07MTlQpeMD3SNiXqKz+f/HNiujCOxfvUO0Yvw5GXx0FBrGjaHY979YGJuuC35yMqnFG0tvdd/8OfdUhHDM6XqRMGmceMkdMldQsw4VwhSd+uI4qIrHopMNQ+XKdqDjrkmlRprATIT9boO1fQ1GFO2l1YVVM2WzORIwVd2hGQr6anfXyOePk8N8MYNuzwZdgOiEKPwS2b6vByMBr/U7jNUuUZNd9rhbZIv5dsMF2xszXq8co3ZngvPjWKYsdZF6eGUDDHqdenBjqeAYZZ0oBZqtgVF3ebT86UWZ40zS0vGZSzSt5P5DhlTP01LRPeQCARoETZ/IsUzvrP5QLwmkOjSoJlYIu/U1SeqvVZblnPHsHVi/0QI4FuZhPL9LnA628+euBTaO6I2DSf1ugPmwMc/YMYwHQdkSKNsejAPczYSxAyPAWLtiLckMPqo5YyyiRYMHpmZwrqJsdbAVogVhIgfYx53OJIhblI',
-                            policy: 'eyJleHBpcmF0aW9uIjogIjIwMjMtMDktMDRUMTM6Mjk6NTRaIiwgImNvbmRpdGlvbnMiOiBbeyJidWNrZXQiOiAibmRyYS1kb2N1bWVudC1zdG9yZSJ9LCB7ImtleSI6ICIzODJkM2RiNC05NjI1LTRiZDktYWQ0Ni1jMmUzMTQ1MWVmZDIifSwgeyJ4LWFtei1hbGdvcml0aG0iOiAiQVdTNC1ITUFDLVNIQTI1NiJ9LCB7IngtYW16LWNyZWRlbnRpYWwiOiAiQVNJQVhZU1VBNDRWWUQyRkZCSEsvMjAyMzA5MDQvZXUtd2VzdC0yL3MzL2F3czRfcmVxdWVzdCJ9LCB7IngtYW16LWRhdGUiOiAiMjAyMzA5MDRUMTI1OTU0WiJ9LCB7IngtYW16LXNlY3VyaXR5LXRva2VuIjogIklRb0piM0pwWjJsdVgyVmpFRzBhQ1dWMUxYZGxjM1F0TWlKSE1FVUNJUUMxTXg0VlRyRjJhbEE5czhQNUhKK2o5eTJYSm1BRjVPRGRaR0V1aS9RTUd3SWdSZEdpdVhWcXE4VXE2bDVkSlpwQXgzOWIxa3JvN3gxUTBGUW42cldSVXZVcWdnTUlSaEFCR2d3MU16TTRNalU1TURZME56VWlET2VCSVF4cGoxQm1BdFRlVlNyZkFzdWdacG83Y1F6aitSMHV3RTlvM2J4TXlSN2xxZFZDa2FBYi9aVkY0aUJZL0NuN0ZZQ0IrcE8zbnZTNkNXTENsY0liTmhoWmtxZ0pPWnZCSitmdWE1UXZjaHhYNExPK3NxME9yK3MyOVltOG1TTkhwQjRtTGkzaXlRWjA4SXcxcDljNTJNZm82QjVtaDVJR0t1OGd2S2YxeTlnT1dLQUpUVHVMYnV6S0trYklJNHRScisxUFNBZUZ3TGdTU3plREludjlRblRiS0FQU2JWTUFXQmljMDdNVGxRcGVNRDNTTmlYcUt6K2YvSE5pdWpDT3hmdlVPMFl2dzVHWHgwRkJyR2phSFk5NzlZR0p1dUMzNXlNcW5GRzB0dmRkLzhPZmRVaEhETTZYcVJNR21jZU1rZE1sZFFzdzRWd2hTZCt1STRxSXJIb3BNTlErWEtkcURqcmttbFJwckFUSVQ5Ym9PMWZRMUdGTzJsMVlWVk0yV3pPUkl3VmQyaEdRcjZhbmZYeU9lUGs4TjhNWU51endaZGdPaUVLUHdTMmI2dkJ5TUJyL1U3ak5VdVVaTmQ5cmhiWkl2NWRzTUYyeHN6WHE4Y28zWm5ndlBqV0tZc2RaRjZlR1VEREhxZGVuQmpxZUFZWlowb0JacXRnVkYzZWJUODZVV1o0MHpTMHZHWlN6U3Q1UDVEaGxUUDAxTFJQZVFDQVJvRVRaL0lzVXp2clA1UUx3bWtPalNvSmxZSXUvVTFTZXF2VlpibG5QSHNIVmkvMFFJNEZ1WmhQTDlMbkE2MjgrZXVCVGFPNkkyRFNmMXVnUG13TWMvWU1Zd0hRZGtTS05zZWpBUGN6WVN4QXlQQVdMdGlMY2tNUHFvNVl5eWlSWU1IcG1ad3JxSnNkYkFWb2dWaElnZll4NTNPSkloYmxJIn1dfQ==',
-                            'x-amz-signature':
-                                '9e861ee405f4930c8fd992e631fa202563b50b23c2f505cbfcbd407198cd594d',
+                            'x-amz-security-token': 'xxxxxxxxxxxxxxxx',
+                            policy: 'xxxxxxxxx====',
+                            'x-amz-signature': 'xxxxxxxxx',
                         },
                     },
                 });
-                req.on('response', (res) => {
-                    // Throttle the response to 1 Mbps to simulate a
-                    // mobile 3G connection
-                    res.setThrottle(1000);
-                });
             });
 
-            selectForm(type).selectFile(uploadedImagesPathNames);
+            selectForm(type).selectFile(uploadedFilePathNames[type][1]);
 
             clickUploadButton();
 
@@ -332,19 +318,28 @@ Object.values(formTypes).forEach((type) => {
                 statusCode: 500,
             });
 
-            selectForm(type).selectFile(uploadedImagesPathNames);
+            selectForm(type).selectFile(uploadedFilePathNames[type][1]);
 
             clickUploadButton();
 
             cy.get('#failed-document-uploads-summary-title').should('be.visible');
             cy.get('#failed-uploads').should('be.visible');
-            cy.get('#failed-uploads tbody tr').should('have.length', 11);
+            cy.get('#failed-uploads tbody tr').should(
+                'have.length',
+                uploadedFilePathNames[type][1].length,
+            );
             cy.get('#failed-uploads tbody tr')
                 .first()
-                .get('td')
+                .children()
                 .first()
-                .should('contain', 'test_image.jpg');
-            cy.get('#failed-uploads').should('contain', '11 of 11 files failed to upload');
+                .should('contain.text', uploadedFileNames[type][1][0]);
+            cy.get('#failed-uploads').should(
+                'contain',
+                uploadedFilePathNames[type][1].length +
+                    ' of ' +
+                    uploadedFilePathNames[type][1].length +
+                    ' files failed to upload',
+            );
             cy.get('#close-page-warning').should('be.visible');
 
             testStartAgainButton();
@@ -356,19 +351,28 @@ Object.values(formTypes).forEach((type) => {
                 statusCode: 404,
             });
 
-            selectForm(type).selectFile(uploadedImagesPathNames);
+            selectForm(type).selectFile(uploadedFilePathNames[type][1]);
 
             clickUploadButton();
 
             cy.get('#failed-document-uploads-summary-title').should('be.visible');
             cy.get('#failed-uploads').should('be.visible');
-            cy.get('#failed-uploads tbody tr').should('have.length', 11);
+            cy.get('#failed-uploads tbody tr').should(
+                'have.length',
+                uploadedFilePathNames[type][1].length,
+            );
             cy.get('#failed-uploads tbody tr')
                 .first()
-                .get('td')
+                .children()
                 .first()
-                .should('contain', 'test_image.jpg');
-            cy.get('#failed-uploads').should('contain', '11 of 11 files failed to upload');
+                .should('contain.text', uploadedFileNames[type][1][0]);
+            cy.get('#failed-uploads').should(
+                'contain',
+                uploadedFilePathNames[type][1].length +
+                    ' of ' +
+                    uploadedFilePathNames[type][1].length +
+                    ' files failed to upload',
+            );
             cy.get('#close-page-warning').should('be.visible');
 
             testStartAgainButton();
@@ -380,16 +384,13 @@ Object.values(formTypes).forEach((type) => {
                 body: {
                     url: bucketUrlIdentifer,
                     fields: {
-                        key: '382d3db4-9625-4bd9-ad46-c2e31451efd2',
+                        key: 'xxxxxxxxxxxx2',
                         'x-amz-algorithm': 'AWS4-HMAC-SHA256',
-                        'x-amz-credential':
-                            'ASIAXYSUA44VYD2FFBHK/20230904/eu-west-2/s3/aws4_request',
+                        'x-amz-credential': 'xxxxxxxxxxxxxxxxxx/20230904/eu-west-2/s3/aws4_request',
                         'x-amz-date': '20230904T125954Z',
-                        'x-amz-security-token':
-                            'IQoJb3JpZ2luX2VjEG0aCWV1LXdlc3QtMiJHMEUCIQC1Mx4VTrF2alA9s8P5HJ+j9y2XJmAF5ODdZGEui/QMGwIgRdGiuXVqq8Uq6l5dJZpAx39b1kro7x1Q0FQn6rWRUvUqggMIRhABGgw1MzM4MjU5MDY0NzUiDOeBIQxpj1BmAtTeVSrfAsugZpo7cQzj+R0uwE9o3bxMyR7lqdVCkaAb/ZVF4iBY/Cn7FYCB+pO3nvS6CWLClcIbNhhZkqgJOZvBJ+fua5QvchxX4LO+sq0Or+s29Ym8mSNHpB4mLi3iyQZ08Iw1p9c52Mfo6B5mh5IGKu8gvKf1y9gOWKAJTTuLbuzKKkbII4tRr+1PSAeFwLgSSzeDInv9QnTbKAPSbVMAWBic07MTlQpeMD3SNiXqKz+f/HNiujCOxfvUO0Yvw5GXx0FBrGjaHY979YGJuuC35yMqnFG0tvdd/8OfdUhHDM6XqRMGmceMkdMldQsw4VwhSd+uI4qIrHopMNQ+XKdqDjrkmlRprATIT9boO1fQ1GFO2l1YVVM2WzORIwVd2hGQr6anfXyOePk8N8MYNuzwZdgOiEKPwS2b6vByMBr/U7jNUuUZNd9rhbZIv5dsMF2xszXq8co3ZngvPjWKYsdZF6eGUDDHqdenBjqeAYZZ0oBZqtgVF3ebT86UWZ40zS0vGZSzSt5P5DhlTP01LRPeQCARoETZ/IsUzvrP5QLwmkOjSoJlYIu/U1SeqvVZblnPHsHVi/0QI4FuZhPL9LnA628+euBTaO6I2DSf1ugPmwMc/YMYwHQdkSKNsejAPczYSxAyPAWLtiLckMPqo5YyyiRYMHpmZwrqJsdbAVogVhIgfYx53OJIhblI',
-                        policy: 'eyJleHBpcmF0aW9uIjogIjIwMjMtMDktMDRUMTM6Mjk6NTRaIiwgImNvbmRpdGlvbnMiOiBbeyJidWNrZXQiOiAibmRyYS1kb2N1bWVudC1zdG9yZSJ9LCB7ImtleSI6ICIzODJkM2RiNC05NjI1LTRiZDktYWQ0Ni1jMmUzMTQ1MWVmZDIifSwgeyJ4LWFtei1hbGdvcml0aG0iOiAiQVdTNC1ITUFDLVNIQTI1NiJ9LCB7IngtYW16LWNyZWRlbnRpYWwiOiAiQVNJQVhZU1VBNDRWWUQyRkZCSEsvMjAyMzA5MDQvZXUtd2VzdC0yL3MzL2F3czRfcmVxdWVzdCJ9LCB7IngtYW16LWRhdGUiOiAiMjAyMzA5MDRUMTI1OTU0WiJ9LCB7IngtYW16LXNlY3VyaXR5LXRva2VuIjogIklRb0piM0pwWjJsdVgyVmpFRzBhQ1dWMUxYZGxjM1F0TWlKSE1FVUNJUUMxTXg0VlRyRjJhbEE5czhQNUhKK2o5eTJYSm1BRjVPRGRaR0V1aS9RTUd3SWdSZEdpdVhWcXE4VXE2bDVkSlpwQXgzOWIxa3JvN3gxUTBGUW42cldSVXZVcWdnTUlSaEFCR2d3MU16TTRNalU1TURZME56VWlET2VCSVF4cGoxQm1BdFRlVlNyZkFzdWdacG83Y1F6aitSMHV3RTlvM2J4TXlSN2xxZFZDa2FBYi9aVkY0aUJZL0NuN0ZZQ0IrcE8zbnZTNkNXTENsY0liTmhoWmtxZ0pPWnZCSitmdWE1UXZjaHhYNExPK3NxME9yK3MyOVltOG1TTkhwQjRtTGkzaXlRWjA4SXcxcDljNTJNZm82QjVtaDVJR0t1OGd2S2YxeTlnT1dLQUpUVHVMYnV6S0trYklJNHRScisxUFNBZUZ3TGdTU3plREludjlRblRiS0FQU2JWTUFXQmljMDdNVGxRcGVNRDNTTmlYcUt6K2YvSE5pdWpDT3hmdlVPMFl2dzVHWHgwRkJyR2phSFk5NzlZR0p1dUMzNXlNcW5GRzB0dmRkLzhPZmRVaEhETTZYcVJNR21jZU1rZE1sZFFzdzRWd2hTZCt1STRxSXJIb3BNTlErWEtkcURqcmttbFJwckFUSVQ5Ym9PMWZRMUdGTzJsMVlWVk0yV3pPUkl3VmQyaEdRcjZhbmZYeU9lUGs4TjhNWU51endaZGdPaUVLUHdTMmI2dkJ5TUJyL1U3ak5VdVVaTmQ5cmhiWkl2NWRzTUYyeHN6WHE4Y28zWm5ndlBqV0tZc2RaRjZlR1VEREhxZGVuQmpxZUFZWlowb0JacXRnVkYzZWJUODZVV1o0MHpTMHZHWlN6U3Q1UDVEaGxUUDAxTFJQZVFDQVJvRVRaL0lzVXp2clA1UUx3bWtPalNvSmxZSXUvVTFTZXF2VlpibG5QSHNIVmkvMFFJNEZ1WmhQTDlMbkE2MjgrZXVCVGFPNkkyRFNmMXVnUG13TWMvWU1Zd0hRZGtTS05zZWpBUGN6WVN4QXlQQVdMdGlMY2tNUHFvNVl5eWlSWU1IcG1ad3JxSnNkYkFWb2dWaElnZll4NTNPSkloYmxJIn1dfQ==',
-                        'x-amz-signature':
-                            '9e861ee405f4930c8fd992e631fa202563b50b23c2f505cbfcbd407198cd594d',
+                        'x-amz-security-token': 'xxxxxxxxxxxxxxxx',
+                        policy: 'xxxxxxxxx====',
+                        'x-amz-signature': 'xxxxxxxxx',
                     },
                 },
             });
@@ -402,14 +403,14 @@ Object.values(formTypes).forEach((type) => {
                 });
             });
 
-            selectForm(type).selectFile(uploadedImagesPathNames);
+            selectForm(type).selectFile(uploadedFilePathNames[type][1]);
 
             clickUploadButton();
 
             cy.get('#upload-summary-header').should('be.visible');
-            cy.get('#failed-uploads').should('contain', '6 of 11 files failed to upload');
-            cy.get('#failed-uploads tbody tr').should('have.length', 6);
-            cy.get('#successful-uploads tbody tr').should('have.length', 5);
+            cy.get('#failed-uploads').should('contain', '1 of 2 files failed to upload');
+            cy.get('#failed-uploads tbody tr').should('have.length', 1);
+            cy.get('#successful-uploads tbody tr').should('have.length', 1);
             cy.get('#close-page-warning').should('be.visible');
 
             testStartAgainButton();
