54de55684d013e37784839d5fa4937010586a6b7
Rio Knightley <rio.knightley2@nhs.net>
2023-10-09 16:20:12 +0100

    Add error page tests

---
+++ app/src/pages/unauthorisedPage/UnauthorisedPage.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import * as ReactRouter from 'react-router';
+import UnauthorisedPage from './UnauthorisedPage';
+import { createMemoryHistory } from 'history';
+describe('UnauthorisedPage', () => {
+    it('renders unauthorised message', () => {
+        const history = createMemoryHistory({
+            initialEntries: ['/'],
+            initialIndex: 0,
+        });
+        render(
+            <ReactRouter.Router navigator={history} location={history.location}>
+                <UnauthorisedPage />
+            </ReactRouter.Router>,
+        );
+        expect(screen.getByText('Unauthorised access')).toBeInTheDocument();
+    });
+});
