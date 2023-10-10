ea9cd2d2974263c226f4fdc6199118558ee25e0b
Rio Knightley <rio.knightley2@nhs.net>
2023-10-10 11:10:57 +0100

    Add session provider unit tests:

---
+++ app/src/providers/sessionProvider/SessionProvider.test.tsx
@@ -0,0 +1,79 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import SessionProvider, { useSessionContext } from './SessionProvider';
+import { buildUserAuth } from '../../helpers/test/testBuilders';
+import userEvent from '@testing-library/user-event';
+const loggedIn = {
+    auth: buildUserAuth(),
+    isLoggedIn: true,
+};
+const loggedOut = {
+    auth: null,
+    isLoggedIn: false,
+};
+describe('SessionProvider', () => {
+    beforeEach(() => {
+        process.env.REACT_APP_ENVIRONMENT = 'jest';
+    });
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('is able to set and retrieve auth data when user has logged in', async () => {
+        renderSessionProvider();
+        expect(screen.getByText('isLoggedIn - false')).toBeInTheDocument();
+        expect(screen.getByText('authCode - undefined')).toBeInTheDocument();
+
+        userEvent.click(screen.getByText('Log in'));
+        await waitFor(async () => {
+            expect(screen.getByText('isLoggedIn - true')).toBeInTheDocument();
+        });
+        expect(
+            screen.getByText(`authCode - ${loggedIn.auth.authorisation_token}`),
+        ).toBeInTheDocument();
+    });
+
+    it('is able to delete auth data when user has logged out', async () => {
+        renderSessionProvider();
+
+        userEvent.click(screen.getByText('Log in'));
+        await waitFor(async () => {
+            expect(screen.getByText('isLoggedIn - true')).toBeInTheDocument();
+        });
+        expect(
+            screen.getByText(`authCode - ${loggedIn.auth.authorisation_token}`),
+        ).toBeInTheDocument();
+
+        userEvent.click(screen.getByText('Log out'));
+        await waitFor(async () => {
+            expect(screen.getByText('isLoggedIn - false')).toBeInTheDocument();
+        });
+        expect(screen.getByText('authCode - undefined')).toBeInTheDocument();
+    });
+});
+
+const TestApp = () => {
+    const [session, setSession] = useSessionContext();
+
+    return (
+        <>
+            <div>
+                <h1>Actions</h1>
+                <div onClick={() => setSession(loggedIn)}>Log in</div>
+                <div onClick={() => setSession(loggedOut)}>Log out</div>
+            </div>
+            <div>
+                <h1>Details</h1>
+                <span>isLoggedIn - {`${session.isLoggedIn}`}</span>
+                <span>authCode - {`${session.auth?.authorisation_token}`}</span>
+            </div>
+        </>
+    );
+};
+
+const renderSessionProvider = () => {
+    render(
+        <SessionProvider>
+            <TestApp />
+        </SessionProvider>,
+    );
+};
