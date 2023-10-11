e42d9d0b88a466153cbef379b20f251ad3479b02
Richard Smith <richard.smith33@nhs.net>
2023-10-11 16:34:07 +0100

    Reduce duplicate code

--- lambdas/tests/unit/handlers/test_back_channel_logout_handler.py
+++ lambdas/tests/unit/handlers/test_back_channel_logout_handler.py
@@ -10,14 +10,13 @@ def test_back_channel_logout_handler_valid_jwt_returns_200_if_session_exists(moc
     mock_token = "mock_token"
     mock_session_id = "mock_session_id"
     mock_decoded_token = {"sid": mock_session_id}
-    monkeypatch.setenv("SSM_PARAM_CIS2_BCL_PUBLIC_KEY", "mock_public_key")
     mock_token_validator = mocker.patch("jwt.decode", return_value=mock_decoded_token)
+    mock_ssm_service = mocker.patch(
+        "handlers.back_channel_logout_handler.get_ssm_parameter", 
+        return_value=MOCK_SINGLE_SECURE_STRING_PARAMETER_RESPONSE,
+    )
     mock_dynamo_service = mocker.patch(
         "handlers.back_channel_logout_handler.remove_session_from_dynamo_db"
-    )
-    mock_ssm_service = mocker.patch(
-        "handlers.back_channel_logout_handler.get_ssm_parameter",
-        return_value=MOCK_SINGLE_SECURE_STRING_PARAMETER_RESPONSE,
     )
 
     expected = ApiGatewayResponse(200, "", "GET").create_api_gateway_response()
@@ -27,32 +26,11 @@ def test_back_channel_logout_handler_valid_jwt_returns_200_if_session_exists(moc
     assert expected == actual
     mock_token_validator.asset_called_with(mock_token)
     mock_dynamo_service.assert_called_with(mock_session_id)
-    mock_ssm_service.assert_called_once()
+    mock_ssm_service.assert_called_once_with("SSM_PARAM_CIS2_BCL_PUBLIC_KEY")
 
 
-def test_logout_handler_invalid_jwt_returns_200_if_session_doesnt_exist(mocker, monkeypatch):
+def test_back_channel_logout_handler_jwt_without_session_id_returns_400(mocker):
     mock_token = "mock_token"
-    monkeypatch.setenv("SSM_PARAM_CIS2_BCL_PUBLIC_KEY", "mock_public_key")
-    mock_token_validator = mocker.patch("jwt.decode", side_effect=PyJWTError())
-    mock_ssm_service = mocker.patch(
-        "handlers.back_channel_logout_handler.get_ssm_parameter",
-        return_value=MOCK_SINGLE_SECURE_STRING_PARAMETER_RESPONSE,
-    )
-
-    expected = ApiGatewayResponse(
-        400,  """{ "error":"Invalid x-auth header"}""", "GET"
-    ).create_api_gateway_response()
-
-    actual = lambda_handler(build_event_from_token(mock_token), None)
-
-    assert expected == actual
-    mock_token_validator.asset_called_with(mock_token)
-    mock_ssm_service.assert_called_once()
-
-
-def test_back_channel_logout_handler_jwt_without_session_id_returns_400(mocker, monkeypatch):
-    mock_token = "mock_token"
-    monkeypatch.setenv("SSM_PARAM_CIS2_BCL_PUBLIC_KEY", "mock_public_key")
     mock_token_validator = mocker.patch(
         "jwt.decode",
         return_value={"token_decode_correctly": "but_no_session_id_in_content"},
@@ -73,11 +51,10 @@ def test_back_channel_logout_handler_jwt_without_session_id_returns_400(mocker, 
     mock_ssm_service.assert_called_once()
 
 
-def test_back_channel_logout_handler_boto_error_returns_400(mocker, monkeypatch):
+def test_back_channel_logout_handler_boto_error_returns_400(mocker):
     mock_token = "mock_token"
     mock_session_id = "mock_session_id"
     mock_decoded_token = {"sid": mock_session_id}
-    monkeypatch.setenv("SSM_PARAM_CIS2_BCL_PUBLIC_KEY", "mock_public_key")
     mock_token_validator = mocker.patch("jwt.decode", return_value=mock_decoded_token)
     mock_dynamo_service = mocker.patch(
         "handlers.back_channel_logout_handler.remove_session_from_dynamo_db",
---
+++ lambdas/tests/unit/services/test_ssm_service.py
@@ -0,0 +1,106 @@
+from services.s3_service import S3Service
+from tests.unit.conftest import (MOCK_BUCKET, TEST_FILE_KEY, TEST_FILE_NAME,
+                                 TEST_OBJECT_KEY)
+
+MOCK_PRESIGNED_POST_RESPONSE = {
+    "url": "https://ndr-dev-document-store.s3.amazonaws.com/",
+    "fields": {
+        "key": "0abed67c-0d0b-4a11-a600-a2f19ee61281",
+        "x-amz-algorithm": "AWS4-HMAC-SHA256",
+        "x-amz-credential": "ASIAXYSUA44VTL5M5LWL/20230911/eu-west-2/s3/aws4_request",
+        "x-amz-date": "20230911T084756Z",
+        "x-amz-security-token": "test-security-token",
+        "policy": "test-policy",
+        "x-amz-signature": "b6afcf8b27fc883b0e0a25a789dd2ab272ea4c605a8c68267f73641d7471132f",
+    },
+}
+
+MOCK_PRESIGNED_URL_RESPONSE = {
+    "url": "https://ndr-dev-document-store.s3.amazonaws.com/",
+    "fields": {
+        "key": "0abed67c-0d0b-4a11-a600-a2f19ee61281",
+        "x-amz-algorithm": "AWS4-HMAC-SHA256",
+        "x-amz-credential": "ASIAXYSUA44VTL5M5LWL/20230911/eu-west-2/s3/aws4_request",
+        "x-amz-date": "20230911T084756Z",
+        "x-amz-expires": "1800",
+        "x-amz-signed-headers": "test-host",
+        "x-amz-signature": "test-signature",
+    },
+}
+
+TEST_DOWNLOAD_PATH = "test_path"
+MOCK_EVENT_BODY = {
+    "resourceType": "DocumentReference",
+    "subject": {"identifier": {"value": 111111000}},
+    "content": [{"attachment": {"contentType": "application/pdf"}}],
+    "description": "test_filename.pdf",
+}
+
+
+def test_create_document_presigned_url(set_env, mocker):
+    mock_generate_presigned_post = mocker.patch(
+        "botocore.signers.generate_presigned_post"
+    )
+
+    mock_generate_presigned_post.return_value = MOCK_PRESIGNED_POST_RESPONSE
+
+    service = S3Service()
+
+    return_value = service.create_document_presigned_url_handler(
+        MOCK_BUCKET, TEST_OBJECT_KEY
+    )
+
+    assert return_value == MOCK_PRESIGNED_POST_RESPONSE
+    mock_generate_presigned_post.assert_called_once()
+
+
+def test_create_zip_presigned_url(set_env, mocker):
+    mock_generate_presigned_url = mocker.patch(
+        "botocore.signers.generate_presigned_url"
+    )
+
+    mock_generate_presigned_url.return_value = MOCK_PRESIGNED_URL_RESPONSE
+
+    service = S3Service()
+
+    return_value = service.create_download_presigned_url(MOCK_BUCKET, TEST_FILE_KEY)
+
+    assert return_value == MOCK_PRESIGNED_URL_RESPONSE
+    mock_generate_presigned_url.assert_called_once()
+
+
+def test_download_file(mocker):
+    mocker.patch("boto3.client")
+    service = S3Service()
+    mock_download_file = mocker.patch.object(service.client, "download_file")
+    service.download_file(MOCK_BUCKET, TEST_FILE_KEY, TEST_DOWNLOAD_PATH)
+
+    mock_download_file.assert_called_once_with(
+        MOCK_BUCKET, TEST_FILE_KEY, TEST_DOWNLOAD_PATH
+    )
+
+
+def test_upload_file(mocker):
+    mocker.patch("boto3.client")
+    service = S3Service()
+    mock_upload_file = mocker.patch.object(service.client, "upload_file")
+
+    service.upload_file(TEST_FILE_NAME, MOCK_BUCKET, TEST_FILE_KEY)
+
+    mock_upload_file.assert_called_once_with(TEST_FILE_NAME, MOCK_BUCKET, TEST_FILE_KEY)
+
+
+def test_upload_file_with_extra_args(mocker):
+    mocker.patch("boto3.client")
+    service = S3Service()
+    mock_upload_file = mocker.patch.object(service.client, "upload_file")
+
+    test_extra_args = {"mock_tag": 123, "apple": "red", "banana": "true"}
+
+    service.upload_file_with_extra_args(
+        TEST_FILE_NAME, MOCK_BUCKET, TEST_FILE_KEY, test_extra_args
+    )
+
+    mock_upload_file.assert_called_once_with(
+        TEST_FILE_NAME, MOCK_BUCKET, TEST_FILE_KEY, test_extra_args
+    )
