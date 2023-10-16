7fac26015b272c5fcc45dcdb4a0049361319a6da
Joe Fong <joe.fong1@nhs.net>
2023-10-16 15:17:10 +0100

    run formatter

--- lambdas/tests/unit/services/test_bulk_upload_service.py
+++ lambdas/tests/unit/services/test_bulk_upload_service.py
@@ -2,21 +2,12 @@ import pytest
 from botocore.exceptions import ClientError
 from services.bulk_upload_service import BulkUploadService
 from services.lloyd_george_validator import LGInvalidFilesException
-from tests.unit.conftest import (
-    MOCK_LG_BUCKET,
-    MOCK_LG_STAGING_STORE_BUCKET,
-    MOCK_LG_TABLE_NAME,
-    TEST_OBJECT_KEY,
-)
+from tests.unit.conftest import (MOCK_LG_BUCKET, MOCK_LG_STAGING_STORE_BUCKET,
+                                 MOCK_LG_TABLE_NAME, TEST_OBJECT_KEY)
 from tests.unit.helpers.data.bulk_upload.test_data import (
-    TEST_DOCUMENT_REFERENCE,
-    TEST_DOCUMENT_REFERENCE_LIST,
-    TEST_FILE_METADATA,
-    TEST_NHS_NUMBER_FOR_BULK_UPLOAD,
-    TEST_SQS_MESSAGE,
-    TEST_STAGING_METADATA,
-    TEST_STAGING_METADATA_WITH_INVALID_FILENAME,
-)
+    TEST_DOCUMENT_REFERENCE, TEST_DOCUMENT_REFERENCE_LIST, TEST_FILE_METADATA,
+    TEST_NHS_NUMBER_FOR_BULK_UPLOAD, TEST_SQS_MESSAGE, TEST_STAGING_METADATA,
+    TEST_STAGING_METADATA_WITH_INVALID_FILENAME)
 from utils.exceptions import InvalidMessageException
 
 
