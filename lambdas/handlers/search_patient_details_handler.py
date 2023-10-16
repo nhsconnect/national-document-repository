379292e81c8c3f086217d5ab675151333471f80a
NogaNHS <noga.sasson1@nhs.net>
2023-10-16 14:26:33 +0100

    prmdr-327 refactor pds service

--- lambdas/handlers/search_patient_details_handler.py
+++ lambdas/handlers/search_patient_details_handler.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from json import JSONDecodeError
 
 from pydantic import ValidationError
@@ -8,8 +9,14 @@ from utils.exceptions import (InvalidResourceIdException,
                               PatientNotFoundException, PdsErrorException)
 from utils.lambda_response import ApiGatewayResponse
 
+from services.ssm_service import SSMService
+
+from services.mock_pds_service import MockPdsApiService
+
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+pds_service = PdsApiService if not bool(os.environ["PDS_FHIR_IS_STUBBED"]) else MockPdsApiService
 
 
 def lambda_handler(event, context):
@@ -20,23 +27,14 @@ def lambda_handler(event, context):
         nhs_number = event["queryStringParameters"]["patientId"]
 
         logger.info("Retrieving patient details")
-        pds_api_service = PdsApiService()
-
+        pds_api_service = pds_service(SSMService())
         patient_details = pds_api_service.fetch_patient_details(nhs_number)
         response = patient_details.model_dump_json(by_alias=True)
 
         return ApiGatewayResponse(200, response, "GET").create_api_gateway_response()
-    except HTTPError as e:
-        logger.error(e.response)
-        return ApiGatewayResponse(400, f"{str(e)}", "GET").create_api_gateway_response()
-    except PatientNotFoundException as e:
+
+    except (PatientNotFoundException, InvalidResourceIdException, PdsErrorException) as e:
         return ApiGatewayResponse(404, f"{str(e)}", "GET").create_api_gateway_response()
-
-    except InvalidResourceIdException as e:
-        return ApiGatewayResponse(400, f"{str(e)}", "GET").create_api_gateway_response()
-
-    except PdsErrorException as e:
-        return ApiGatewayResponse(400, f"{str(e)}", "GET").create_api_gateway_response()
 
     except ValidationError as e:
         return ApiGatewayResponse(
--- lambdas/services/pds_api_service.py
+++ lambdas/services/pds_api_service.py
@@ -1,14 +1,11 @@
-import json
 import logging
-import os
 import uuid
 from time import time
 
-import boto3
 import jwt
 import requests
 from models.pds_models import Patient, PatientDetails
-from requests.models import Response
+from requests.models import Response, HTTPError
 from utils.exceptions import (InvalidResourceIdException,
                               PatientNotFoundException, PdsErrorException)
 from utils.utilities import validate_id
@@ -17,18 +14,15 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 class PdsApiService:
-    def fetch_patient_details(self, nhs_number: str) -> PatientDetails:
-        validate_id(nhs_number)
-        if os.environ["PDS_FHIR_IS_STUBBED"] :
-            response = self.pds_request(nhs_number, retry_on_expired=True)
-        else:
-            response = self.fake_pds_request(nhs_number)
+    def __init__(self, ssm_service):
+        self.ssm_service = ssm_service
 
+    def fetch_patient_details(self, nhs_number: str,) -> PatientDetails:
+        validate_id(nhs_number)
+        response = self.pds_request(nhs_number, retry_on_expired=True)
         return self.handle_response(response, nhs_number)
 
     def handle_response(self, response: Response, nhs_number: str) -> PatientDetails:
-        logging.info(response.status_code)
-        logging.info(json.dumps(response.json()))
         if response.status_code == 200:
             patient = Patient.model_validate(response.json())
             patient_details = patient.get_patient_details(nhs_number)
@@ -45,10 +39,10 @@ class PdsApiService:
         raise PdsErrorException("Error when requesting patient from PDS")
 
     def pds_request(self, nshNumber: str, retry_on_expired: bool):
-        endpoint = self.get_ssm_parameter("/prs/dev/user-input/pds-fhir-endpoint")
-        access_token_response = self.get_ssm_parameter("/prs/dev-ndr/pds-fhir-access-token")
+        endpoint, access_token_response = self.get_parameters_for_pds_api_request()
         access_token = access_token_response['access_token']
-        if access_token_response['expires_at'] < time():
+        access_token_expiration = access_token_response['expires_at']
+        if access_token_expiration < time():
             access_token = self.get_new_access_token()
 
         x_request_id = str(uuid.uuid4())
@@ -63,76 +57,61 @@ class PdsApiService:
             return self.pds_request(nshNumber, retry_on_expired=False)
         return pds_response
 
-    def fake_pds_request(self, nhsNumber: str) -> Response:
-        mock_pds_results: list[dict] = []
-
-        try:
-            with open("services/mock_data/pds_patient.json") as f:
-                mock_pds_results.append(json.load(f))
-
-            with open("services/mock_data/pds_patient_restricted.json") as f:
-                mock_pds_results.append(json.load(f))
-
-        except FileNotFoundError:
-            raise PdsErrorException("Error when requesting patient from PDS")
-
-        pds_patient: dict = {}
-
-        for result in mock_pds_results:
-            for k, v in result.items():
-                if v == nhsNumber:
-                    pds_patient = result.copy()
-
-        response = Response()
-
-        if bool(pds_patient):
-            response.status_code = 200
-            response._content = pds_patient
-        else:
-            response.status_code = 404
-
-        return response
-
-    def get_ssm_parameter(self, parameter_key):
-        client = boto3.client("ssm", region_name="eu-west-2")
-        ssm_response = client.get_parameter(Name=parameter_key, WithDecryption=True)
-        return ssm_response["Parameter"]["Value"]
-
-    def encode_token(self, token_content, key, additional_headers ):
-        return jwt.encode(token_content, key, algorithm="RS512", headers=additional_headers)
 
     def get_new_access_token(self):
-        nhs_oauth_endpoint = self.get_ssm_parameter("/prs/dev/user-input/nhs-oauth-endpoint")
-        kid = self.get_ssm_parameter("/prs/dev/user-input/pds-fhir-kid")
-        nhs_key = self.get_ssm_parameter("/prs/dev/user-input/nhs-api-key")
-        pds_key = self.get_ssm_parameter("/prs/dev/user-input/pds-fhir-private-key")
+        try:
+            access_token_ssm_parameter = self.get_parameters_for_new_access_token()
+            jwt_token = self.create_jwt_token_for_new_access_token_request(access_token_ssm_parameter)
+            nhs_oauth_endpoint = access_token_ssm_parameter["/prs/dev/user-input/nhs-oauth-endpoint"]
+            nhs_oauth_response = self.request_new_access_token(jwt_token, nhs_oauth_endpoint)
+            nhs_oauth_response.raise_for_status()
+            token_access_response = nhs_oauth_response.json()
+            token_access_response['expires_at'] = self.change_token_expires_in_to_expires_at(token_access_response['expires_in'])
+            self.update_access_token_ssm(token_access_response)
+        except HTTPError as e:
+            logger.error(f"Issue while creating new access token: {e.response}")
+            raise PdsErrorException("Error accessing PDS API")
+        return token_access_response['access_token']
+
+    def get_parameters_for_new_access_token(self):
+        parameters = ["/prs/dev/user-input/nhs-oauth-endpoint", "/prs/dev/user-input/pds-fhir-kid", "/prs/dev/user-input/nhs-api-key", "/prs/dev/user-input/pds-fhir-private-key"]
+        return self.ssm_service.get_ssm_parameters(parameters, with_decryption=True)
+
+    def update_access_token_ssm(self, parameter_value: str):
+        parameter_key = "/prs/dev-ndr/pds-fhir-access-token"
+        self.ssm_service.update_ssm_parameter(parameter_key=parameter_key, parameter_value=parameter_value, parameter_type='SecureString')
+
+    def get_parameters_for_pds_api_request(self):
+        parameters = ["/prs/dev/user-input/pds-fhir-endpoint", "/prs/dev-ndr/pds-fhir-access-token"]
+        ssm_response = self.ssm_service.get_ssm_parameters(parameters, with_decryption=True)
+        return ssm_response[parameters[0]], ssm_response[parameters[1]]
+
+    def create_jwt_token_for_new_access_token_request(self, access_token_ssm_parameter):
+        nhs_oauth_endpoint = access_token_ssm_parameter["/prs/dev/user-input/nhs-oauth-endpoint"]
+        kid = access_token_ssm_parameter["/prs/dev/user-input/pds-fhir-kid"]
+        nhs_key = access_token_ssm_parameter["/prs/dev/user-input/nhs-api-key"]
+        pds_key = access_token_ssm_parameter["/prs/dev/user-input/pds-fhir-private-key"]
         payload = {
-        "iss": nhs_key,
-        "sub": nhs_key,
-        "aud": nhs_oauth_endpoint,
-        "jti": str(uuid.uuid4()),
-        "exp": int(time()) + 300
+            "iss": nhs_key,
+            "sub": nhs_key,
+            "aud": nhs_oauth_endpoint,
+            "jti": str(uuid.uuid4()),
+            "exp": int(time()) + 300
         }
-        token = self.encode_token(payload, key=pds_key, additional_headers={"kid": kid})
+        return jwt.encode(payload, pds_key, algorithm="RS512", headers={"kid": kid})
+
+    def request_new_access_token(self, jwt_token, nhs_oauth_endpoint):
         access_token_headers = {
             "content-type": "application/x-www-form-urlencoded"
         }
         access_token_data = {
             "grant_type" : "client_credentials",
             "client_assertion_type": "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
-            "client_assertion": token
+            "client_assertion": jwt_token
         }
-        response = requests.post(url=nhs_oauth_endpoint, headers=access_token_headers, data=access_token_data)
-        logging.error(response.content)
-        response.raise_for_status()
-        token_access_response = response.json()
-        safety_time_delta = 5
-        token_access_response['expires_at'] = token_access_response['expires_in'] + time() - safety_time_delta
-        self.update_access_token_ssm(token_access_response)
-        return token_access_response['access_token']
+        return requests.post(url=nhs_oauth_endpoint, headers=access_token_headers, data=access_token_data)
 
-    def update_access_token_ssm(self, parameter_value):
-        parameter_key = "/prs/dev-ndr/pds-fhir-access-token"
-        client = boto3.client("ssm", region_name="eu-west-2")
-        client.put_parameter(Name=parameter_key, Value=parameter_value, Type='SecureString', Overwrite=True)
+    def change_token_expires_in_to_expires_at(self, token_access_expires_in: int):
+        safety_time_delta = 5
+        return token_access_expires_in + time() - safety_time_delta
 
