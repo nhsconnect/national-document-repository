78f4a6672c80ec787ce664e03d1f62d6ffd1253b
NogaNHS <127490765+NogaNHS@users.noreply.github.com>
2023-10-18 12:47:52 +0100

    Prmdr 327 subtask - connect to PDS API (#95)
    SSM service + unit tests
    creating token access to pds services
    updating pds model
    add permission to access ssm in search lambda
    enable fake service for sandboxes
    API call to PDS

--- lambdas/tests/unit/handlers/test_search_patient_details_handler.py
+++ lambdas/tests/unit/handlers/test_search_patient_details_handler.py
@@ -1,15 +1,31 @@
+import os
+from unittest.mock import patch
+
+import pytest
 from handlers.search_patient_details_handler import lambda_handler
 from requests.models import Response
 from tests.unit.helpers.data.pds.pds_patient_response import PDS_PATIENT
 
 
-def test_lambda_handler_valid_id_returns_200(valid_id_event, context, mocker):
+@pytest.fixture
+def patch_env_vars():
+    env_vars = {
+        "PDS_FHIR_IS_STUBBED": "1",
+    }
+    with patch.dict(os.environ, env_vars):
+        yield env_vars
+
+
+def test_lambda_handler_valid_id_returns_200(
+    valid_id_event, context, mocker, patch_env_vars
+):
     response = Response()
     response.status_code = 200
     response._content = PDS_PATIENT
 
     mocker.patch(
-        "services.pds_api_service.PdsApiService.fake_pds_request", return_value=response
+        "services.mock_pds_service.MockPdsApiService.fake_pds_request",
+        return_value=response,
     )
 
     actual = lambda_handler(valid_id_event, context)
@@ -30,12 +46,15 @@ def test_lambda_handler_valid_id_returns_200(valid_id_event, context, mocker):
     assert expected == actual
 
 
-def test_lambda_handler_invalid_id_returns_400(invalid_id_event, context, mocker):
+def test_lambda_handler_invalid_id_returns_400(
+    invalid_id_event, context, mocker, patch_env_vars
+):
     response = Response()
     response.status_code = 400
 
     mocker.patch(
-        "services.pds_api_service.PdsApiService.fake_pds_request", return_value=response
+        "services.mock_pds_service.MockPdsApiService.fake_pds_request",
+        return_value=response,
     )
 
     actual = lambda_handler(invalid_id_event, context)
@@ -55,13 +74,14 @@ def test_lambda_handler_invalid_id_returns_400(invalid_id_event, context, mocker
 
 
 def test_lambda_handler_valid_id_not_in_pds_returns_404(
-    valid_id_event, context, mocker
+    valid_id_event, context, mocker, patch_env_vars
 ):
     response = Response()
     response.status_code = 404
 
     mocker.patch(
-        "services.pds_api_service.PdsApiService.fake_pds_request", return_value=response
+        "services.mock_pds_service.MockPdsApiService.fake_pds_request",
+        return_value=response,
     )
 
     actual = lambda_handler(valid_id_event, context)
@@ -81,13 +101,14 @@ def test_lambda_handler_valid_id_not_in_pds_returns_404(
 
 
 def test_lambda_handler_missing_id_in_query_params_returns_400(
-    missing_id_event, context, mocker
+    missing_id_event, context, mocker, patch_env_vars
 ):
     response = Response()
     response.status_code = 400
 
     mocker.patch(
-        "services.pds_api_service.PdsApiService.fake_pds_request", return_value=response
+        "services.mock_pds_service.MockPdsApiService.fake_pds_request",
+        return_value=response,
     )
 
     actual = lambda_handler(missing_id_event, context)
---
+++ lambdas/tests/unit/services/test_mock_pds_api_service.py
@@ -0,0 +1,47 @@
+import pytest
+from models.pds_models import PatientDetails
+from requests.models import Response
+from tests.unit.helpers.data.pds.pds_patient_response import PDS_PATIENT
+from utils.exceptions import (
+    InvalidResourceIdException,
+    PatientNotFoundException,
+    PdsErrorException,
+)
+
+from services.mock_pds_service import MockPdsApiService
+
+pds_service = MockPdsApiService()
+
+
+def test_fetch_patient_details_valid_returns_PatientDetails(mocker):
+    nhs_number = "9000000025"
+
+    response = Response()
+    response.status_code = 200
+    response._content = PDS_PATIENT
+
+    mocker.patch(
+        "services.mock_pds_service.MockPdsApiService.fake_pds_request",
+        return_value=response,
+    )
+
+    actual = pds_service.fetch_patient_details(nhs_number)
+
+    expected = PatientDetails(
+        givenName=["Jane"],
+        familyName="Smith",
+        birthDate="2010-10-22",
+        postalCode="LS1 6AE",
+        nhsNumber="9000000009",
+        superseded=False,
+        restricted=False,
+    )
+
+    assert actual == expected
+
+
+def test_fetch_patient_details_invalid_nhs_number_raises_InvalidResourceIdException():
+    nhs_number = "000000000"
+
+    with pytest.raises(InvalidResourceIdException):
+        pds_service.fetch_patient_details(nhs_number)
