ed14392616cb2e03e1c0b7d144de5a1ef7e1a5e0
abbas-khan10 <abbas.khan10@nhs.net>
2023-10-20 11:58:12 +0100

    update delete handler

--- lambdas/handlers/delete_document_reference_handler.py
+++ lambdas/handlers/delete_document_reference_handler.py
@@ -3,6 +3,7 @@ import os
 
 from botocore.exceptions import ClientError
 from enums.supported_document_types import SupportedDocumentTypes
+from models.document_reference import DocumentReference
 from services.document_service import DocumentService
 from utils.decorators.ensure_env_var import ensure_environment_variables
 from utils.decorators.validate_document_type import validate_document_type
@@ -25,30 +26,30 @@ def lambda_handler(event, context):
     nhs_number = event["queryStringParameters"]["patientId"]
     doc_type = event["queryStringParameters"]["docType"]
 
+    results:  list[DocumentReference] = []
+    table = ""
+
     document_service = DocumentService()
 
-    if SupportedDocumentTypes.ARF.name == doc_type:
-        logger.info("Retrieving ARF documents")
-        table = os.environ["DOCUMENT_STORE_DYNAMODB_NAME"]
-        results = document_service.fetch_documents_from_table(nhs_number, table)
-    elif SupportedDocumentTypes.LG.name == doc_type:
-        logger.info("Retrieving Lloyd George documents")
-        table = os.environ["LLOYD_GEORGE_DYNAMODB_NAME"]
-        results = document_service.fetch_documents_from_table(nhs_number, table)
-    else:
-        results = document_service.retrieve_all_document_references(
-            nhs_number, doc_type
-        )
-
-    if not results:
-        return ApiGatewayResponse(
-            404, "No documents available", "DELETE"
-        ).create_api_gateway_response()
-
-    for result in results:
-        print(result.file_location)
-
     try:
+        if SupportedDocumentTypes.ARF.name == doc_type:
+            logger.info("Retrieving ARF documents for deletion")
+            table = os.environ["DOCUMENT_STORE_DYNAMODB_NAME"]
+            results = document_service.fetch_documents_from_table(nhs_number, table)
+
+        if SupportedDocumentTypes.LG.name == doc_type:
+            logger.info("Retrieving Lloyd George documents for deletion")
+            table = os.environ["LLOYD_GEORGE_DYNAMODB_NAME"]
+            results = document_service.fetch_documents_from_table(nhs_number, table)
+
+        if not results:
+            return ApiGatewayResponse(
+                404, "No documents available", "DELETE"
+            ).create_api_gateway_response()
+
+        for result in results:
+            print(result.file_location)
+
         document_service.delete_documents(table, results)
     except ClientError as e:
         logger.info(str(e))
