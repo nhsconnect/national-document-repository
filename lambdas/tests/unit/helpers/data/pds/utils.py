5ff1535206a4490935dcb88461be1a510faa5809
NogaNHS <noga.sasson1@nhs.net>
2023-10-25 13:30:16 +0100

    prmdr-370 small change to tests

--- lambdas/tests/unit/helpers/data/pds/utils.py
+++ lambdas/tests/unit/helpers/data/pds/utils.py
@@ -1,11 +1,9 @@
 from models.pds_models import Patient
 
-from .pds_patient_response import PDS_PATIENT, PDS_PATIENT_RESTRICTED
+
+def create_unrestricted_patient(pds_response):
+    return Patient.model_validate(pds_response)
 
 
-def create_unrestricted_patient():
-    return Patient.model_validate(PDS_PATIENT)
-
-
-def create_restricted_patient():
-    return Patient.model_validate(PDS_PATIENT_RESTRICTED)
+def create_restricted_patient(pds_response):
+    return Patient.model_validate(pds_response)
