1400568e9711044a26fd35deb99efc940d843d13
NogaNHS <noga.sasson1@nhs.net>
2023-10-25 10:26:28 +0100

    write to csv

--- lambdas/handlers/bulk_upload_report_handler.py
+++ lambdas/handlers/bulk_upload_report_handler.py
@@ -1,3 +1,4 @@
+import csv
 import os
 import boto3
 from services.dynamo_service import DynamoDBService
@@ -7,8 +8,8 @@ s3_resource = boto3.resource('s3')
 def lambda_handler(event, context):
     db_service = DynamoDBService()
     staging_bucket_name = os.getenv("STAGING_STORE_BUCKET_NAME")
-
-    # Upload temp file to S3
+    report_data = get_dynamo_data(db_service)
+    write_items_to_csv(report_data)
     s3_resource.Bucket(staging_bucket_name).upload_file(TEMP_FILENAME, OUTPUT_KEY)
 
 def get_dynamo_data(db_service):
@@ -22,9 +23,10 @@ def get_dynamo_data(db_service):
         items.append(db_response['Items'])
     return items
 
-# def write_items_to_csv(item: list):
-#    with open(TEMP_FILENAME, 'w') as output_file:
-
-#     writer = csv.writer(output_file)
-#     writer.writerow(item.keys())
-# writer.writerow(item.values())
+def write_items_to_csv(items: list, csv_file_path: str):
+   with open(csv_file_path, 'a') as output_file:
+        field_names = items[0].keys()
+        dictwriter_object = csv.DictWriter(output_file, fieldnames=field_names)
+        for item in items:
+            dictwriter_object.writerow(item)
+        output_file.close()
