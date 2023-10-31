b5789353ed9f88502ed1f00510e8e57b33493a75
Scott Alexander <scott.alexander@madetech.com>
2023-10-31 16:56:24 +0000

    added addtional logging for token handler params

--- lambdas/services/token_handler_ssm_service.py
+++ lambdas/services/token_handler_ssm_service.py
@@ -13,23 +13,32 @@ class TokenHandlerSSMService(SSMService):
 
     def get_smartcard_role_codes(self):
         logger.info("starting ssm request to retrieve required smartcard role codes")
-        return self.get_ssm_parameters([
+        params = self.get_ssm_parameters([
             "/auth/smartcard/role/gp_admin",
             "/auth/smartcard/role/gp_clinical",
             "/auth/smartcard/role/pcse",
         ])
+
+        logger.info(f"smartcard role params: {params}")
+        return params
     
     def get_org_role_codes(self):
-        logger.info("starting ssm request to retrieve required smartcard role codes")
-        return self.get_ssm_parameters([
+        logger.info("starting ssm request to retrieve required org roles codes")
+        params = self.get_ssm_parameters([
             "/auth/org/role_code/gpp",
         ])
 
+        logger.info(f"role code params: {params}")
+        return params
+
     def get_org_ods_codes(self):
-        logger.info("starting ssm request to retrieve required smartcard role codes")
-        return self.get_ssm_parameters([
+        logger.info("starting ssm request to retrieve required org ods codes")
+        params = self.get_ssm_parameters([
             "/auth/org/ods_code/pcse",
         ])
+
+        logger.info(f"org ods params: {params}")
+        return params
 
     def get_jwt_private_key(self):
         logger.info("starting ssm request to retrieve NDR private key")
