4a24aa266dc7bc1eb04cc4da08b6039e1a44fcff
Richard Smith <richard.smith33@nhs.net>
2023-11-03 16:57:17 +0000

    unit tests for fetch org data in ods api service

--- lambdas/tests/unit/services/test_ods_api_service.py
+++ lambdas/tests/unit/services/test_ods_api_service.py
@@ -1,21 +1,24 @@
 import pytest
-from enums.permitted_role import PermittedRole
 from services.ods_api_service import OdsApiService
 from tests.unit.helpers.data.ods.utils import load_ods_response_data
 from tests.unit.helpers.mock_response import MockResponse
 from utils.exceptions import OdsErrorException, OrganisationNotFoundException
 
 
-def skip_test_fetch_organisation_data_valid_returns_organisation_data():
+def test_fetch_organisation_data_valid_returns_organisation_data(mocker):
     test_ods_code = "X26"
+    ord_api_request_url = f"https://directory.spineservices.nhs.uk/ORD/2-0-0/organisations/{test_ods_code}"
+    expected = """{"successfulJSONResponse"}"""
+    response_200 = MockResponse(200, {expected})
+    mock_api = mocker.patch("requests.get", return_value=response_200)
 
     actual = OdsApiService.fetch_organisation_data(test_ods_code)
 
-    assert "NHS ENGLAND - X26" in str(actual)
-    assert "PrimaryRoleId" in str(actual)
+    assert actual == expected
+    assert mock_api.assert_called_with(ord_api_request_url)
 
 
-def skip_test_fetch_organisation_data_404_raise_OrganisationNotFoundException(mocker):
+def test_fetch_organisation_data_404_raise_OrganisationNotFoundException(mocker):
     response_404 = MockResponse(404, {"errorCode": 404, "errorText": "Not found"})
 
     mocker.patch("requests.get", return_value=response_404)
@@ -23,61 +26,60 @@ def skip_test_fetch_organisation_data_404_raise_OrganisationNotFoundException(mo
         OdsApiService.fetch_organisation_data("non-exist-ods-code")
 
 
-def skip_test_fetch_organisation_data_catch_all_raises_OdsErrorException(mocker):
+def test_fetch_organisation_data_catch_all_raises_OdsErrorException(mocker):
     response_400 = MockResponse(400, "BadRequest")
 
     mocker.patch("requests.get", return_value=response_400)
-    invalid_ods_code = "!@?£?$?@?"
 
     with pytest.raises(OdsErrorException):
-        OdsApiService.fetch_organisation_data(invalid_ods_code)
+        OdsApiService.fetch_organisation_data("bad-ods-code")
 
 
-@pytest.fixture()
-def mock_ods_responses():
-    # load test data from several json files and pass to below tests in a dict
-    yield load_ods_response_data()
+# @pytest.fixture()
+# def mock_ods_responses():
+#     # load test data from several json files and pass to below tests in a dict
+#     yield load_ods_response_data()
 
 
-def skip_test_parse_ods_response_extract_organisation_with_permitted_gp_role(
-    mock_ods_responses,
-):
-    test_response = mock_ods_responses["with_valid_gp_role"]
+# def skip_test_parse_ods_response_extract_organisation_with_permitted_gp_role(
+#     mock_ods_responses,
+# ):
+#     test_response = mock_ods_responses["with_valid_gp_role"]
 
-    actual = OdsApiService.parse_ods_response(test_response)
-    expect = ("PORTWAY LIFESTYLE CENTRE", "A9A5A", PermittedRole.GP.name)
+#     actual = OdsApiService.parse_ods_response(test_response)
+#     expect = ("PORTWAY LIFESTYLE CENTRE", "A9A5A", PermittedRole.GP.name)
 
-    assert actual == expect
-
-
-def skip_test_parse_ods_response_extract_organisation_with_permitted_PCSE_role(
-    mock_ods_responses,
-):
-    test_response = mock_ods_responses["with_valid_pcse_role"]
-
-    actual = OdsApiService.parse_ods_response(test_response)
-    expect = ("Primary Care Support England", "B9A5A", PermittedRole.PCSE.name)
-
-    assert actual == expect
+#     assert actual == expect
 
 
-def skip_test_parse_ods_response_return_the_first_valid_role_if_more_than_one_exists(
-    mock_ods_responses,
-):
-    test_response = mock_ods_responses["with_multiple_valid_roles"]
+# def skip_test_parse_ods_response_extract_organisation_with_permitted_PCSE_role(
+#     mock_ods_responses,
+# ):
+#     test_response = mock_ods_responses["with_valid_pcse_role"]
 
-    actual = OdsApiService.parse_ods_response(test_response)
-    expect = ("PORTWAY LIFESTYLE CENTRE", "A9A5A", PermittedRole.GP.name)
+#     actual = OdsApiService.parse_ods_response(test_response)
+#     expect = ("Primary Care Support England", "B9A5A", PermittedRole.PCSE.name)
 
-    assert actual == expect
+#     assert actual == expect
 
 
-def skip_test_parse_ods_response_should_return_none_if_no_valid_role_was_found(
-    mock_ods_responses,
-):
-    test_response = mock_ods_responses["with_no_valid_roles"]
+# def skip_test_parse_ods_response_return_the_first_valid_role_if_more_than_one_exists(
+#     mock_ods_responses,
+# ):
+#     test_response = mock_ods_responses["with_multiple_valid_roles"]
 
-    actual = OdsApiService.parse_ods_response(test_response)
-    expect = None
+#     actual = OdsApiService.parse_ods_response(test_response)
+#     expect = ("PORTWAY LIFESTYLE CENTRE", "A9A5A", PermittedRole.GP.name)
 
-    assert actual == expect
+#     assert actual == expect
+
+
+# def skip_test_parse_ods_response_should_return_none_if_no_valid_role_was_found(
+#     mock_ods_responses,
+# ):
+#     test_response = mock_ods_responses["with_no_valid_roles"]
+
+#     actual = OdsApiService.parse_ods_response(test_response)
+#     expect = None
+
+#     assert actual == expect
