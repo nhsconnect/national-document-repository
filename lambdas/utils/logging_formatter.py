2e19091ec742d5ae27c2bb22d21968b2f5ed554d
scott alexander <scott.r.alexander@me.com>
2023-11-07 11:00:26 +0000

    removed splunk logging due to correlation id bug (#137)
    * Added Splunk logging required decorator to Token handler
    * Added additional None checks to potential breaking areas
    
    ---------
    
    Co-authored-by: Scott Alexander <scott.alexander@madetech.com>

--- lambdas/utils/logging_formatter.py
+++ lambdas/utils/logging_formatter.py
@@ -6,11 +6,19 @@ from utils.request_context import request_context
 
 class LoggingFormatter(logging.Formatter):
     def format(self, record: logging.LogRecord) -> str:
+
+        auth = "No Auth"
+        if request_context.authorization is not None:
+            auth = request_context.authorization
+
         s = super().format(record)
         d = {
             "correlation_id": request_context.request_id,
-            "auth": request_context.authorization,
-            **record.__dict__.get("custom_args", {}),
-            "Message": s,
+            "auth": auth,
+            "Message": s
         }
+
+        if record.__dict__.get("custom_args", {}) is not None: 
+            d.update(record.__dict__.get("custom_args", {}))
+
         return json.dumps(d)
