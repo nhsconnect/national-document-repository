b8a0844e201f2e5ce01026b37b26fb198cb21469
Scott Alexander <scott.alexander@madetech.com>
2023-11-07 10:26:20 +0000

    test auth request_context process

--- lambdas/utils/logging_formatter.py
+++ lambdas/utils/logging_formatter.py
@@ -6,10 +6,15 @@ from utils.request_context import request_context
 
 class LoggingFormatter(logging.Formatter):
     def format(self, record: logging.LogRecord) -> str:
+
+        auth = "No Auth"
+        if request_context.authorization:
+            auth = request_context.authorization
+            
         s = super().format(record)
         d = {
             "correlation_id": request_context.request_id,
-            "auth": request_context.authorization,
+            "auth": auth
             **record.__dict__.get("custom_args", {}),
             "Message": s,
         }
