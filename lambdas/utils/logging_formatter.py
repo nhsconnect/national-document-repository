6fd4bf37abf5f498c24082f0977cf8d8bc11fcea
Scott Alexander <scott.alexander@madetech.com>
2023-11-07 10:39:48 +0000

    fixed new None check

--- lambdas/utils/logging_formatter.py
+++ lambdas/utils/logging_formatter.py
@@ -8,7 +8,7 @@ class LoggingFormatter(logging.Formatter):
     def format(self, record: logging.LogRecord) -> str:
 
         auth = "No Auth"
-        if request_context.authorization is None:
+        if request_context.authorization is not None:
             auth = request_context.authorization
 
         logging.info(f"auth: {auth}")
