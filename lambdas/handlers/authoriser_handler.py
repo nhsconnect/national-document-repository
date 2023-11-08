e39caf0d9728d87d79216c66a3886d709905fba1
Scott Alexander <scott.alexander@madetech.com>
2023-11-08 12:06:39 +0000

    removed additional logging

--- lambdas/handlers/authoriser_handler.py
+++ lambdas/handlers/authoriser_handler.py
@@ -83,7 +83,6 @@ def validate_access_policy(http_verb, path, user_role):
     logger.info(f"path: {path}")
     match path:
         case "/DocumentDelete":
-            logger.info("doc delete here")
             deny_resource = (
                 user_role == RepositoryRole.GP_CLINICAL.value
             )
@@ -105,7 +104,6 @@ def validate_access_policy(http_verb, path, user_role):
         case _:
             deny_resource = False
 
-    logger.info(f"deny_resource: {deny_resource}")
     logger.info("Allow resource: %s" % bool(deny_resource) is False)
 
     return bool(deny_resource)
