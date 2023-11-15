18046aabd83f3b7f11db27a5b975a85642bde114
AlexHerbertNHS <alexandra.herbert1@nhs.net>
2023-11-15 12:42:06 +0000

    [PRMDR-436]
    Adding logging, testing to see why logout is broke

--- lambdas/handlers/logout_handler.py
+++ lambdas/handlers/logout_handler.py
@@ -19,8 +19,11 @@ def lambda_handler(event, context):
     request_context.app_interaction = LoggingAppInteraction.LOGOUT.value
     token = None
     if event.get("headers"):
+        headers = event.get("headers")
+        logger.info(f"headers = {headers}")
         token = event.get("headers").get("x-auth")
-    return logout_handler(token)
+        byte_token = str.encode(token)
+    return logout_handler(byte_token)
 
 
 def logout_handler(token):
@@ -29,7 +32,7 @@ def logout_handler(token):
         ssm_response = get_ssm_parameter(key=ssm_public_key_parameter_name)
         jwt_class = jwt
         public_key = ssm_response["Parameter"]["Value"]
-        logger.info("decoding token")
+        logger.info(f"decoding token: {token}")
         decoded_token = decode_token(jwt_class=jwt_class, token=token, key=public_key)
         session_id = decoded_token["ndr_session_id"]
         remove_session_from_dynamo_db(session_id)
