cc7fcd5e6af27c899c9445cd683c226bf474d8fb
Scott Alexander <scott.alexander@madetech.com>
2023-11-17 12:06:07 +0000

    fixed import error

---
+++ lambdas/utils/decorators/override_error_check.py
@@ -0,0 +1,39 @@
+from typing import Callable
+
+import os
+from utils.error_testing_utils import error_testing_utils
+from utils.decorators.ensure_env_var import ensure_environment_variables
+
+def override_error_check(lambda_func: Callable):
+    """A decorator for lambda handler.
+    Interceptor to identiy if any manual error throwing has been enabled for testing purposes.
+    This should always return to the initial handler call if the conditions are not meet to trigger
+    a manual error
+
+    Usage:
+    @validate_patient_id
+    def lambda_handler(event, context):
+        ...
+    """
+
+    disabled_workspaces = ["pre-prod", "prod"]
+
+    def interceptor(event, context):
+        try:
+            workspace = os.environ["WORKSPACE"]
+            error_override = os.environ["ERROR_TRIGGER"]
+
+            # fail fast if workspace is invalid 
+            if workspace == None or workspace in disabled_workspaces:
+                return lambda_func(event, context)
+            
+            # fail fast if error trigger is not set
+            if error_override is None or error_override == "":
+                return lambda_func(event, context)
+                
+            error_testing_utils.check_manual_error_conditions(error_override)
+
+        except :
+            return lambda_func(event, context)
+
+    return interceptor
---
+++ lambdas/utils/error_testing_utils.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+def check_manual_error_conditions(type: str, env: str):
+    if type == "TIMEOUT":
+        trigger_timeout_error()
+    if type == "MEMORY":
+        trigger_memory_error
+
+
+def trigger_memory_error(): 
+    memoryBreakString = 'string_length_to_double_each_iteration'
+    for x in range(1000000000000000000000000000000000):
+        memoryBreakString = memoryBreakString
+
+def trigger_timeout_error():
+    memoryBreakString = 'string_to_set_each_iteration'
+    for x in range(1000000000000000000000000000000000):
+        memoryBreakString = memoryBreakString
