1d2380b61118e1cb5d31088a6f6e0cbef26852d5
Richard Smith <richard.smith33@nhs.net>
2023-11-17 16:14:24 +0000

    Back button tests

--- app/src/components/generic/backButton/BackButton.test.tsx
+++ app/src/components/generic/backButton/BackButton.test.tsx
@@ -3,6 +3,8 @@ import { render, screen, waitFor } from '@testing-library/react';
 import * as ReactRouter from 'react-router';
 import userEvent from '@testing-library/user-event';
 import BackButton from './BackButton';
+import { routes } from '../../../types/generic/routes';
+import { endpoints } from '../../../types/generic/endpoints';
 
 describe('BackButton', () => {
     it('navigates to previous page when clicking the back buttonand not on the search pages', async () => {
@@ -24,13 +26,25 @@ describe('BackButton', () => {
         });
     });
 
-    it.skip('navigates calls the login handler when clicking the back button on a patient search page', async () => {
+    it('calls the login handler when clicking the back button on the upload search page', async () => {
+        const test_location_prefix = 'http://test';
+
         const history = createMemoryHistory({
-            initialEntries: [],
+            initialEntries: ['/', '/example'],
+            initialIndex: 1,
         });
 
+        //Override the default window.location property as it is not configurable so can't be mocked by jest
+        Object.defineProperty(window, 'location', {
+            configurable: true,
+            enumerable: true,
+            value: new URL(window.location.href),
+        });
+
+        window.location.replace = jest.fn();
+
         render(
-            <ReactRouter.Router navigator={history} location={'exampleBaseUrl/search'}>
+            <ReactRouter.Router navigator={history} location={routes.UPLOAD_SEARCH}>
                 <BackButton />
             </ReactRouter.Router>,
         );
@@ -38,7 +52,37 @@ describe('BackButton', () => {
         userEvent.click(screen.getByText('Back'));
 
         await waitFor(() => {
-            expect(history.location.pathname).toBe('/');
+            expect(window.location.replace).toBeCalledWith(test_location_prefix + endpoints.LOGIN);
+        });
+    });
+
+    it('calls the login handler when clicking the back button on the download search page', async () => {
+        const test_location_prefix = 'http://test';
+
+        const history = createMemoryHistory({
+            initialEntries: ['/', '/example'],
+            initialIndex: 1,
+        });
+
+        //Override the default window.location property as it is not configurable so can't be mocked by jest
+        Object.defineProperty(window, 'location', {
+            configurable: true,
+            enumerable: true,
+            value: new URL(window.location.href),
+        });
+
+        window.location.replace = jest.fn();
+
+        render(
+            <ReactRouter.Router navigator={history} location={routes.DOWNLOAD_SEARCH}>
+                <BackButton />
+            </ReactRouter.Router>,
+        );
+
+        userEvent.click(screen.getByText('Back'));
+
+        await waitFor(() => {
+            expect(window.location.replace).toBeCalledWith(test_location_prefix + endpoints.LOGIN);
         });
     });
 });
