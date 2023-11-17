314c21756a8071bce3a8140e829c9e2f685ca561
Scott Alexander <scott.alexander@madetech.com>
2023-11-17 12:51:01 +0000

    fixed memory error trigger condition

--- lambdas/utils/error_testing_utils.py
+++ lambdas/utils/error_testing_utils.py
@@ -11,7 +11,7 @@ def check_manual_error_conditions(type: str):
 def trigger_memory_error(): 
     memoryBreakString = 'string_length_to_double_each_iteration'
     for x in range(1000000000000000000000000000000000):
-        memoryBreakString = memoryBreakString
+        memoryBreakString = memoryBreakString + memoryBreakString
 
 def trigger_timeout_error():
     memoryBreakString = 'string_to_set_each_iteration'
