8e4c29bbe71f2da2c296d8af3b8e532a86f357f3
Richard Smith <richard.smith33@nhs.net>
2023-11-17 11:42:20 +0000

    Get locations from routes

--- app/src/components/generic/backButton/BackButton.tsx
+++ app/src/components/generic/backButton/BackButton.tsx
@@ -4,6 +4,7 @@ import type { MouseEvent } from 'react';
 import { useNavigate, useLocation } from 'react-router';
 import { endpoints } from '../../../types/generic/endpoints';
 import { useBaseAPIUrl } from '../../../providers/configProvider/ConfigProvider';
+import { routes } from '../../../types/generic/routes';
 
 const BackButton = () => {
     const navigate = useNavigate();
@@ -12,25 +13,15 @@ const BackButton = () => {
 
     const onBack = (e: MouseEvent<HTMLAnchorElement>) => {
         e.preventDefault();
-        if (location.pathname === '/search/upload' || location.pathname === '/search/patient') {
+        if (
+            location.pathname === routes.UPLOAD_SEARCH ||
+            location.pathname === routes.DOWNLOAD_SEARCH
+        ) {
             window.location.replace(`${baseAPIUrl}${endpoints.LOGIN}`);
         } else {
             navigate(-1);
         }
     };
-
-    // const onSearchPage = () => {
-    //     (location.pathname.includes('/search/upload') ||
-    //         location.pathname.includes('/search/patient')) &&
-    //         !(
-    //             location.pathname.includes('/result') ||
-    //             location.pathname.includes('/results') ||
-    //             location.pathname.includes('/delete') ||
-    //             location.pathname.includes('/upload') ||
-    //             location.pathname.includes('/submit') ||
-    //             location.pathname.includes('/lloyd-george-record')
-    //         );
-    // };
 
     return (
         <BackLink className="clickable" onClick={onBack}>
