eada0657b12257cd6ec4dd4327e3b61e999cc340
Rio Knightley <rio.knightley2@nhs.net>
2023-11-21 11:09:33 +0000

    Remove configprovider import

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import './styles/App.scss';
-import ConfigProvider from './providers/configProvider/ConfigProvider';
-import config from './config';
 import PatientDetailsProvider from './providers/patientProvider/PatientProvider';
 import SessionProvider from './providers/sessionProvider/SessionProvider';
 import AppRouter from './router/AppRouter';
 
 function App() {
     return (
-        <ConfigProvider config={config}>
-            <SessionProvider>
-                <PatientDetailsProvider>
-                    <AppRouter />
-                </PatientDetailsProvider>
-            </SessionProvider>
-        </ConfigProvider>
+        <SessionProvider>
+            <PatientDetailsProvider>
+                <AppRouter />
+            </PatientDetailsProvider>
+        </SessionProvider>
     );
 }
 
