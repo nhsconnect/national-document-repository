7e67c73e1b75cbef2906665bfc92574de8f2d568
Rio Knightley <rio.knightley2@nhs.net>
2023-11-22 09:37:54 +0000

    Remove unused providers from uploading stage tests

--- app/src/components/blocks/uploadingStage/UploadingStage.test.tsx
+++ app/src/components/blocks/uploadingStage/UploadingStage.test.tsx
@@ -6,26 +6,15 @@ import {
     DOCUMENT_UPLOAD_STATE as documentUploadStates,
     UploadDocument,
 } from '../../../types/pages/UploadDocumentsPage/types';
-import {
-    buildPatientDetails,
-    buildTextFile,
-    buildUserAuth,
-} from '../../../helpers/test/testBuilders';
+import { buildTextFile } from '../../../helpers/test/testBuilders';
 import UploadingStage from './UploadingStage';
-import { createMemoryHistory } from 'history';
-import SessionProvider, { Session } from '../../../providers/sessionProvider/SessionProvider';
-import { PatientDetails } from '../../../types/generic/patientDetails';
-import PatientDetailsProvider from '../../../providers/patientProvider/PatientProvider';
-
-jest.mock('react-router');
-const mockPatient = buildPatientDetails();
 
 describe('<UploadDocumentsPage />', () => {
     describe('with NHS number', () => {
         const triggerUploadStateChange = (
             document: UploadDocument,
             state: DOCUMENT_UPLOAD_STATE,
-            progress: number
+            progress: number,
         ) => {
             act(() => {
                 document.state = state;
@@ -55,16 +44,15 @@ describe('<UploadDocumentsPage />', () => {
                 progress: 0,
                 docType: DOCUMENT_TYPE.ARF,
             };
-
-            renderUploadingStage([documentOne, documentTwo, documentThree]);
+            render(<UploadingStage documents={[documentOne, documentTwo, documentThree]} />);
 
             triggerUploadStateChange(documentOne, documentUploadStates.UPLOADING, 0);
 
             expect(screen.queryByTestId('upload-document-form')).not.toBeInTheDocument();
             expect(
                 screen.getByText(
-                    'Do not close or navigate away from this browser until upload is complete.'
-                )
+                    'Do not close or navigate away from this browser until upload is complete.',
+                ),
             ).toBeInTheDocument();
         });
 
@@ -92,10 +80,7 @@ describe('<UploadDocumentsPage />', () => {
             };
 
             const { rerender } = render(
-                <UploadingStage
-                    patientDetails={mockPatient}
-                    documents={[documentOne, documentTwo, documentThree]}
-                />
+                <UploadingStage documents={[documentOne, documentTwo, documentThree]} />,
             );
             const getProgressBarValue = (document: UploadDocument) => {
                 const progressBar: HTMLProgressElement = screen.getByRole('progressbar', {
@@ -110,80 +95,29 @@ describe('<UploadDocumentsPage />', () => {
             };
 
             triggerUploadStateChange(documentOne, documentUploadStates.UPLOADING, 10);
-            rerender(
-                <UploadingStage
-                    patientDetails={mockPatient}
-                    documents={[documentOne, documentTwo, documentThree]}
-                />
-            );
+            rerender(<UploadingStage documents={[documentOne, documentTwo, documentThree]} />);
             expect(getProgressBarValue(documentOne)).toEqual(10);
             expect(getProgressText(documentOne)).toContain('10% uploaded...');
 
             triggerUploadStateChange(documentOne, documentUploadStates.UPLOADING, 70);
-            rerender(
-                <UploadingStage
-                    patientDetails={mockPatient}
-                    documents={[documentOne, documentTwo, documentThree]}
-                />
-            );
+            rerender(<UploadingStage documents={[documentOne, documentTwo, documentThree]} />);
             expect(getProgressBarValue(documentOne)).toEqual(70);
             expect(getProgressText(documentOne)).toContain('70% uploaded...');
 
             triggerUploadStateChange(documentTwo, documentUploadStates.UPLOADING, 20);
-            rerender(
-                <UploadingStage
-                    patientDetails={mockPatient}
-                    documents={[documentOne, documentTwo, documentThree]}
-                />
-            );
+            rerender(<UploadingStage documents={[documentOne, documentTwo, documentThree]} />);
             expect(getProgressBarValue(documentTwo)).toEqual(20);
             expect(getProgressText(documentTwo)).toContain('20% uploaded...');
 
             triggerUploadStateChange(documentTwo, documentUploadStates.SUCCEEDED, 100);
-            rerender(
-                <UploadingStage
-                    patientDetails={mockPatient}
-                    documents={[documentOne, documentTwo, documentThree]}
-                />
-            );
+            rerender(<UploadingStage documents={[documentOne, documentTwo, documentThree]} />);
             expect(getProgressBarValue(documentTwo)).toEqual(100);
             expect(getProgressText(documentTwo)).toContain('Uploaded');
 
             triggerUploadStateChange(documentOne, documentUploadStates.FAILED, 0);
-            rerender(
-                <UploadingStage
-                    patientDetails={mockPatient}
-                    documents={[documentOne, documentTwo, documentThree]}
-                />
-            );
+            rerender(<UploadingStage documents={[documentOne, documentTwo, documentThree]} />);
             expect(getProgressBarValue(documentOne)).toEqual(0);
             expect(getProgressText(documentOne)).toContain('Upload failed');
         });
     });
 });
-
-const homeRoute = '/example';
-const renderUploadingStage = (
-    documents: Array<UploadDocument>,
-    patientOverride: Partial<PatientDetails> = {},
-    history = createMemoryHistory({
-        initialEntries: [homeRoute],
-        initialIndex: 1,
-    })
-) => {
-    const auth: Session = {
-        auth: buildUserAuth(),
-        isLoggedIn: true,
-    };
-    const patient: PatientDetails = {
-        ...buildPatientDetails(),
-        ...patientOverride,
-    };
-    render(
-        <SessionProvider sessionOverride={auth}>
-            <PatientDetailsProvider patientDetails={patient}>
-                <UploadingStage patientDetails={mockPatient} documents={documents} />
-            </PatientDetailsProvider>
-        </SessionProvider>
-    );
-};
