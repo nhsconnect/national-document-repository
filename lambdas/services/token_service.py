6162492cc94c5dcf39c9c0ce938ab7b6cfdc119c
RachelHowellNHS <rachel.howell6@nhs.net>
2023-11-24 13:03:53 +0000

    [PRMDR-469] format

--- lambdas/services/token_service.py
+++ lambdas/services/token_service.py
@@ -1,6 +1,5 @@
 import jwt
 from botocore.exceptions import ClientError
-
 from services.ssm_service import SSMService
 
 ssm_service = SSMService()
@@ -8,13 +7,9 @@ ssm_service = SSMService()
 
 class TokenService:
     @staticmethod
-    def get_public_key_and_decode_auth_token(
-            auth_token, ssm_public_key_parameter
-    ):
+    def get_public_key_and_decode_auth_token(auth_token, ssm_public_key_parameter):
         try:
-            public_key = ssm_service.get_ssm_parameter(
-                ssm_public_key_parameter, True
-            )
+            public_key = ssm_service.get_ssm_parameter(ssm_public_key_parameter, True)
             decoded_token = jwt.decode(auth_token, public_key, algorithms=["RS256"])
             return decoded_token
         except (jwt.PyJWTError, ClientError):
--- lambdas/tests/unit/services/test_token_service.py
+++ lambdas/tests/unit/services/test_token_service.py
@@ -3,10 +3,16 @@ from services.token_service import TokenService
 
 def test_get_public_key_and_decode_auth_token(mocker):
     mock_jwt_decode = mocker.patch("jwt.decode", return_value="decoded token")
-    mocker.patch("services.ssm_service.SSMService.get_ssm_parameter", return_value="test_value_param_key")
+    mocker.patch(
+        "services.ssm_service.SSMService.get_ssm_parameter",
+        return_value="test_value_param_key",
+    )
     token_service = TokenService()
-    response = token_service.get_public_key_and_decode_auth_token(auth_token="token_test",
-                                                                  ssm_public_key_parameter="param_key")
+    response = token_service.get_public_key_and_decode_auth_token(
+        auth_token="token_test", ssm_public_key_parameter="param_key"
+    )
 
     assert response == "decoded token"
-    mock_jwt_decode.assert_called_with("token_test", "test_value_param_key", algorithms=['RS256'])
+    mock_jwt_decode.assert_called_with(
+        "token_test", "test_value_param_key", algorithms=["RS256"]
+    )
