9310cf07314156f31f11f303fa3dc8ebb357b780
AlexHerbertNHS <alexandra.herbert1@nhs.net>
2023-11-24 17:19:55 +0000

    [PRMDR-529]
    Missed the key change I intended to make due to Git weirdness

--- lambdas/services/bulk_upload_service.py
+++ lambdas/services/bulk_upload_service.py
@@ -208,10 +208,10 @@ class BulkUploadService:
             )
             source_file_key = self.strip_leading_slash(file_metadata.file_path)
             dest_file_key = document_reference.s3_file_key
-            self.create_record_in_lg_dynamo_table(document_reference)
             self.copy_to_lg_bucket(
                 source_file_key=source_file_key, dest_file_key=dest_file_key
             )
+            self.create_record_in_lg_dynamo_table(document_reference)
 
     def create_record_in_lg_dynamo_table(
         self, document_reference: NHSDocumentReference
