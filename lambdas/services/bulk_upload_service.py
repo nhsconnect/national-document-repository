1114b260d5df8a3019f9802a1a741f5dabf6843d
AlexHerbertNHS <alexandra.herbert1@nhs.net>
2023-11-24 17:13:55 +0000

    [PRMDR-529]
    Changing bulk upload code to ensure that bulk uploaded docs are moved to the appropriate S3 bucket BEFORE any Dynamo data is created about those files in the unlikely event that this will happen
    
    Small log change to keep SonarCloud happy

--- lambdas/services/bulk_upload_service.py
+++ lambdas/services/bulk_upload_service.py
@@ -59,9 +59,9 @@ class BulkUploadService:
             self.validate_files(staging_metadata)
         except LGInvalidFilesException as error:
             logger.info(
-                f"Detected invalid file name related to patient number: {staging_metadata.nhs_number}"
+                f"Detected invalid file name related to patient number: {staging_metadata.nhs_number}. Will stop "
+                f"processing Lloyd George record for this patient "
             )
-            logger.info("Will stop processing Lloyd George record for this patient")
 
             failure_reason = str(error)
             self.report_upload_failure(staging_metadata, failure_reason)
