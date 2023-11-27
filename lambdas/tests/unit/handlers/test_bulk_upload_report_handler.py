05ba2b3f3d173a4cbcab04aa06c52eb47161494a
RachelHowellNHS <rachel.howell6@nhs.net>
2023-11-27 17:12:11 +0000

    [PRMDR-473] WIP lambda tests

--- lambdas/tests/unit/handlers/test_bulk_upload_report_handler.py
+++ lambdas/tests/unit/handlers/test_bulk_upload_report_handler.py
@@ -1,8 +1,15 @@
-import pytest
-from services.bulk_upload_report_service import BulkUploadReportService
+from handlers.bulk_upload_report_handler import lambda_handler
 
 
-@pytest.fixture(scope="function")
-def mock_bulk_upload_report_service():
-    mock_bulk_upload_report_service = BulkUploadReportService()
-    yield mock_bulk_upload_report_service
+def test_bulk_upload_report_service_is_called(set_env, mocker, event, context):
+    mock_report_handler = mocker.patch(
+        "services.bulk_upload_report_service.BulkUploadReportService.report_handler"
+    )
+
+    lambda_handler(event, context)
+
+    mock_report_handler.assert_called_once()
+
+
+def test_client_error_is_thrown(mocker, event, context):
+    pass
