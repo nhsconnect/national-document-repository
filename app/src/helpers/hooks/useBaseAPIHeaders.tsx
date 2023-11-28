a39c2976a4b60b2346b16fe08d4634eb67387e55
Rio Knightley <rio.knightley2@nhs.net>
2023-11-28 15:58:05 +0000

    Remove baseApiHeaders throw

--- app/src/helpers/hooks/useBaseAPIHeaders.tsx
+++ app/src/helpers/hooks/useBaseAPIHeaders.tsx
@@ -4,11 +4,6 @@ import { AuthHeaders } from '../../types/blocks/authHeaders';
 function useBaseAPIHeaders(authHeaderName = 'Authorization') {
     const [session] = useSessionContext();
     const jwtToken = session.auth?.authorisation_token ?? '';
-
-    if (!jwtToken) {
-        throw Error('Session context has not been set!');
-    }
-
     const headers: AuthHeaders = {
         'Content-Type': 'application/json',
         [authHeaderName]: jwtToken,
