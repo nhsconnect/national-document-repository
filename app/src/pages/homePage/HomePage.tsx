4885c19de9a1c833d46ab9da4351536960faf7a2
Rio Knightley <rio.knightley2@nhs.net>
2023-11-29 13:57:27 +0000

    Add new content

--- app/src/pages/homePage/HomePage.tsx
+++ app/src/pages/homePage/HomePage.tsx
@@ -26,41 +26,42 @@ function HomePage(props: Props) {
     };
     return !isLoading ? (
         <>
-            <h1>Inactive Patient Record Administration</h1>
-            <p>
-                When a patient is inactive, NHS England via Primary Care Support England are
-                responsible for administration of their Electronic health record (EHR) and
-                attachments until they register at their next GP Practice.
-            </p>
-            <p>
-                General Practice Staff should use this service to upload an inactive patient&apos;s
-                electronic health record and attachments.
-            </p>
-            <p>
-                PCSE should use this service to search for and download patient records where there
-                has been an access request for an inactive patient health record.
-            </p>
-            <p>
-                If there is an issue with the service please contact the{' '}
-                <a
-                    href="https://digital.nhs.uk/about-nhs-digital/contact-us#nhs-digital-service-desks"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    NHS National Service Desk
-                </a>
-                .
-            </p>
+            <h1>Access and store digital GP records</h1>
+            <p>This service gives you access to Lloyd George digital health records.</p>
+            <p>You can use this service if you are:</p>
+            <ul>
+                <li>part of a GP practise and need to view, download or remove a patient record</li>
+                <li>
+                    managing records on behalf of NHS England and need to download a patient record
+                </li>
+            </ul>
+            <p>Not every patient will have a digital record available.</p>
             <h2>Before You Start</h2>
-            <p>You can only use this service if you have a valid NHS smartcard.</p>
+            <p>You'll be asked for:</p>
+            <ul>
+                <li>your NHS smartcard</li>
+                <li>patient details including their name, date of birth and NHS number</li>
+            </ul>
             <ButtonLink role="button" id="start-button" onClick={handleLogin}>
                 Start now
             </ButtonLink>
+            <h3>Get support with the service</h3>
+            Contact the{' '}
+            <a
+                href="https://digital.nhs.uk/about-nhs-digital/contact-us#nhs-digital-service-desks"
+                target="_blank"
+                rel="noreferrer"
+            >
+                NHS National Service Desk
+            </a>
+            if there is an issue with this service or call 0300 303 5678
             {(process.env.REACT_APP_ENVIRONMENT === 'local' ||
                 process.env.REACT_APP_ENVIRONMENT === 'development' ||
                 process.env.REACT_APP_ENVIRONMENT === 'test') && (
                 <div>
-                    <br></br>
+                    <br />
+                    <br />
+                    <br />
                     <h2>Test Panel</h2>
                     <p>
                         This section should only be displayed on a test/dev environment and should
