0764f601f34b300935348c4090cb6a3626289813
Joe Fong <joe.fong1@nhs.net>
2023-11-30 17:22:05 +0000

    [PRMDR-475] use extract_nhs_number_from_event instead of hardcoded keys extraction

--- lambdas/handlers/delete_document_reference_handler.py
+++ lambdas/handlers/delete_document_reference_handler.py
@@ -7,7 +7,8 @@ from utils.decorators.override_error_check import override_error_check
 from utils.decorators.set_audit_arg import set_request_context_for_logging
 from utils.decorators.validate_document_type import (
     extract_document_type_as_enum, validate_document_type)
-from utils.decorators.validate_patient_id import validate_patient_id
+from utils.decorators.validate_patient_id import (
+    extract_nhs_number_from_event, validate_patient_id)
 from utils.lambda_response import ApiGatewayResponse
 from utils.request_context import request_context
 
@@ -27,7 +28,7 @@ logger = LoggingService(__name__)
 def lambda_handler(event, context):
     logger.info("Delete Document Reference handler has been triggered")
 
-    nhs_number = event["queryStringParameters"]["patientId"]
+    nhs_number = extract_nhs_number_from_event(event)
     doc_type = extract_document_type_as_enum(event["queryStringParameters"]["docType"])
 
     request_context.app_interaction = LoggingAppInteraction.DELETE_RECORD.value
