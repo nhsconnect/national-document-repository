3a376cffc8bd6d8fb96f3908e36fe4f975567bfc
Rio Knightley <rio.knightley2@nhs.net>
2023-11-30 13:50:40 +0000

    Add phase banner content

--- app/src/components/layout/phaseBanner/phaseBanner.tsx
+++ app/src/components/layout/phaseBanner/phaseBanner.tsx
@@ -5,16 +5,25 @@ type Props = {};
 function PhaseBanner({}: Props) {
     return (
         <div className="govuk-phase-banner">
-            <p className="govuk-phase-banner__content">
-                <strong className="govuk-tag govuk-phase-banner__content__tag">Alpha</strong>
-                <span className="govuk-phase-banner__text">
-                    This is a new service – your{' '}
-                    <a className="govuk-link" href="#">
-                        feedback
-                    </a>{' '}
-                    will help us to improve it.
-                </span>
-            </p>
+            <div className="nhsuk-width-container">
+                <p className="govuk-phase-banner__content">
+                    <strong className="govuk-tag govuk-phase-banner__content__tag">
+                        New Service
+                    </strong>
+                    <span className="govuk-phase-banner__text">
+                        {'This is a new service - your '}
+                        <a
+                            className="govuk-link"
+                            href="https://digital.nhs.uk/about-nhs-digital/contact-us#nhs-digital-service-desks"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            feedback
+                        </a>
+                        {' will help us to improve it.'}
+                    </span>
+                </p>
+            </div>
         </div>
     );
 }
