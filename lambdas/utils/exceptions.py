28bf4f1b1f2dd13419740d151257a8231cb98031
RachelHowellNHS <rachel.howell6@nhs.net>
2023-12-07 15:31:57 +0000

    [PRMDR-479] remove duplicate

--- lambdas/utils/exceptions.py
+++ lambdas/utils/exceptions.py
@@ -110,11 +110,5 @@ class LogoutFailureException(Exception):
     pass
 
 
-class LambdaException(Exception):
-    def __init__(self, status_code, message):
-        self.status_code = status_code
-        self.message = message
-
-
 class LoginRedirectException(LambdaException):
     pass
