4a33f321e908ddb21bd8f8d40e4d01d98f55fc46
Rio Knightley <rio.knightley2@nhs.net>
2023-12-19 15:34:50 +0000

    Add log in smoke test

--- app/cypress/e2e/1-ndr-smoke-tests/homepage.cy.js
+++ app/cypress/e2e/1-ndr-smoke-tests/homepage.cy.js
@@ -1,9 +1,10 @@
 describe('Home Page', () => {
     const baseUrl = Cypress.config('baseUrl');
-    const username = Cypress.env('USERNAME') ?? 'test';
+    const username = Cypress.env('USERNAME');
     const password = Cypress.env('PASSWORD');
     const homeUrl = '/';
-
+    const authCallback = '/auth-callback';
+    const searchUrl = '/search/upload';
     beforeEach(() => {
         cy.visit(homeUrl);
     });
@@ -12,49 +13,51 @@ describe('Home Page', () => {
         cy.url().should('eq', baseUrl + homeUrl);
     });
 
-    context('Login tests', () => {
-        it('displays expected page header on home page when logged in', { tags: 'smoke' }, () => {
-            // Add CIS2 login steps
-            cy.get('header').should('have.length', 1);
+    it('[Smoke] should display patient search page after user log in', { tags: 'smoke' }, () => {
+        // Add CIS2 login steps
+        cy.get('header').should('have.length', 1);
 
-            cy.get('.nhsuk-logo__background').should('have.length', 1);
-            cy.get('.nhsuk-header__transactional-service-name').should('have.length', 1);
-            cy.get('.nhsuk-header__transactional-service-name').children().should('have.length', 1);
-            cy.get('.nhsuk-header__transactional-service-name--link').should(
-                'have.text',
-                'Access and store digital GP records',
-            );
+        cy.get('.nhsuk-logo__background').should('have.length', 1);
+        cy.get('.nhsuk-header__transactional-service-name').should('have.length', 1);
+        cy.get('.nhsuk-header__transactional-service-name').children().should('have.length', 1);
+        cy.get('.nhsuk-header__transactional-service-name--link').should(
+            'have.text',
+            'Access and store digital GP records',
+        );
+        cy.get('.nhsuk-header__navigation').should('not.exist');
+        cy.get('.nhsuk-header__navigation-list').should('not.exist');
 
-            cy.getByTestId('start-btn').should('exist');
-            cy.getByTestId('start-btn').click();
-            cy.origin(
-                'https://am.nhsdev.auth-ptl.cis2.spineservices.nhs.uk',
-                { args: { username, password } },
-                (args) => {
-                    const { username, password } = args;
-                    cy.url().should('include', 'cis2.spineservices.nhs.uk');
+        cy.getByTestId('start-btn').should('exist');
+        cy.getByTestId('start-btn').click();
+        cy.origin(
+            'https://am.nhsdev.auth-ptl.cis2.spineservices.nhs.uk',
+            { args: { username, password } },
+            (args) => {
+                Cypress.on('uncaught:exception', () => false);
+                const practitionerRoleId = 555053929106;
+                const { username, password } = args;
+                cy.url().should('include', 'cis2.spineservices.nhs.uk');
+                cy.get('.nhsuk-cis2-cia-header-text').should('exist');
+                cy.get('.nhsuk-cis2-cia-header-text').should(
+                    'have.text',
+                    'CIS2 - Care Identity Authentication',
+                );
+                cy.get('#floatingLabelInput19').should('exist');
+                cy.get('#floatingLabelInput19').type(username);
+                cy.get('#floatingLabelInput25').should('exist');
+                cy.get('#floatingLabelInput25').type(password);
 
-                    cy.get('.nhsuk-cis2-cia-header-text').should('exist');
-                    cy.get('.nhsuk-cis2-cia-header-text').should(
-                        'have.text',
-                        'CIS2 - Care Identity Authentication',
-                    );
+                cy.get('.nhsuk-button').should('exist');
+                cy.get('.nhsuk-button').invoke('attr', 'type').should('eq', 'submit');
+                cy.get('.nhsuk-button').click();
+                cy.get(`#nhsRoleId_${practitionerRoleId}`).should('exist');
+                cy.get(`#nhsRoleId_${practitionerRoleId}`).click();
+            },
+        );
+        cy.url().should('contain', baseUrl + authCallback);
+        cy.url({ timeout: 10000 }).should('eq', baseUrl + searchUrl);
 
-                    cy.get('#floatingLabelInput19').should('exist');
-                    cy.get('#floatingLabelInput19').type(username);
-
-                    cy.get('#floatingLabelInput25').should('exist');
-                    cy.get('#floatingLabelInput25').type(password);
-
-                    cy.get('.nhsuk-button').should('exist');
-                    cy.get('.nhsuk-button').invoke('attr', 'type').should('eq', 'submit');
-                    cy.get('.nhsuk-button').click();
-                },
-            );
-
-            // Restore once login steps implemented
-            // cy.get('.nhsuk-header__navigation').should('have.length', 1);
-            // cy.get('.nhsuk-header__navigation-list').should('have.length', 1);
-        });
+        cy.get('.nhsuk-header__navigation').should('exist');
+        cy.get('.nhsuk-header__navigation-list').should('exist');
     });
 });
