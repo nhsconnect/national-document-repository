6df364e4cfce6dba0d6dd70da673517149d0d662
Rio Knightley <rio.knightley2@nhs.net>
2024-01-03 10:01:40 +0000

    Add smoke tests for pcse workflow

---
+++ app/cypress/e2e/1-ndr-smoke-tests/pcse_user_workflows/patient_search_and_verify_workflow.cy.js
@@ -0,0 +1,63 @@
+const { Roles } = require('../../../support/roles');
+
+describe('PCSE Workflow: patient search and verify', () => {
+    // env vars
+    const baseUrl = Cypress.config('baseUrl');
+    const homeUrl = '/';
+
+    beforeEach(() => {
+        cy.visit(homeUrl);
+    });
+
+    it(
+        '[Smoke] It redirects to the patient download screen when patient search successfully by a PCSE user',
+        { tags: 'smoke' },
+        () => {
+            const activePatient = '9449305552';
+            cy.smokeLogin(Roles.PCSE);
+            cy.get('#nhs-number-input').click();
+            cy.get('#nhs-number-input').type(activePatient);
+
+            cy.get('#search-submit').click();
+            cy.url({ timeout: 10000 }).should('eq', baseUrl + '/search/patient/result');
+            cy.get('#gp-message').should('not.exist');
+
+            cy.get('#verify-submit').click();
+            cy.url({ timeout: 10000 }).should('eq', baseUrl + '/search/results');
+        },
+    );
+
+    it(
+        '[Smoke] It searches for a valid patient successfully when the user enters a known nhs number with spaces by a PCSE user',
+        { tags: 'smoke' },
+        () => {
+            const activePatient = '944 930 5552';
+            cy.smokeLogin(Roles.PCSE);
+            cy.get('#nhs-number-input').click();
+            cy.get('#nhs-number-input').type(activePatient);
+
+            cy.get('#search-submit').click();
+            cy.wait('@search');
+
+            cy.url().should('include', 'result');
+            cy.url().should('eq', baseUrl + '/search/patient/result');
+        },
+    );
+
+    it(
+        '[Smoke] It searches for a valid patient successfully when the user enters a known nhs number with dashes by a PCSE user',
+        { tags: 'smoke' },
+        () => {
+            const activePatient = '944-930-5552';
+
+            cy.smokeLogin(Roles.PCSE);
+            cy.get('#nhs-number-input').click();
+            cy.get('#nhs-number-input').type(activePatient);
+
+            cy.get('#search-submit').click();
+
+            cy.url().should('include', 'result');
+            cy.url().should('eq', baseUrl + '/search/patient/result');
+        },
+    );
+});
