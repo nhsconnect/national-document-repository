da00abf4f13fc1b7c4bde20943bc030a90481982
abbas-khan10 <abbas.khan10@nhs.net>
2024-01-08 12:59:27 +0000

    Add error handling to token ssm service

--- lambdas/tests/unit/services/test_token_handler_ssm_service.py
+++ lambdas/tests/unit/services/test_token_handler_ssm_service.py
@@ -2,13 +2,21 @@ from datetime import datetime
 
 import pytest
 from services.token_handler_ssm_service import TokenHandlerSSMService
+from utils.constants.ssm import (
+    GP_ADMIN_USER_ROLE_CODE,
+    GP_CLINICAL_USER_ROLE_CODE,
+    GP_ORG_ROLE_CODE,
+    PCSE_ODS_CODE,
+    PCSE_USER_ROLE_CODE,
+)
+from utils.lambda_exceptions import LoginException
 
 MOCK_ROLE_CODE_RESPONSE = {
     "Parameters": [
         {
-            "Name": "ods_code_pcse",
+            "Name": GP_ORG_ROLE_CODE,
             "Type": "String",
-            "Value": "X0123",
+            "Value": "G0123",
             "Version": 123,
             "Selector": "string",
             "SourceResult": "string",
@@ -17,7 +25,18 @@ MOCK_ROLE_CODE_RESPONSE = {
             "DataType": "string",
         },
         {
-            "Name": "/auth/smartcard/role/gp_admin_multiple",
+            "Name": PCSE_ODS_CODE,
+            "Type": "String",
+            "Value": "P0123",
+            "Version": 123,
+            "Selector": "string",
+            "SourceResult": "string",
+            "LastModifiedDate": datetime(2015, 1, 1),
+            "ARN": "string",
+            "DataType": "string",
+        },
+        {
+            "Name": GP_ADMIN_USER_ROLE_CODE,
             "Type": "StringList",
             "Value": "R0001,R0002,R0003",
             "Version": 123,
@@ -28,7 +47,7 @@ MOCK_ROLE_CODE_RESPONSE = {
             "DataType": "string",
         },
         {
-            "Name": "/auth/smartcard/role/gp_clinical",
+            "Name": GP_CLINICAL_USER_ROLE_CODE,
             "Type": "String",
             "Value": "R0002",
             "Version": 123,
@@ -39,7 +58,7 @@ MOCK_ROLE_CODE_RESPONSE = {
             "DataType": "string",
         },
         {
-            "Name": "/auth/smartcard/role/pcse",
+            "Name": PCSE_USER_ROLE_CODE,
             "Type": "String",
             "Value": "R0003",
             "Version": 123,
@@ -120,3 +139,127 @@ def test_get_smartcard_role_gp_admin(mock_service, mock_ssm):
     )
 
     assert actual == expected
+
+
+def test_get_smartcard_role_gp_admin_raises_login_exception(mock_service, mock_ssm):
+    mock_ssm.get_parameters.return_value = {"Parameters": []}
+    expected = LoginException(500, "Failed to find SSM parameter value for user role")
+
+    with pytest.raises(LoginException) as actual:
+        mock_service.get_smartcard_role_gp_admin()
+
+    mock_ssm.get_parameters.assert_called_once_with(
+        Names=["/auth/smartcard/role/gp_admin_multiple"], WithDecryption=False
+    )
+
+    assert actual.value.__dict__ == expected.__dict__
+
+
+def test_get_smartcard_role_gp_clinical(mock_service, mock_ssm):
+    mock_ssm.get_parameters.return_value = MOCK_ROLE_CODE_RESPONSE
+    expected = "R0002"
+
+    actual = mock_service.get_smartcard_role_gp_clinical()
+
+    mock_ssm.get_parameters.assert_called_once_with(
+        Names=["/auth/smartcard/role/gp_clinical"], WithDecryption=False
+    )
+
+    assert actual == expected
+
+
+def test_get_smartcard_role_gp_clinical_raises_login_exception(mock_service, mock_ssm):
+    mock_ssm.get_parameters.return_value = {"Parameters": []}
+    expected = LoginException(500, "Failed to find SSM parameter value for user role")
+
+    with pytest.raises(LoginException) as actual:
+        mock_service.get_smartcard_role_gp_clinical()
+
+    mock_ssm.get_parameters.assert_called_once_with(
+        Names=["/auth/smartcard/role/gp_clinical"], WithDecryption=False
+    )
+
+    assert actual.value.__dict__ == expected.__dict__
+
+
+def test_get_smartcard_role_pcse(mock_service, mock_ssm):
+    mock_ssm.get_parameters.return_value = MOCK_ROLE_CODE_RESPONSE
+    expected = "R0003"
+
+    actual = mock_service.get_smartcard_role_pcse()
+
+    mock_ssm.get_parameters.assert_called_once_with(
+        Names=["/auth/smartcard/role/pcse"], WithDecryption=False
+    )
+
+    assert actual == expected
+
+
+def test_get_smartcard_role_pcse_raises_login_exception(mock_service, mock_ssm):
+    mock_ssm.get_parameters.return_value = {"Parameters": []}
+    expected = LoginException(500, "Failed to find SSM parameter value for user role")
+
+    with pytest.raises(LoginException) as actual:
+        mock_service.get_smartcard_role_pcse()
+
+    mock_ssm.get_parameters.assert_called_once_with(
+        Names=["/auth/smartcard/role/pcse"], WithDecryption=False
+    )
+
+    assert actual.value.__dict__ == expected.__dict__
+
+
+def test_get_org_role_codes(mock_service, mock_ssm):
+    mock_ssm.get_parameters.return_value = MOCK_ROLE_CODE_RESPONSE
+    expected = ["G0123"]
+
+    actual = mock_service.get_org_role_codes()
+
+    mock_ssm.get_parameters.assert_called_once_with(
+        Names=["/auth/org/role_code/gpp"], WithDecryption=False
+    )
+
+    assert actual == expected
+
+
+def test_get_org_role_codes_raises_login_exception(mock_service, mock_ssm):
+    mock_ssm.get_parameters.return_value = {"Parameters": []}
+    expected = LoginException(500, "Failed to find SSM parameter value for GP org role")
+
+    with pytest.raises(LoginException) as actual:
+        mock_service.get_org_role_codes()
+
+    mock_ssm.get_parameters.assert_called_once_with(
+        Names=["/auth/org/role_code/gpp"], WithDecryption=False
+    )
+
+    assert actual.value.__dict__ == expected.__dict__
+
+
+def test_get_org_ods_codes(mock_service, mock_ssm):
+    mock_ssm.get_parameters.return_value = MOCK_ROLE_CODE_RESPONSE
+    expected = ["P0123"]
+
+    actual = mock_service.get_org_ods_codes()
+
+    mock_ssm.get_parameters.assert_called_once_with(
+        Names=["/auth/org/ods_code/pcse"], WithDecryption=False
+    )
+
+    assert actual == expected
+
+
+def test_get_org_ods_codes_raises_login_exception(mock_service, mock_ssm):
+    mock_ssm.get_parameters.return_value = {"Parameters": []}
+    expected = LoginException(
+        500, "SSM parameter values for PSCE ODS code may not exist"
+    )
+
+    with pytest.raises(LoginException) as actual:
+        mock_service.get_org_ods_codes()
+
+    mock_ssm.get_parameters.assert_called_once_with(
+        Names=["/auth/org/ods_code/pcse"], WithDecryption=False
+    )
+
+    assert actual.value.__dict__ == expected.__dict__
