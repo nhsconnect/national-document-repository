9e63948bb1f49fddb9b693628c543991a11c743c
Rio Knightley <128376976+RioKnightleyNHS@users.noreply.github.com>
2024-01-08 16:30:33 +0000

    PRMDR-551 BSOL (#222)
    Add error handling for non BSOL users

--- app/src/components/blocks/lloydGeorgeRecordStage/LloydGeorgeRecordStage.tsx
+++ app/src/components/blocks/lloydGeorgeRecordStage/LloydGeorgeRecordStage.tsx
@@ -1,7 +1,16 @@
 import React, { Dispatch, SetStateAction, useState } from 'react';
-import { BackLink, Card, Details, WarningCallout } from 'nhsuk-react-components';
+import {
+    BackLink,
+    Card,
+    Checkboxes,
+    Details,
+    Fieldset,
+    InsetText,
+    WarningCallout,
+} from 'nhsuk-react-components';
 import { getFormattedDate } from '../../../helpers/utils/formatDate';
 import { DOWNLOAD_STAGE } from '../../../types/generic/downloadStage';
+import { Button } from 'nhsuk-react-components';
 import PdfViewer from '../../generic/pdfViewer/PdfViewer';
 import LloydGeorgeRecordDetails from '../lloydGeorgeRecordDetails/LloydGeorgeRecordDetails';
 import { formatNhsNumber } from '../../../helpers/utils/formatNhsNumber';
@@ -11,6 +20,9 @@ import LloydGeorgeRecordError from '../lloydGeorgeRecordError/LloydGeorgeRecordE
 import useRole from '../../../helpers/hooks/useRole';
 import { REPOSITORY_ROLE } from '../../../types/generic/authRole';
 import useIsBSOL from '../../../helpers/hooks/useIsBSOL';
+import ErrorBox from '../../layout/errorBox/ErrorBox';
+import { useForm } from 'react-hook-form';
+import { InputRef } from '../../../types/generic/inputRef';
 
 export type Props = {
     downloadStage: DOWNLOAD_STAGE;
@@ -36,6 +48,13 @@ function LloydGeorgeRecordStage({
     const dob: String = patientDetails?.birthDate
         ? getFormattedDate(new Date(patientDetails.birthDate))
         : '';
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({ reValidateMode: 'onSubmit' });
+    const { ref: inputRef, ...checkboxProps } = register('confirmBsol', { required: true });
 
     const nhsNumber: string = patientDetails?.nhsNumber || '';
     const formattedNhsNumber = formatNhsNumber(nhsNumber);
@@ -63,6 +82,13 @@ function LloydGeorgeRecordStage({
 
     return (
         <div className="lloydgeorge_record-stage">
+            {errors.confirmBsol && (
+                <ErrorBox
+                    errorBoxSummaryId="78"
+                    messageTitle="There is a problem"
+                    errorBody="You must confirm if you want to download and remove this record"
+                />
+            )}
             {fullScreen && (
                 <BackLink
                     data-testid="back-link"
@@ -89,7 +115,47 @@ function LloydGeorgeRecordStage({
                             should follow data protection principles as outlined in UK General Data
                             Protection Regulation (GDPR).
                         </p>
+
+                        <InsetText>
+                            <form
+                                onSubmit={handleSubmit(() => {
+                                    // Success handler here
+                                })}
+                                className={
+                                    errors.confirmBsol
+                                        ? 'nhsuk-form-group--error'
+                                        : 'nhsuk-form-group'
+                                }
+                            >
+                                <Fieldset aria-describedby="waste-hint">
+                                    <h4>
+                                        Are you sure you want to download and remove this record?
+                                    </h4>
+                                    <Checkboxes
+                                        error={
+                                            errors.confirmBsol
+                                                ? 'Confirm if you want to download and remove this record'
+                                                : undefined
+                                        }
+                                    >
+                                        <Checkboxes.Box
+                                            inputRef={inputRef as InputRef}
+                                            {...checkboxProps}
+                                        >
+                                            I understand that downloading this record removes it
+                                            from storage
+                                        </Checkboxes.Box>
+                                    </Checkboxes>
+                                </Fieldset>
+
+                                <Button type="submit" id="blah">
+                                    {' '}
+                                    Test submit button{' '}
+                                </Button>
+                            </form>
+                        </InsetText>
                     </WarningCallout>
+
                     <h1>Available records</h1>
                 </div>
             )}
