92592a7adcb94acd0617d46bca59fc0a2b8c4948
Richard Smith <richard.smith33@nhs.net>
2024-01-11 13:56:15 +0000

    Formatting

--- lambdas/handlers/document_manifest_by_nhs_number_handler.py
+++ lambdas/handlers/document_manifest_by_nhs_number_handler.py
@@ -1,5 +1,4 @@
 from botocore.exceptions import ClientError
-
 from enums.logging_app_interaction import LoggingAppInteraction
 from services.document_manifest_service import DocumentManifestService
 from utils.audit_logging_setup import LoggingService
@@ -7,7 +6,8 @@ from utils.decorators.ensure_env_var import ensure_environment_variables
 from utils.decorators.override_error_check import override_error_check
 from utils.decorators.set_audit_arg import set_request_context_for_logging
 from utils.decorators.validate_document_type import (
-    validate_document_type, extract_document_type_as_enum,
+    extract_document_type_as_enum,
+    validate_document_type,
 )
 from utils.decorators.validate_patient_id import validate_patient_id
 from utils.lambda_exceptions import DocumentManifestServiceException
@@ -61,6 +61,6 @@ def lambda_handler(event, context):
     except ClientError as e:
         logger.error(str(e), {"Result": f"Unsuccessful download due to {str(e)}"})
         response = ApiGatewayResponse(
-            500, "An error occurred when creating document manifest", "GET"
+            500, f"An error occurred when creating document manifest: {str(e)}", "GET"
         ).create_api_gateway_response()
         return response
