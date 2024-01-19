5bbd3e4a7bde077a716582729cee9a6c7a64704d
premnhs4 <142592124+premnhs4@users.noreply.github.com>
2024-01-19 16:33:26 +0000

    smoke test scenarios for unhappy path (#255)
    * smoke test scenarios for unhappy path where non matching ODS patient on PDS is not accessible for the defined valid roles

---
+++ app/cypress/e2e/1-ndr-smoke-tests/gp_user_workflows/patient_search_and_verify_unhappy_workflow.cy.js
@@ -0,0 +1,36 @@
+import { Roles, roleName } from '../../../support/roles';
+
+describe('GP Workflow: GP Role rejected from accessing a non mating ODS patient on PDS', () => {
+    // env vars
+    const baseUrl = Cypress.config('baseUrl');
+    const gpRoles = [Roles.SMOKE_GP_ADMIN_H85686, Roles.SMOKE_GP_CLINICAL_H85686];
+
+    const workspace = Cypress.env('WORKSPACE');
+    const activePatient = workspace === 'ndr-dev' ? '9730153817' : '9000000002';
+    const homeUrl = '/home';
+
+    gpRoles.forEach((role) => {
+        it(
+            `[Smoke] Shows that non matching ODS patient on PDS is not accessable for this  ${roleName(
+                role,
+            )} `,
+            { tags: 'smoke' },
+            () => {
+                cy.smokeLogin(role);
+
+                cy.url({ timeout: 10000 }).should('eq', baseUrl + homeUrl);
+                cy.getByTestId('search-patient-btn').should('exist');
+                cy.getByTestId('search-patient-btn').click();
+
+                cy.get('#nhs-number-input').click();
+                cy.get('#nhs-number-input').type(activePatient);
+                cy.get('#search-submit').click();
+                // Assert
+                cy.get('#nhs-number-input--error-message', { timeout: 20000 }).should(
+                    'include.text',
+                    'Sorry, patient data not found.',
+                );
+            },
+        );
+    });
+});
