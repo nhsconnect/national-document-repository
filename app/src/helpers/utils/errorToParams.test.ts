542fb778d97fac49237bfb0d2878723957bcc005
RachelHowellNHS <rachel.howell6@nhs.net>
2024-01-23 10:04:35 +0000

    [PRMDR-554] fix util test

--- app/src/helpers/utils/errorToParams.test.ts
+++ app/src/helpers/utils/errorToParams.test.ts
@@ -1,26 +1,28 @@
 import { errorToParams } from './errorToParams';
 import { AxiosError } from 'axios';
 
-test('returns empty string param if error has no err_code', () => {
-    const errorResponse = {
-        response: {
-            status: 500,
-            data: { message: '500 Unknown Service Error.' },
-        },
-    };
-    const error = errorResponse as AxiosError;
+describe('errorToParams util function', () => {
+    test('returns empty string param if error has no err_code', () => {
+        const errorResponse = {
+            response: {
+                status: 500,
+                data: { message: '500 Unknown Service Error.' },
+            },
+        };
+        const error = errorResponse as AxiosError;
 
-    expect(errorToParams(error)).toBe('');
-});
+        expect(errorToParams(error)).toBe('');
+    });
 
-test('returns param with error code', () => {
-    const errorResponse = {
-        response: {
-            status: 500,
-            data: { message: '500 Unknown Service Error.', err_code: 'test' },
-        },
-    };
-    const error = errorResponse as AxiosError;
+    test('returns param with error code', () => {
+        const errorResponse = {
+            response: {
+                status: 500,
+                data: { message: '500 Unknown Service Error.', err_code: 'test' },
+            },
+        };
+        const error = errorResponse as AxiosError;
 
-    expect(errorToParams(error)).toBe('?errorCode=test');
+        expect(errorToParams(error)).toBe('?errorCode=test');
+    });
 });
