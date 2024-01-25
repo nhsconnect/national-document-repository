5ac0d84f5265797b6024d63a0677986a78a345e7
NogaNHS <noga.sasson1@nhs.net>
2024-01-25 16:25:00 +0000

    add text to LG for non BSOL

--- app/src/components/blocks/lloydGeorgeDownloadComplete/LloydGeorgeDownloadComplete.tsx
+++ app/src/components/blocks/lloydGeorgeDownloadComplete/LloydGeorgeDownloadComplete.tsx
@@ -36,9 +36,51 @@ function LloydGeorgeDownloadComplete({ setStage, setDownloadStage, deleteAfterDo
                     <div>{`(NHS number: ${patientDetails?.nhsNumber})`}</div>
                 </Card.Content>
             </Card>
+            (deleteAfterDownload ?
+            <>
+                <p>This record has been removed from our storage.</p>
+                <h2>Keep this patient's record safe</h2>
+                <ol>
+                    <li>
+                        Store the record in accessible and recoverable format within a secure
+                        network folder
+                    </li>
+                    <li>
+                        Ensure all access to the record is auditable, so information can be provided
+                        about who accessed the record
+                    </li>
+                    <li>
+                        If the patient moves practice, you must make sure the patient record safely
+                        transfers to their new practice
+                    </li>
+                    <li>
+                        Follow data protection principles outlined in
+                        <a href="https://ico.org.uk/for-organisations/uk-gdpr-guidance-and-resources/data-protection-principles/a-guide-to-the-data-protection-principles/">
+                            UK General Data Protection Regulation (UK GDPR)
+                        </a>
+                    </li>
+                </ol>
+                <h3>Your responsibilities with this record</h3>
+                <p>
+                    Everyone in a health and care organisation is responsible for managing records
+                    appropriately. It is important all general practice staff understand their
+                    responsibilities for creating, maintaining, and disposing of records
+                    appropriately.
+                </p>
+                <h3>Follow the Record Management Code of Practice </h3>
+                <p>
+                    <a href="https://transform.england.nhs.uk/information-governance/guidance/records-management-code">
+                        Record Management Code of Practice
+                    </a>
+                    provides a framework for consistent and effective records management, based on
+                    established standards.
+                </p>
+            </>
+            :
             <Button onClick={handleReturnButtonClick} data-testid="return-btn">
                 Return to patient's available medical records
             </Button>
+            )
         </div>
     );
 }
