09eac5b8b46fbf3570d8452d8ff1867b912fa128
RachelHowellNHS <rachel.howell6@nhs.net>
2024-01-25 12:23:16 +0000

    [PRMDR-604] return true from find org relationship for testing

--- lambdas/services/ods_api_service.py
+++ lambdas/services/ods_api_service.py
@@ -109,16 +109,17 @@ def find_and_get_pcse_ods(ods_code):
 
 def find_org_relationship(org_data):
     logger.info("Checking relationships")
-    try:
-        relationships: List[Dict] = org_data["Rels"]["Rel"]
-        for rel in relationships:
-            if (
-                rel["Status"] == "Active"
-                and rel["id"] == OrganisationRelationship.BSOL_REL_ID.value
-                and rel["Target"]["OrgId"]["extension"]
-                == OrganisationRelationship.BSOL_ORG_ID.value
-            ):
-                return True
-    except (KeyError, TypeError):
-        logger.info("Failure fetching relationships")
-    return False
+    # try:
+    #     relationships: List[Dict] = org_data["Rels"]["Rel"]
+    #     for rel in relationships:
+    #         if (
+    #             rel["Status"] == "Active"
+    #             and rel["id"] == OrganisationRelationship.BSOL_REL_ID.value
+    #             and rel["Target"]["OrgId"]["extension"]
+    #             == OrganisationRelationship.BSOL_ORG_ID.value
+    #         ):
+    #             return True
+    # except (KeyError, TypeError):
+    #     logger.info("Failure fetching relationships")
+    # return False
+    return True
