6f6c5c5404425d5a59c7de064f5a7c6f9e785e4c
Joe Fong <joe.fong1@nhs.net>
2024-01-25 14:13:55 +0000

    [PRMDR-605] Change homepage to redirect to search patient page for BSOL GP users or PCSE users

--- app/src/pages/homePage/HomePage.test.tsx
+++ app/src/pages/homePage/HomePage.test.tsx
@@ -2,6 +2,8 @@ import { render, screen, waitFor } from '@testing-library/react';
 import HomePage from './HomePage';
 import useIsBSOL from '../../helpers/hooks/useIsBSOL';
 import { routes } from '../../types/generic/routes';
+import useRole from '../../helpers/hooks/useRole';
+import { REPOSITORY_ROLE } from '../../types/generic/authRole';
 
 const mockedUseNavigate = jest.fn();
 jest.mock('react-router', () => ({
@@ -9,70 +11,38 @@ jest.mock('react-router', () => ({
 }));
 
 jest.mock('../../helpers/hooks/useIsBSOL');
+jest.mock('../../helpers/hooks/useRole');
 const mockUseIsBsol = useIsBSOL as jest.Mock;
+const mockUseRole = useRole as jest.Mock;
 
 describe('HomePage', () => {
     afterEach(() => {
         jest.clearAllMocks();
     });
-    describe('BSOL Rendering', () => {
+
+    const gpRoles = [REPOSITORY_ROLE.GP_ADMIN, REPOSITORY_ROLE.GP_CLINICAL];
+
+    describe('BSOL Rendering for GP roles', () => {
         beforeEach(() => {
             mockUseIsBsol.mockReturnValue(true);
         });
-
-        it('renders homepage content', () => {
-            const contentStrings = [
-                'This service gives you access to Lloyd George digital health records. ' +
-                    'You may have received a note within a patient record, stating that the record has been digitised.',
-                'If you are part of a GP practice, you can use this service to:',
-                'view a patient record',
-                'remove a patient record',
-                'If you are managing records on behalf of NHS England, you can:',
-                'Not every patient will have a digital record available.',
-                'Before you start',
-                'You’ll be asked for:',
-                'your NHS smartcard',
-                'patient details including their name, date of birth and NHS number',
-            ];
+        it.each(gpRoles)('[%s] redirect user to search patient page', (role) => {
+            mockUseRole.mockReturnValue(role);
 
             render(<HomePage />);
 
-            contentStrings.forEach((s) => {
-                expect(screen.getByText(s)).toBeInTheDocument();
-            });
-
-            const downloadPatientRecord = screen.getAllByText('download a patient record');
-            expect(downloadPatientRecord).toHaveLength(2);
-
-            expect(screen.getByText(/Contact the/i)).toBeInTheDocument();
-            expect(
-                screen.getByRole('link', {
-                    name: /NHS National Service Desk/i,
-                }),
-            ).toBeInTheDocument();
-            expect(
-                screen.getByText(/if there is an issue with this service or call 0300 303 5678\./i),
-            ).toBeInTheDocument();
-
-            expect(screen.getByTestId('search-patient-btn')).toBeInTheDocument();
+            expect(mockedUseNavigate).toHaveBeenCalledWith(routes.SEARCH_PATIENT);
         });
+    });
 
-        it('renders a service link that takes you to service help-desk in a new tab', () => {
+    describe('PCSE Rendering', () => {
+        beforeEach(() => {
+            mockUseRole.mockReturnValue(REPOSITORY_ROLE.PCSE);
+        });
+        it('redirect user to search patient page', () => {
             render(<HomePage />);
 
-            expect(screen.getByText(/Contact the/i)).toBeInTheDocument();
-            const nationalServiceDeskLink = screen.getByRole('link', {
-                name: /NHS National Service Desk/i,
-            });
-            expect(
-                screen.getByText(/if there is an issue with this service or call 0300 303 5678/i),
-            ).toBeInTheDocument();
-
-            expect(nationalServiceDeskLink).toHaveAttribute(
-                'href',
-                'https://digital.nhs.uk/about-nhs-digital/contact-us#nhs-digital-service-desks',
-            );
-            expect(nationalServiceDeskLink).toHaveAttribute('target', '_blank');
+            expect(mockedUseNavigate).toHaveBeenCalledWith(routes.SEARCH_PATIENT);
         });
     });
 
