81b3fc181a8eeb5ce1fd1f0f3288ebc54d3f3e77
Rio Knightley <rio.knightley2@nhs.net>
2024-02-14 16:08:59 +0000

    Add lloyd george complete view

--- app/src/pages/lloydGeorgeUploadPage/LloydGeorgeUploadPage.tsx
+++ app/src/pages/lloydGeorgeUploadPage/LloydGeorgeUploadPage.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import LloydGeorgeUploadingStage from '../../components/blocks/lloydGeorgeUploadingStage/LloydGeorgeUploadingStage';
 import { UploadDocument } from '../../types/pages/UploadDocumentsPage/types';
 import LloydGeorgeFileInputStage from '../../components/blocks/lloydGeorgeFileInputStage/LloydGeorgeFileInputStage';
+import LloydGeorgeUploadComplete from '../../components/blocks/lloydGeorgeUploadComplete/LloydGeorgeUploadComplete';
 
 type Props = {};
 
@@ -14,16 +15,7 @@ export enum LG_UPLOAD_STAGE {
 function LloydGeorgeUploadPage({}: Props) {
     const [stage, setStage] = useState<LG_UPLOAD_STAGE>(LG_UPLOAD_STAGE.SELECT);
     const [documents, setDocuments] = useState<Array<UploadDocument>>([]);
-    // const files = [
-    //     buildTextFile('1of3_Lloyd_George_Record_[Barry ONEIL]_[9730182000]_[23-10-2018]', 100),
-    //     buildTextFile('2of3_Lloyd_George_Record_[Barry ONEIL]_[9730182000]_[23-10-2018]', 101),
-    // ];
-    // const documents = files.map((file) => {
-    //     return {
-    //         ...buildDocument(file, DOCUMENT_UPLOAD_STATE.SUCCEEDED),
-    //         progress: 100,
-    //     };
-    // });
+
     switch (stage) {
         case LG_UPLOAD_STAGE.SELECT:
             return (
@@ -36,19 +28,7 @@ function LloydGeorgeUploadPage({}: Props) {
         case LG_UPLOAD_STAGE.UPLOAD:
             return <LloydGeorgeUploadingStage documents={documents} setStage={setStage} />;
         case LG_UPLOAD_STAGE.COMPLETE:
-            return (
-                // TODO, ADD UPLOAD COMPLETE STAGE
-                <div>
-                    <button
-                        onClick={() => {
-                            setStage(LG_UPLOAD_STAGE.SELECT);
-                        }}
-                    >
-                        back to upload
-                    </button>
-                    <div>upload complete</div>
-                </div>
-            );
+            return <LloydGeorgeUploadComplete documents={documents} />;
         default:
             return null;
     }
