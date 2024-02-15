0b2be5f75138fc4bf26ab0d78f439321d7fe2623
RachelHowellNHS <rachel.howell6@nhs.net>
2024-02-15 10:29:17 +0000

    [PRMDR-642] copy ts file and test file to check they are picked up by sonarcloud

---
+++ app/src/helpers/utils/errorToParams2TEST.ts
@@ -0,0 +1,10 @@
+import { ErrorResponse } from '../../types/generic/errorResponse';
+import { AxiosError } from 'axios';
+import { unixTimestamp } from './createTimestamp';
+
+export const errorToParams = (error: AxiosError) => {
+    const errorResponse = (error.response?.data as ErrorResponse) ?? {};
+    const { err_code = '', interaction_id = unixTimestamp().toString() } = errorResponse;
+    const params = JSON.stringify([err_code, interaction_id]);
+    return '?encodedError=' + btoa(params);
+};
