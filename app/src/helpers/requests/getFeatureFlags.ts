47d9fa4fa8d82f1c7f18d2bae9680efeb9966f15
Scott Alexander <scott.alexander@madetech.com>
2024-02-21 12:04:48 +0000

    added new api calling helper getFeatureFlags

---
+++ app/src/helpers/requests/getFeatureFlags.ts
@@ -0,0 +1,39 @@
+import { AppConfig} from '../../providers/featureFlagsProvider/FeatureFlagsProvider';
+import { AuthHeaders } from '../../types/blocks/authHeaders';
+import { endpoints } from '../../types/generic/endpoints';
+
+import axios from 'axios';
+
+type Args = {
+    baseUrl: string;
+    baseHeaders: AuthHeaders;
+};
+
+
+type GetFeatureFlagsResponse = {
+    data: AppConfig;
+};
+
+const defaultFeatureFlags = {
+    testFeature: true,
+    testRoute: true,
+    testFeature1: false,
+    testFeature2: false,
+    testFeature3: false
+}
+
+const getFeatureFlags = async ({ baseUrl, baseHeaders }: Args) => {
+    const gatewayUrl = baseUrl + endpoints.FEATURE_FLAGS;
+    try {
+        const { data }: GetFeatureFlagsResponse = await axios.get(gatewayUrl, {
+            headers: {
+                ...baseHeaders,
+            }
+        });
+        return data;
+    } catch (e) {
+        return defaultFeatureFlags;
+    }
+};
+
+export default getFeatureFlags;
