0255595fc710574835ae661cda69b50bc0008022
RachelHowellNHS <rachel.howell6@nhs.net>
2024-02-23 10:18:28 +0000

    [PRMDR-699] remove string from enum params

--- lambdas/enums/feature_flags.py
+++ lambdas/enums/feature_flags.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 
-class FeatureFlags(str, Enum):
+class FeatureFlags(Enum):
     NEMS_ENABLED = "nemsEnabled"
     UPLOAD_LLOYD_GEORGE_WORKFLOW_ENABLED = "uploadLloydGeorgeWorkflowEnabled"
     UPLOAD_LAMBDA_ENABLED = "uploadLambdaEnabled"
