802924600f0aeac2603fa3f5bf5e8f9d3f112eb0
Rio Knightley <rio.knightley2@nhs.net>
2024-03-07 16:00:11 +0000

    Add retry upload warning stage

--- app/src/components/blocks/lloydGeorgeRetryUploadStage/LloydGeorgeRetryUploadStage.tsx
+++ app/src/components/blocks/lloydGeorgeRetryUploadStage/LloydGeorgeRetryUploadStage.tsx
@@ -1,9 +1,62 @@
+import { Button, ButtonLink, WarningCallout } from 'nhsuk-react-components';
 import React from 'react';
+import { useNavigate } from 'react-router';
+import { routes } from '../../../types/generic/routes';
 
 type Props = {};
 
 function LloydGeorgeRetryUploadStage({}: Props) {
-    return <div>Retry upload warning</div>;
+    const navigate = useNavigate();
+    return (
+        <>
+            <WarningCallout>
+                <WarningCallout.Label headingLevel="h2">
+                    The record did not upload
+                </WarningCallout.Label>
+                <p>
+                    <strong>
+                        One or more files failed to upload, which prevented the full record being
+                        uploaded
+                    </strong>
+                </p>
+                <p>
+                    The Lloyd George record was not uploaded for this patient. You will need to
+                    check your files and try again.
+                </p>
+                <p>
+                    Make sure to safely store the full digital or paper Lloyd George record until
+                    it’s completely uploaded to this storage.
+                </p>
+                <p>
+                    Contact the{' '}
+                    <a
+                        href="https://digital.nhs.uk/about-nhs-digital/contact-us#nhs-digital-service-desks"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        National Service Desk
+                    </a>{' '}
+                    if this issue continues.
+                </p>
+            </WarningCallout>
+            <div>
+                <Button type="button" id="upload-retry-button">
+                    Try upload again
+                </Button>
+                <ButtonLink
+                    className="nhsuk-button nhsuk-button--secondary"
+                    data-testid="search-patient-btn"
+                    style={{ marginLeft: 18 }}
+                    role="button"
+                    onClick={() => {
+                        navigate(routes.SEARCH_PATIENT);
+                    }}
+                >
+                    Search for a patient
+                </ButtonLink>
+            </div>
+        </>
+    );
 }
 
 export default LloydGeorgeRetryUploadStage;
