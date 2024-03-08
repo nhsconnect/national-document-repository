c4a86feeb2911a1073d0ef72b086f865f2941091
RachelHowellNHS <rachel.howell6@nhs.net>
2024-03-08 15:16:32 +0000

    [PRMDR-689] add lambda unit tests

--- lambdas/tests/unit/helpers/data/upload_confirm_result.py
+++ lambdas/tests/unit/helpers/data/upload_confirm_result.py
@@ -2,22 +2,38 @@ import json
 
 from tests.unit.conftest import TEST_FILE_KEY, TEST_NHS_NUMBER
 
+MOCK_ARF_DOCUMENTS = {"ARF": [TEST_FILE_KEY, TEST_FILE_KEY]}
+MOCK_LG_DOCUMENTS = {"LG": [TEST_FILE_KEY]}
+MOCK_BOTH_DOCUMENTS = {"ARF": [TEST_FILE_KEY, TEST_FILE_KEY], "LG": [TEST_FILE_KEY]}
+
 MOCK_VALID_LG_EVENT_BODY = {
     "patientId": TEST_NHS_NUMBER,
-    "documents": {"LG": [TEST_FILE_KEY]},
+    "documents": MOCK_LG_DOCUMENTS,
 }
-MOCK_VALID_LG_EVENT = {"body": json.dumps(MOCK_VALID_LG_EVENT_BODY)}
+MOCK_VALID_LG_EVENT = {
+    "httpMethod": "POST",
+    "body": json.dumps(MOCK_VALID_LG_EVENT_BODY),
+}
 
 MOCK_VALID_ARF_EVENT_BODY = {
     "patientId": TEST_NHS_NUMBER,
-    "documents": {"ARF": [TEST_FILE_KEY, TEST_FILE_KEY]},
+    "documents": MOCK_ARF_DOCUMENTS,
 }
 MOCK_VALID_ARF_EVENT = {
     "httpMethod": "POST",
     "body": json.dumps(MOCK_VALID_ARF_EVENT_BODY),
 }
 
-MOCK_MISSING_NHS_NUMBER_BODY = {"documents": {"ARF": [TEST_FILE_KEY, TEST_FILE_KEY]}}
+MOCK_VALID_BOTH_DOC_TYPES_EVENT_BODY = {
+    "patientId": TEST_NHS_NUMBER,
+    "documents": MOCK_BOTH_DOCUMENTS,
+}
+MOCK_VALID_BOTH_DOC_TYPES_EVENT = {
+    "httpMethod": "POST",
+    "body": json.dumps(MOCK_VALID_BOTH_DOC_TYPES_EVENT_BODY),
+}
+
+MOCK_MISSING_NHS_NUMBER_BODY = {"documents": MOCK_ARF_DOCUMENTS}
 MOCK_MISSING_NHS_NUMBER_EVENT = {
     "httpMethod": "POST",
     "body": json.dumps(MOCK_MISSING_NHS_NUMBER_BODY),
@@ -25,7 +41,7 @@ MOCK_MISSING_NHS_NUMBER_EVENT = {
 
 MOCK_INVALID_NHS_NUMBER_BODY = {
     "patientId": "900000000040",
-    "documents": {"ARF": [TEST_FILE_KEY, TEST_FILE_KEY]},
+    "documents": MOCK_ARF_DOCUMENTS,
 }
 MOCK_INVALID_NHS_NUMBER_EVENT = {
     "httpMethod": "POST",
@@ -38,4 +54,4 @@ MOCK_MISSING_DOCUMENTS_EVENT = {
     "body": json.dumps(MOCK_MISSING_DOCUMENTS_BODY),
 }
 
-MOCK_MISSING_BODY_EVENT = {"httpMethod": "POST", "body": "test"}
+MOCK_INVALID_BODY_EVENT = {"httpMethod": "POST", "body": "test"}
