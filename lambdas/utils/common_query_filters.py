7edd3f054ce74edd08a7d2b044867dc2a9a7ef88
Joe Fong <joe.fong1@nhs.net>
2024-03-22 15:05:38 +0000

    [PRMDR-743] remove unused filter

--- lambdas/utils/common_query_filters.py
+++ lambdas/utils/common_query_filters.py
@@ -1,4 +1,4 @@
-from enums.dynamo_filter import AttributeOperator, ConditionOperator
+from enums.dynamo_filter import AttributeOperator
 from enums.metadata_field_names import DocumentReferenceMetadataFields
 from utils.dynamo_query_filter_builder import DynamoQueryFilterBuilder
 
@@ -9,21 +9,5 @@ NotDeleted = (
         attr_operator=AttributeOperator.EQUAL,
         filter_value="",
     )
-    .build()
-)
-
-UploadCompleted = (
-    DynamoQueryFilterBuilder()
-    .add_condition(
-        attribute=str(DocumentReferenceMetadataFields.DELETED.value),
-        attr_operator=AttributeOperator.EQUAL,
-        filter_value="",
-    )
-    .add_condition(
-        attribute=str(DocumentReferenceMetadataFields.UPLOADED.value),
-        attr_operator=AttributeOperator.EQUAL,
-        filter_value=True,
-    )
-    .set_combination_operator(ConditionOperator.AND)
     .build()
 )
