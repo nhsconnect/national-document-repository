5dacb80fec90de726e31caeacc5911a4bb256dff
Rio Knightley <rio.knightley2@nhs.net>
2024-04-09 14:10:00 +0100

    Add lg upload steps

--- app/cypress/support/roles.ts
+++ app/cypress/support/roles.ts
@@ -2,6 +2,7 @@ export enum Roles {
     'GP_ADMIN' = 555053928105,
     'GP_CLINICAL' = 555053929106,
     'PCSE' = 555053983103,
+    'GP_ADMIN_BSOL' = 555056255109,
     'SMOKE_GP_CLINICAL_H85686' = 555054036108, // anything prefixed with smoke shouldnt be used for cy.login
     'SMOKE_GP_ADMIN_H85686' = 555054037109,
 }
