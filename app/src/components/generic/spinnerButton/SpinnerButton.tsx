1fc03be80d8f215f7181fb21081009047de300cc
RachelHowellNHS <rachel.howell6@nhs.net>
2024-04-18 11:11:38 +0100

    [PRMDR-792] try output element

--- app/src/components/generic/spinnerButton/SpinnerButton.tsx
+++ app/src/components/generic/spinnerButton/SpinnerButton.tsx
@@ -11,7 +11,7 @@ const SpinnerButton = ({ id, status, disabled }: Props) => {
     return (
         <Button id={id} aria-label={status} className="spinner_button" disabled={disabled}>
             <div className="spinner_button-spinner"></div>
-            <div role="status">{status}</div>
+            <output>{status}</output>
         </Button>
     );
 };
