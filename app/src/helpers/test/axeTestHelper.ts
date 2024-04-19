bfe7195cc6f5033bb1fa202a168f745f5bcec2fc
Joe Fong <joe.fong1@nhs.net>
2024-04-19 12:16:02 +0100

    [PRMDR-809] Add a toggle at axeTestHelpers to turn off all aXe checks

--- app/src/helpers/test/axeTestHelper.ts
+++ app/src/helpers/test/axeTestHelper.ts
@@ -1,11 +1,21 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { configureAxe } from 'jest-axe';
+import { configureAxe, JestAxeConfigureOptions } from 'jest-axe';
 import 'jest-axe/extend-expect';
 
-// to allow setting global ignore rule option if needed.
-export const runAxeTest = configureAxe({
+const axeConfigs: JestAxeConfigureOptions = {
     rules: {
-        // disable landmark rules as we are testing components.
+        // disable landmark rules as we are testing components
         region: { enabled: false },
     },
-});
+};
+
+// TODO: Remove this global suppress options once we complete Scenario 4 of PRMDR-809
+const SUPPRESS_ACCESSIBILITY_TEST = false;
+if (SUPPRESS_ACCESSIBILITY_TEST) {
+    // eslint-disable-next-line no-console
+    console.warn('Accessibility tests are turned off');
+    // @ts-ignore
+    axeConfigs['impactLevels'] = [null];
+}
+
+export const runAxeTest = configureAxe(axeConfigs);
