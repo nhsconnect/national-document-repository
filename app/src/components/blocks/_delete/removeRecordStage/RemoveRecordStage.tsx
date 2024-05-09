6c9dedfef628eff7ba21a27292e7df8ba39f0932
Rio Knightley <rio.knightley2@nhs.net>
2024-05-09 15:56:45 +0100

    Add remove record stage results

--- app/src/components/blocks/_delete/removeRecordStage/RemoveRecordStage.tsx
+++ app/src/components/blocks/_delete/removeRecordStage/RemoveRecordStage.tsx
@@ -1,10 +1,21 @@
-import React, { Dispatch, SetStateAction } from 'react';
+import React, { Dispatch, SetStateAction, useEffect, useRef, useState } from 'react';
 import { LG_RECORD_STAGE } from '../../../../types/blocks/lloydGeorgeStages';
 import useTitle from '../../../../helpers/hooks/useTitle';
 import { BackLink, Button, Table, WarningCallout } from 'nhsuk-react-components';
 import PatientDetails from '../../../generic/patientDetails/PatientDetails';
 import moment, { Moment } from 'moment';
 import LinkButton from '../../../generic/linkButton/LinkButton';
+import { SearchResult } from '../../../../types/generic/searchResult';
+import getDocumentSearchResults from '../../../../helpers/requests/getDocumentSearchResults';
+import usePatient from '../../../../helpers/hooks/usePatient';
+import { SUBMISSION_STATE } from '../../../../types/pages/documentSearchResultsPage/types';
+import useBaseAPIUrl from '../../../../helpers/hooks/useBaseAPIUrl';
+import useBaseAPIHeaders from '../../../../helpers/hooks/useBaseAPIHeaders';
+import { AxiosError } from 'axios';
+import { useNavigate } from 'react-router';
+import { routes } from '../../../../types/generic/routes';
+import { errorToParams } from '../../../../helpers/utils/errorToParams';
+import ServiceError from '../../../layout/serviceErrorBox/ServiceErrorBox';
 
 export type Props = {
     setStage: Dispatch<SetStateAction<LG_RECORD_STAGE>>;
@@ -18,11 +29,64 @@ type DownloadDocument = {
 
 function RemoveRecordStage({ setStage, recordType }: Props) {
     useTitle({ pageTitle: 'Remove record' });
+    const patientDetails = usePatient();
+    const [submissionState, setSubmissionState] = useState(SUBMISSION_STATE.INITIAL);
+
+    const baseUrl = useBaseAPIUrl();
+    const baseHeaders = useBaseAPIHeaders();
+    const nhsNumber: string = patientDetails?.nhsNumber ?? '';
+    const navigate = useNavigate();
+
     const nameTest = 'of4_Lloyd_George_Record_[Jane Smith]_[9000000004]_[22-10-2010]';
+
+    const [searchResults, setSearchResults] = useState<Array<SearchResult>>([]);
+
+    const mounted = useRef(false);
+    useEffect(() => {
+        const onPageLoad = async () => {
+            setSubmissionState(SUBMISSION_STATE.PENDING);
+
+            try {
+                const results = await getDocumentSearchResults({
+                    nhsNumber,
+                    baseUrl,
+                    baseHeaders,
+                });
+                setSearchResults(results ?? []);
+
+                setSubmissionState(SUBMISSION_STATE.SUCCEEDED);
+            } catch (e) {
+                const error = e as AxiosError;
+                if (error.response?.status === 403) {
+                    navigate(routes.SESSION_EXPIRED);
+                } else if (error.response?.status && error.response?.status >= 500) {
+                    navigate(routes.SERVER_ERROR + errorToParams(error));
+                } else {
+                    setSubmissionState(SUBMISSION_STATE.FAILED);
+                }
+            }
+        };
+        if (!mounted.current) {
+            mounted.current = true;
+            void onPageLoad();
+        }
+    }, [
+        patientDetails,
+        searchResults,
+        nhsNumber,
+        setSearchResults,
+        setSubmissionState,
+        navigate,
+        baseUrl,
+        baseHeaders,
+    ]);
+
     const documents = Array.apply(null, Array(4)).map((x, i) => ({
         filename: i + 1 + nameTest,
         uploaded: moment(),
     }));
+
+    const hasDocuments = searchResults.length && patientDetails;
 
     return (
         <>
@@ -46,44 +110,65 @@ function RemoveRecordStage({ setStage, recordType }: Props) {
                 </p>
             </WarningCallout>
             <PatientDetails />
-            {documents && documents.length > 0 && (
-                <Table caption="List of files in record" id="current-documents-table">
-                    <Table.Head>
-                        <Table.Row>
-                            <Table.Cell>Filename</Table.Cell>
-                            <Table.Cell>Upload date</Table.Cell>
-                        </Table.Row>
-                    </Table.Head>
 
-                    <Table.Body>
-                        {documents.map(({ filename, uploaded }: DownloadDocument) => {
-                            return (
-                                <Table.Row key={filename}>
-                                    <Table.Cell>
-                                        <div>{filename}</div>
-                                    </Table.Cell>
-                                    <Table.Cell>{uploaded.format()}</Table.Cell>
+            {submissionState === SUBMISSION_STATE.FAILED && <ServiceError />}
+
+            {submissionState === SUBMISSION_STATE.SUCCEEDED && (
+                <>
+                    {hasDocuments ? (
+                        <Table caption="List of files in record" id="current-documents-table">
+                            <Table.Head>
+                                <Table.Row>
+                                    <Table.Cell>Filename</Table.Cell>
+                                    <Table.Cell>Upload date</Table.Cell>
                                 </Table.Row>
-                            );
-                        })}
-                    </Table.Body>
-                </Table>
+                            </Table.Head>
+
+                            <Table.Body>
+                                {documents.map(({ filename, uploaded }: DownloadDocument) => {
+                                    return (
+                                        <Table.Row key={filename}>
+                                            <Table.Cell>
+                                                <div>{filename}</div>
+                                            </Table.Cell>
+                                            <Table.Cell>{uploaded.format()}</Table.Cell>
+                                        </Table.Row>
+                                    );
+                                })}
+                            </Table.Body>
+                        </Table>
+                    ) : (
+                        <p>
+                            <strong id="no-files-message">
+                                There are no documents available for this patient.
+                            </strong>
+                        </p>
+                    )}
+                </>
             )}
-            <Button
-                onClick={() => {
-                    setStage(LG_RECORD_STAGE.DELETE_ALL);
-                }}
-            >
-                Remove all files
-            </Button>
-            <LinkButton
-                type="button"
-                onClick={() => {
-                    setStage(LG_RECORD_STAGE.RECORD);
-                }}
-            >
-                Start again
-            </LinkButton>
+
+            {hasDocuments && (
+                <Button
+                    onClick={() => {
+                        setStage(LG_RECORD_STAGE.DELETE_ALL);
+                    }}
+                >
+                    Remove all files
+                </Button>
+            )}
+
+            {(submissionState === SUBMISSION_STATE.FAILED ||
+                submissionState === SUBMISSION_STATE.SUCCEEDED) && (
+                <LinkButton
+                    id="start-again-link"
+                    type="button"
+                    onClick={() => {
+                        setStage(LG_RECORD_STAGE.RECORD);
+                    }}
+                >
+                    Start again
+                </LinkButton>
+            )}
         </>
     );
 }
