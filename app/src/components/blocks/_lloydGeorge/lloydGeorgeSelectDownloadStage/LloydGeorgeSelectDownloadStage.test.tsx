65320a3ef17f8c23f6543325829ea8602f501e89
RachelHowellNHS <rachel.howell6@nhs.net>
2024-05-16 16:54:48 +0100

    [PRMDR-803] unit tests - WIP

--- app/src/components/blocks/_lloydGeorge/lloydGeorgeSelectDownloadStage/LloydGeorgeSelectDownloadStage.test.tsx
+++ app/src/components/blocks/_lloydGeorge/lloydGeorgeSelectDownloadStage/LloydGeorgeSelectDownloadStage.test.tsx
@@ -1,12 +1,20 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import React from 'react';
-import { buildPatientDetails } from '../../../../helpers/test/testBuilders';
+import {
+    buildLgSearchResult,
+    buildPatientDetails,
+    buildSearchResult,
+} from '../../../../helpers/test/testBuilders';
 import usePatient from '../../../../helpers/hooks/usePatient';
 import LloydGeorgeSelectDownloadStage from './LloydGeorgeSelectDownloadStage';
 import { getFormattedDate } from '../../../../helpers/utils/formatDate';
+import LloydGeorgeRecordPage from '../../../../pages/lloydGeorgeRecordPage/LloydGeorgeRecordPage';
+import axios from 'axios';
+import { SEARCH_AND_DOWNLOAD_STATE } from '../../../../types/pages/documentSearchResultsPage/types';
 
 jest.mock('../../../../helpers/hooks/usePatient');
 jest.mock('../../../../helpers/hooks/useBaseAPIHeaders');
+jest.mock('axios');
 jest.mock('react-router', () => ({
     useNavigate: () => mockNavigate,
 }));
@@ -16,6 +24,14 @@ const mockSetDownloadStage = jest.fn();
 const mockPatient = buildPatientDetails();
 const mockedUsePatient = usePatient as jest.Mock;
 const mockNavigate = jest.fn();
+const mockSetSearchResults = jest.fn();
+const mockSetSubmissionSearchState = jest.fn();
+const mockAxios = axios as jest.Mocked<typeof axios>;
+const searchResults = [
+    buildSearchResult({ fileName: '1of2_test.pdf', ID: 'test-id-1' }),
+    buildSearchResult({ fileName: '2of2_test.pdf', ID: 'test-id-2' }),
+    buildSearchResult({ fileName: '1of1_test.pdf', ID: 'test-id-3' }),
+];
 
 describe('LloydGeorgeSelectDownloadStage', () => {
     beforeEach(() => {
@@ -53,5 +69,24 @@ describe('LloydGeorgeSelectDownloadStage', () => {
         expect(screen.getByText(`${mockPatient.postalCode}`)).toBeInTheDocument();
 
         expect(screen.getByText('Loading...')).toBeInTheDocument();
+    });
+
+    it('renders initial lg record view with file info when LG record is returned by search', async () => {
+        // mockAxios.get.mockImplementation(() => Promise.resolve());
+        // mockAxios.get.mockReturnValue(Promise.resolve({data: searchResults}));
+        render(
+            <LloydGeorgeSelectDownloadStage
+                setStage={mockSetStage}
+                setDownloadStage={mockSetDownloadStage}
+            />,
+        );
+        await waitFor(async () => {
+            expect(
+                screen.getByText('Download the Lloyd George record for this patient'),
+            ).toBeInTheDocument();
+        });
+
+        // expect(mockSetSearchResults).toBeCalled();
+        // expect(mockSetSubmissionSearchState).toBeCalledWith(SEARCH_AND_DOWNLOAD_STATE.SEARCH_SUCCEEDED);
     });
 });
