36e3866a63f4908c0af06b032044e8f2ca3d5715
NogaNHS <noga.sasson1@nhs.net>
2024-05-23 12:02:21 +0100

    update fun call with new keyword

--- lambdas/services/search_patient_details_service.py
+++ lambdas/services/search_patient_details_service.py
@@ -27,7 +27,9 @@ class SearchPatientDetailsService:
             pds_api_service = get_pds_service()(self.ssm_service)
             patient_details = pds_api_service.fetch_patient_details(nhs_number)
 
-            self.check_if_user_authorise(gp_ods=patient_details.general_practice_ods)
+            self.check_if_user_authorise(
+                gp_ods_for_patient=patient_details.general_practice_ods
+            )
 
             logger.audit_splunk_info(
                 "Searched for patient details", {"Result": "Patient found"}
