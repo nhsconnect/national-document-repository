ab683dde9a1f55f162e621234f17989083abb0ee
Joe Fong <joe.fong1@nhs.net>
2024-05-24 15:44:14 +0100

    [PRMDR-904] refactor

--- app/src/components/blocks/_lloydGeorge/lloydGeorgeSelectSearchResults/LloydGeorgeSelectSearchResults.tsx
+++ app/src/components/blocks/_lloydGeorge/lloydGeorgeSelectSearchResults/LloydGeorgeSelectSearchResults.tsx
@@ -3,7 +3,7 @@ import { SearchResult } from '../../../../types/generic/searchResult';
 import { getFormattedDatetime } from '../../../../helpers/utils/formatDatetime';
 import { Link, useNavigate } from 'react-router-dom';
 import { routeChildren, routes } from '../../../../types/generic/routes';
-import React, { Dispatch, SetStateAction, SyntheticEvent, useState } from 'react';
+import React, { Dispatch, ReactNode, SetStateAction, SyntheticEvent, useState } from 'react';
 import { SEARCH_AND_DOWNLOAD_STATE } from '../../../../types/pages/documentSearchResultsPage/types';
 import ErrorBox from '../../../layout/errorBox/ErrorBox';
 import PatientSummary from '../../../generic/patientSummary/PatientSummary';
@@ -14,6 +14,87 @@ export type Props = {
     setSubmissionSearchState: Dispatch<SetStateAction<SEARCH_AND_DOWNLOAD_STATE>>;
     setSelectedDocuments: Dispatch<React.SetStateAction<Array<string>>>;
     selectedDocuments: Array<string>;
+};
+
+type AvailableFilesTableProps = {
+    tableCaption: ReactNode;
+    searchResults: Array<SearchResult>;
+    setSelectedDocuments: Dispatch<React.SetStateAction<Array<string>>>;
+    selectedDocuments: Array<string>;
+    allowSelectDocument: boolean;
+};
+
+const AvailableFilesTable = ({
+    tableCaption,
+    searchResults,
+    selectedDocuments,
+    setSelectedDocuments,
+    allowSelectDocument,
+}: AvailableFilesTableProps) => {
+    const handleChangeCheckboxes = (e: SyntheticEvent<HTMLInputElement>) => {
+        const target = e.target as HTMLInputElement;
+        const toggledDocumentId = target.value;
+        if (target.checked) {
+            setSelectedDocuments([...selectedDocuments, toggledDocumentId]);
+        } else {
+            setSelectedDocuments(selectedDocuments.filter((id) => id !== toggledDocumentId));
+        }
+    };
+    return (
+        <Table
+            id="available-files-table-title"
+            data-testid="available-files-table-title"
+            caption={tableCaption}
+        >
+            <Table.Head>
+                <Table.Row>
+                    {allowSelectDocument && (
+                        <Table.Cell className={'table-column-header'}>Selected</Table.Cell>
+                    )}
+                    <Table.Cell className={'table-column-header'}>Filename</Table.Cell>
+                    <Table.Cell className={'table-column-header'}>Upload date</Table.Cell>
+                </Table.Row>
+            </Table.Head>
+            <Table.Body>
+                {searchResults.map((result, index) => (
+                    <Table.Row
+                        className="available-files-row"
+                        id={`search-result-${index}`}
+                        key={`document-${result.fileName + result.created}`}
+                        data-testid={`search-result-${index}`}
+                    >
+                        {allowSelectDocument && (
+                            <Table.Cell id={`selected-files-row-${index}`}>
+                                <Checkboxes onChange={handleChangeCheckboxes}>
+                                    <Checkboxes.Box
+                                        value={result.ID}
+                                        data-testid={`checkbox-${index}`}
+                                        checked={selectedDocuments.includes(result.ID)}
+                                    >
+                                        <span className="nhsuk-u-visually-hidden">
+                                            {result.fileName}
+                                        </span>
+                                    </Checkboxes.Box>
+                                </Checkboxes>
+                            </Table.Cell>
+                        )}
+                        <Table.Cell
+                            id={'available-files-row-' + index + '-filename'}
+                            data-testid="filename"
+                        >
+                            {result.fileName}
+                        </Table.Cell>
+                        <Table.Cell
+                            id={'available-files-row-' + index + '-created-date'}
+                            data-testid="created"
+                        >
+                            {getFormattedDatetime(new Date(result.created))}
+                        </Table.Cell>
+                    </Table.Row>
+                ))}
+            </Table.Body>
+        </Table>
+    );
 };
 
 const LloydGeorgeSelectSearchResults = ({
@@ -38,16 +119,8 @@ const LloydGeorgeSelectSearchResults = ({
     const noOptionSelectedError = 'You must select a file to download or download all files';
     const pageHeader = 'Download the Lloyd George record for this patient';
 
-    const allowSelectFile = searchResults.length > 1;
+    const allowSelectDocument = searchResults.length > 1;
 
-    const handleChangeCheckboxes = (e: SyntheticEvent<HTMLInputElement>) => {
-        const target = e.target as HTMLInputElement;
-        if (target.checked) {
-            setSelectedDocuments([...selectedDocuments, target.value]);
-        } else {
-            setSelectedDocuments(selectedDocuments.filter((e) => e !== target.value));
-        }
-    };
     const handleClickSelectedDownload = () => {
         if (selectedDocuments.length === searchResults.length) {
             handleClickDownloadAll();
@@ -79,62 +152,15 @@ const LloydGeorgeSelectSearchResults = ({
             )}
             <h1 id="download-page-title">{pageHeader}</h1>
             <PatientSummary />
-
-            <Table
-                id="available-files-table-title"
-                data-testid="available-files-table-title"
-                caption={tableCaption}
-            >
-                <Table.Head>
-                    <Table.Row>
-                        {allowSelectFile && (
-                            <Table.Cell className={'table-column-header'}>Selected</Table.Cell>
-                        )}
-                        <Table.Cell className={'table-column-header'}>Filename</Table.Cell>
-                        <Table.Cell className={'table-column-header'}>Upload date</Table.Cell>
-                    </Table.Row>
-                </Table.Head>
-                <Table.Body>
-                    {orderedResults.map((result, index) => (
-                        <Table.Row
-                            className={'available-files-row'}
-                            id={`search-result-${index}`}
-                            key={`document-${result.fileName + result.created}`}
-                            data-testid={`search-result-${index}`}
-                        >
-                            {allowSelectFile && (
-                                <Table.Cell id={'selected-files-row-' + index + ''}>
-                                    <Checkboxes onChange={handleChangeCheckboxes}>
-                                        <Checkboxes.Box
-                                            value={result.ID}
-                                            data-testid={`checkbox-${index}`}
-                                            checked={selectedDocuments.includes(result.ID)}
-                                        >
-                                            <span className="nhsuk-u-visually-hidden">
-                                                {result.fileName}
-                                            </span>
-                                        </Checkboxes.Box>
-                                    </Checkboxes>{' '}
-                                </Table.Cell>
-                            )}
-                            <Table.Cell
-                                id={'available-files-row-' + index + '-filename'}
-                                data-testid="filename"
-                            >
-                                {result.fileName}
-                            </Table.Cell>
-                            <Table.Cell
-                                id={'available-files-row-' + index + '-created-date'}
-                                data-testid="created"
-                            >
-                                {getFormattedDatetime(new Date(result.created))}
-                            </Table.Cell>
-                        </Table.Row>
-                    ))}
-                </Table.Body>
-            </Table>
+            <AvailableFilesTable
+                tableCaption={tableCaption}
+                searchResults={orderedResults}
+                selectedDocuments={selectedDocuments}
+                setSelectedDocuments={setSelectedDocuments}
+                allowSelectDocument={allowSelectDocument}
+            />
             <div className="align-baseline gap-4">
-                {allowSelectFile && (
+                {allowSelectDocument && (
                     <Button
                         onClick={handleClickSelectedDownload}
                         data-testid="download-selected-files-btn"
@@ -144,7 +170,7 @@ const LloydGeorgeSelectSearchResults = ({
                 )}
                 <Button
                     onClick={handleClickDownloadAll}
-                    secondary={allowSelectFile}
+                    secondary={allowSelectDocument}
                     data-testid="download-all-files-btn"
                 >
                     Download all files
