7c6ea3f51535afe6e2683641ce05f21bee0e33c0
Joe Fong <joe.fong1@nhs.net>
2024-06-05 14:09:37 +0100

    [PRMDR-919] Add unit test for data_collection_handler

--- lambdas/handlers/data_collection_handler.py
+++ lambdas/handlers/data_collection_handler.py
@@ -1,23 +1,23 @@
 from services.data_collection_service import DataCollectionService
 from utils.audit_logging_setup import LoggingService
-from utils.decorators.ensure_env_var import ensure_environment_variables
-from utils.decorators.handle_lambda_exceptions import handle_lambda_exceptions
+from utils.decorators.ensure_env_var import ensure_environment_variables_for_non_webapi
 from utils.decorators.override_error_check import override_error_check
 
 logger = LoggingService(__name__)
 
 
-@ensure_environment_variables(
+@ensure_environment_variables_for_non_webapi(
     names=[
         "LLOYD_GEORGE_DYNAMODB_NAME",
         "LLOYD_GEORGE_BUCKET_NAME",
+        "DOCUMENT_STORE_DYNAMODB_NAME",
+        "DOCUMENT_STORE_BUCKET_NAME",
         "WORKSPACE",
         "STATISTICS_TABLE",
     ]
 )
 @override_error_check
-@handle_lambda_exceptions
-def lambda_handler(event, context):
+def lambda_handler(_event, _context):
     logger.info("Starting data collection process")
     service = DataCollectionService()
     service.collect_all_data_and_write_to_dynamodb()
--- lambdas/utils/decorators/ensure_env_var.py
+++ lambdas/utils/decorators/ensure_env_var.py
@@ -3,6 +3,7 @@ from typing import Callable
 
 from enums.lambda_error import LambdaError
 from utils.audit_logging_setup import LoggingService
+from utils.exceptions import MissingEnvVarException
 from utils.lambda_response import ApiGatewayResponse
 
 logger = LoggingService(__name__)
@@ -30,6 +31,37 @@ def ensure_environment_variables(names: list[str]) -> Callable:
                     return ApiGatewayResponse(
                         500, error_body, event["httpMethod"]
                     ).create_api_gateway_response()
+
+            # Validation done. Return control flow to original lambda handler
+            return lambda_func(event, context)
+
+        return interceptor
+
+    return wrapper
+
+
+def ensure_environment_variables_for_non_webapi(names: list[str]) -> Callable:
+    """A decorator for lambda handler.
+    Verify that the lambda environment got a set of specific environment variables.
+    If not, log and throw an error.
+    Use for lambdas that are NOT supposed to be integrated with API Gateway.
+
+    Usage:
+    @ensure_environment_variables(names=["LLOYD_GEORGE_BUCKET_NAME", "LLOYD_GEORGE_DYNAMODB_NAME"])
+    def lambda_handler(event, context):
+        ...
+    """
+
+    def wrapper(lambda_func: Callable):
+        def interceptor(event, context):
+            missing_env_vars = set(names) - set(os.environ)
+            if missing_env_vars:
+                missing_env_vars_in_string = ", ".join(sorted(missing_env_vars))
+                error_body = LambdaError.EnvMissing.create_error_body(
+                    {"name": missing_env_vars_in_string}
+                )
+                logger.error(error_body, {"Result": "Failed to run lambda"})
+                raise MissingEnvVarException(error_body)
 
             # Validation done. Return control flow to original lambda handler
             return lambda_func(event, context)
