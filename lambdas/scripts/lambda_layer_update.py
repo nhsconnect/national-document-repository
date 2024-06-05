b987ba36857c8ec62d281fe6623ba59f3dc26b1d
abbas-khan10 <127417949+abbas-khan10@users.noreply.github.com>
2024-06-05 16:20:58 +0100

    PRMDR-879: Implement lambda layers into workflow (#370)
    * Add girhub workflow for lambda layers and script to update layers
    
    * reenable all lambdas
    
    * fix script to remove unwanted layers on update
    
    * update cryptography
    
    * update cryptography to 42.0.4
    
    * pr changes
    
    * remove old requirement files
    
    * fix python version warning
    
    * update pre-prod and prod with layers, and update nod version
    
    * PR changes
    
    * enable bsol tmp
    
    * re-enable bsol
    
    * sort requirements
    
    * change input from environment to sandbox
    
    * fix message

---
+++ lambdas/scripts/lambda_layer_update.py
@@ -0,0 +1,64 @@
+import re
+import sys
+
+import boto3
+
+
+class LambdaLayerUpdate:
+    def __init__(self):
+        self.client = boto3.client("lambda")
+
+        self.sandbox = sys.argv[1]
+        self.function_name_aws = f"{self.sandbox}_{sys.argv[2]}"
+        layers = sys.argv[3].strip().split(",")
+
+        self.updated_lambda_arns = {}
+        self.updated_layer_names = []
+
+        print(f"Function Name: {self.function_name_aws}")
+        print("Layers to add:")
+        for layer in layers:
+            updated_layer_name = f"{self.sandbox}_{layer}"
+            self.updated_layer_names.append(updated_layer_name)
+            print(f"- {updated_layer_name}")
+        print("")
+
+    def start(self):
+        self.extract_default_layer_arns()
+        self.extract_updated_layer_arns()
+        self.update_lambda()
+
+    def extract_default_layer_arns(self):
+        response = self.client.get_function(FunctionName=self.function_name_aws)
+        layer_responses = response["Configuration"]["Layers"]
+
+        for layer_response in layer_responses:
+            layer_arn = layer_response["Arn"]
+            layer_name_match = re.search(r"layer:([^:]+):", layer_arn)
+            if layer_name_match:
+                layer_name = layer_name_match.group(1)
+
+                if layer_name in ["AWS-AppConfig-Extension", "LambdaInsightsExtension"]:
+                    self.updated_lambda_arns.update({layer_name: layer_arn})
+
+    def extract_updated_layer_arns(self):
+        for updated_layer_name in self.updated_layer_names:
+            print(f"Extracting latest version ARN for {updated_layer_name}...")
+            response = self.client.list_layer_versions(LayerName=updated_layer_name)
+            latest_layer_version = response["LayerVersions"][0]
+            layer_arn = latest_layer_version["LayerVersionArn"]
+
+            self.updated_lambda_arns[updated_layer_name] = layer_arn
+
+    def update_lambda(self):
+        print(f"Updating {self.function_name_aws} with new layer ARNs...")
+        self.client.update_function_configuration(
+            FunctionName=self.function_name_aws,
+            Layers=list(self.updated_lambda_arns.values()),
+        )
+
+
+if __name__ == "__main__":
+    layer_update = LambdaLayerUpdate()
+    layer_update.start()
+    print("\nUpdate Process Complete.")
