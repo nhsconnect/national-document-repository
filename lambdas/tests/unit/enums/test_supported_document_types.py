422fa707a79a0d185398e38708f794230ccc3ba7
Joe Fong <joe.fong1@nhs.net>
2024-06-10 11:52:37 +0100

    improve test coverage

--- lambdas/tests/unit/enums/test_supported_document_types.py
+++ lambdas/tests/unit/enums/test_supported_document_types.py
@@ -1,6 +1,11 @@
 import pytest
 from enums.supported_document_types import SupportedDocumentTypes
-from tests.unit.conftest import MOCK_ARF_TABLE_NAME, MOCK_LG_TABLE_NAME
+from tests.unit.conftest import (
+    MOCK_ARF_BUCKET,
+    MOCK_ARF_TABLE_NAME,
+    MOCK_LG_BUCKET,
+    MOCK_LG_TABLE_NAME,
+)
 
 
 @pytest.mark.parametrize(
@@ -13,5 +18,19 @@ from tests.unit.conftest import MOCK_ARF_TABLE_NAME, MOCK_LG_TABLE_NAME
 def test_get_dynamodb_table_name_return_table_name(set_env, doc_type, expected):
     doc_type_enum = SupportedDocumentTypes(doc_type)
     actual = doc_type_enum.get_dynamodb_table_name()
+
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    ["doc_type", "expected"],
+    [
+        (SupportedDocumentTypes.ARF, MOCK_ARF_BUCKET),
+        (SupportedDocumentTypes.LG, MOCK_LG_BUCKET),
+    ],
+)
+def test_get_s3_bucket_name_return_bucket_name(set_env, doc_type, expected):
+    doc_type_enum = SupportedDocumentTypes(doc_type)
+    actual = doc_type_enum.get_s3_bucket_name()
 
     assert actual == expected
