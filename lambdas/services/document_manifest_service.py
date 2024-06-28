1441b86e3a77d43cef48b17178e0b97efdfc012f
NogaNHS <noga.sasson1@nhs.net>
2024-06-28 17:03:00 +0100

    [PRMDR-537] refactoring manifest service

--- lambdas/services/document_manifest_service.py
+++ lambdas/services/document_manifest_service.py
@@ -4,9 +4,7 @@ import tempfile
 import zipfile
 
 from botocore.exceptions import ClientError
-from enums.dynamo_filter import AttributeOperator, ConditionOperator
 from enums.lambda_error import LambdaError
-from enums.metadata_field_names import DocumentReferenceMetadataFields
 from enums.supported_document_types import SupportedDocumentTypes
 from models.document_reference import DocumentReference
 from models.zip_trace import ZipTrace
@@ -16,13 +14,9 @@ from services.base.s3_service import S3Service
 from services.document_service import DocumentService
 from utils.audit_logging_setup import LoggingService
 from utils.common_query_filters import UploadCompleted
-from utils.dynamo_query_filter_builder import DynamoQueryFilterBuilder
 from utils.exceptions import DynamoServiceException
 from utils.lambda_exceptions import DocumentManifestServiceException
-from utils.lloyd_george_validator import (
-    LGInvalidFilesException,
-    check_for_number_of_files_match_expected,
-)
+from utils.lloyd_george_validator import LGInvalidFilesException
 
 logger = LoggingService(__name__)
 
@@ -40,6 +34,8 @@ class DocumentManifestService:
         self.temp_output_dir = tempfile.mkdtemp()
         self.zip_output_bucket = os.environ["ZIPPED_STORE_BUCKET_NAME"]
         self.zip_trace_table = os.environ["ZIPPED_STORE_DYNAMODB_NAME"]
+        self.zip_file_path = os.path.join(self.temp_output_dir, self.zip_file_name)
+        self.file_names_to_be_zipped = {}
 
     def create_document_manifest_presigned_url(
         self,
@@ -58,12 +54,6 @@ class DocumentManifestService:
                 raise DocumentManifestServiceException(
                     status_code=404, error=LambdaError.ManifestNoDocs
                 )
-            self.download_documents_to_be_zipped(documents)
-            self.upload_zip_file()
-
-            # Removes the parent of each removed directory until the parent does not exist or the parent is not empty
-            shutil.rmtree(self.temp_downloads_dir)
-            shutil.rmtree(self.temp_output_dir)
 
             return self.s3_service.create_download_presigned_url(
                 s3_bucket_name=self.zip_output_bucket, file_key=self.zip_file_name
@@ -94,6 +84,12 @@ class DocumentManifestService:
                 status_code=400, error=LambdaError.IncompleteRecordError
             )
 
+    def handle_zip_request(self, job_id: str):
+        documents = self.get_document_locations_list_from_dynamo(job_id)
+        self.download_documents_to_be_zipped(documents)
+        self.upload_zip_file()
+        self.remove_temp_files()
+
     def arrange_documents_for_download(
         self,
         doc_types: list[SupportedDocumentTypes],
@@ -102,38 +98,16 @@ class DocumentManifestService:
         documents = []
         query_filter = UploadCompleted
 
-        if document_references:
-            query_filter = (
-                query_filter
-                & self.create_filter_expression_for_document_references(
-                    document_references
-                )
-            )
-
         for doc_type in doc_types:
             documents_for_doc_type = self.retrieve_document_metadata_from_dynamo(
                 doc_type, query_filter
             )
-            if documents_for_doc_type and doc_type == SupportedDocumentTypes.LG:
-                check_for_number_of_files_match_expected(
-                    documents_for_doc_type[0].file_name, len(documents_for_doc_type)
-                )
-            documents += documents_for_doc_type
-        return documents
 
-    def create_filter_expression_for_document_references(self, document_references):
-        dynamo_filter_document_by_references = (
-            DynamoQueryFilterBuilder().set_combination_operator(
-                operator=ConditionOperator.OR
-            )
-        )
-        for document_reference in document_references:
-            dynamo_filter_document_by_references.add_condition(
-                attribute=str(DocumentReferenceMetadataFields.ID.value),
-                attr_operator=AttributeOperator.EQUAL,
-                filter_value=document_reference,
-            )
-        return dynamo_filter_document_by_references.build()
+            for document in documents_for_doc_type:
+                if document.id in document_references:
+                    documents += document
+
+        return documents
 
     def retrieve_document_metadata_from_dynamo(self, doc_type, query_filter):
         return self.document_service.fetch_available_document_references_by_type(
@@ -144,59 +118,68 @@ class DocumentManifestService:
 
     def download_documents_to_be_zipped(self, documents: list[DocumentReference]):
         logger.info("Downloading documents to be zipped")
-        file_names_to_be_zipped = {}
 
         for document in documents:
-            file_name = document.file_name
+            self.handle_duplication_in_filename(document)
+            self.download_file(document)
 
-            duplicated_filename = file_name in file_names_to_be_zipped
+    def download_file(self, document):
+        download_path = os.path.join(self.temp_downloads_dir, document.file_name)
 
-            if duplicated_filename:
-                file_names_to_be_zipped[file_name] += 1
-                document.file_name = document.create_unique_filename(
-                    file_names_to_be_zipped[file_name]
-                )
+        try:
+            self.s3_service.download_file(
+                document.get_file_bucket(), document.get_file_key(), download_path
+            )
+        except ClientError as e:
+            msg = f"{document.get_file_key()} may reference missing file in s3 bucket: {document.get_file_bucket()}"
+            logger.error(
+                f"{LambdaError.ManifestClient.to_str()} {msg + str(e)}",
+                {"Result": "Failed to create document manifest"},
+            )
+            raise DocumentManifestServiceException(
+                status_code=500, error=LambdaError.ManifestClient
+            )
 
-            else:
-                file_names_to_be_zipped[file_name] = 1
+    def handle_duplication_in_filename(self, document):
+        duplicated_filename = document.file_name in self.file_names_to_be_zipped
 
-            download_path = os.path.join(self.temp_downloads_dir, document.file_name)
+        if duplicated_filename:
+            self.file_names_to_be_zipped[document.file_name] += 1
+            document.file_name = document.create_unique_filename(
+                self.file_names_to_be_zipped[document.file_name]
+            )
 
-            try:
-                self.s3_service.download_file(
-                    document.get_file_bucket(), document.get_file_key(), download_path
-                )
-            except ClientError as e:
-                msg = f"{document.get_file_key()} may reference missing file in s3 bucket: {document.get_file_bucket()}"
-                logger.error(
-                    f"{LambdaError.ManifestClient.to_str()} {msg + str(e)}",
-                    {"Result": "Failed to create document manifest"},
-                )
-                raise DocumentManifestServiceException(
-                    status_code=500, error=LambdaError.ManifestClient
-                )
+        else:
+            self.file_names_to_be_zipped[document.file_name] = 1
 
     def upload_zip_file(self):
-        logger.info("Creating zip from files")
+        logger.info("Uploading zip file to s3")
+        self.s3_service.upload_file(
+            file_name=self.zip_file_path,
+            s3_bucket_name=self.zip_output_bucket,
+            file_key=f"{self.zip_file_name}",
+        )
 
-        zip_file_path = os.path.join(self.temp_output_dir, self.zip_file_name)
-        with zipfile.ZipFile(zip_file_path, "w", zipfile.ZIP_DEFLATED) as zipf:
+    def zip_file(self):
+        logger.info("Creating zip from files")
+        with zipfile.ZipFile(self.zip_file_path, "w", zipfile.ZIP_DEFLATED) as zipf:
             for root, _, files in os.walk(self.temp_downloads_dir):
                 for file in files:
                     file_path = os.path.join(root, file)
                     arc_name = os.path.relpath(file_path, self.temp_downloads_dir)
                     zipf.write(file_path, arc_name)
 
-        logger.info("Uploading zip file to s3")
-        self.s3_service.upload_file(
-            file_name=zip_file_path,
-            s3_bucket_name=self.zip_output_bucket,
-            file_key=f"{self.zip_file_name}",
-        )
-
+    def update_dynamo_with_zip_location(self):
         logger.info("Writing zip trace to db")
         zip_trace = ZipTrace(
             f"s3://{self.zip_output_bucket}/{self.zip_file_name}",
         )
-
         self.dynamo_service.create_item(self.zip_trace_table, zip_trace.to_dict())
+
+    def remove_temp_files(self):
+        # Removes the parent of each removed directory until the parent does not exist or the parent is not empty
+        shutil.rmtree(self.temp_downloads_dir)
+        shutil.rmtree(self.temp_output_dir)
+
+    def get_document_locations_list_from_dynamo(self, job_id):
+        pass
