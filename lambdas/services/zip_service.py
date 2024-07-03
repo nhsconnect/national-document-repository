136ba83f8bbcbd08a0a1c5b829446f7d8ac9328d
NogaNHS <noga.sasson1@nhs.net>
2024-07-03 12:58:33 +0100

    [PRMDR-537] add update_processing_status

--- lambdas/services/zip_service.py
+++ lambdas/services/zip_service.py
@@ -5,6 +5,7 @@ import zipfile
 
 from botocore.exceptions import ClientError
 from enums.lambda_error import LambdaError
+from enums.zip_trace import ZipTraceStatus
 from services.base.dynamo_service import DynamoDBService
 from services.base.s3_service import S3Service
 from utils.audit_logging_setup import LoggingService
@@ -27,11 +28,12 @@ class DocumentZipService:
         self.zip_file_path = os.path.join(self.temp_output_dir, self.zip_file_name)
 
     def handle_zip_request(self):
+        self.update_processing_status()
         self.download_documents_to_be_zipped()
         self.zip_files()
         self.upload_zip_file()
         self.remove_temp_files()
-        self.update_dynamo_with_zip_location()
+        self.update_dynamo_with_fields({"status", "zip_file_location"})
 
     def download_documents_to_be_zipped(self):
         logger.info("Downloading documents to be zipped")
@@ -45,6 +47,7 @@ class DocumentZipService:
         try:
             self.s3_service.download_file(file_bucket, file_key, download_path)
         except ClientError as e:
+            self.update_failed_status()
             msg = f"{file_key} may reference missing file in s3 bucket: {file_bucket}"
             logger.error(
                 f"{LambdaError.ZipServiceClientError.to_str()} {msg + str(e)}",
@@ -54,18 +57,21 @@ class DocumentZipService:
                 status_code=500, error=LambdaError.ZipServiceClientError
             )
 
-    @staticmethod
-    def get_file_bucket_and_key(file_location):
+    def get_file_bucket_and_key(self, file_location):
         try:
             file_bucket, file_key = file_location.replace("s3://", "").split("/", 1)
             return file_bucket, file_key
         except ValueError:
+            self.update_failed_status()
             raise InvalidDocumentReferenceException(
                 "Failed to parse bucket from file location string"
             )
 
     def upload_zip_file(self):
         logger.info("Uploading zip file to s3")
+        self.zip_trace_object.zip_file_location = (
+            f"s3://{self.zip_output_bucket}/{self.zip_file_name}"
+        )
 
         try:
             self.s3_service.upload_file(
@@ -74,10 +80,13 @@ class DocumentZipService:
                 file_key=f"{self.zip_file_name}",
             )
         except ClientError as e:
+            self.update_failed_status()
             logger.error(e, {"Result": "Failed to create document manifest"})
             raise DocumentManifestServiceException(
                 status_code=500, error=LambdaError.ZipServiceClientError
             )
+
+        self.zip_trace_object.status = ZipTraceStatus.COMPLETED.value
 
     def zip_files(self):
         logger.info("Creating zip from files")
@@ -88,19 +97,21 @@ class DocumentZipService:
                     arc_name = os.path.relpath(file_path, self.temp_downloads_dir)
                     zipf.write(file_path, arc_name)
 
-    def update_dynamo_with_zip_location(self):
+    def update_dynamo_with_fields(self, fields: set):
         logger.info("Writing zip trace to db")
-        self.zip_trace_object.zip_file_location = (
-            f"s3://{self.zip_output_bucket}/{self.zip_file_name}",
-        )
-        self.zip_trace_object.status = "Complete"
         self.dynamo_service.update_item(
             self.zip_trace_table,
             self.zip_trace_object.id,
-            self.zip_trace_object.model_dump(
-                by_alias=True, include={"created", "status", "zip_file_location"}
-            ),
+            self.zip_trace_object.model_dump(by_alias=True, include=fields),
         )
+
+    def update_processing_status(self):
+        self.zip_trace_object.status = ZipTraceStatus.PROCESSING.value
+        self.update_dynamo_with_fields({"status"})
+
+    def update_failed_status(self):
+        self.zip_trace_object.status = ZipTraceStatus.FAILED.value
+        self.update_dynamo_with_fields({"status"})
 
     def remove_temp_files(self):
         # Removes the parent of each removed directory until the parent does not exist or the parent is not empty
