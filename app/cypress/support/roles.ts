2e9b8641267eb3b32a1ac21563724efe408467ad
bethany-kish-nhs <beth.kish1@nhs.net>
2024-08-05 15:33:21 +0100

    revert roles update

--- app/cypress/support/roles.ts
+++ app/cypress/support/roles.ts
@@ -1,10 +1,10 @@
 export enum Roles {
-    'GP_ADMIN' = 555053913108,
-    'GP_CLINICAL' = 555053912107,
-    'PCSE' = 555053975103,
-    'GP_ADMIN_BSOL' = 555056245106,
-    'SMOKE_GP_CLINICAL_H85686' = 555054025105, // anything prefixed with smoke shouldnt be used for cy.login
-    'SMOKE_GP_ADMIN_H85686' = 555054024104,
+    'GP_ADMIN' = 555053928105,
+    'GP_CLINICAL' = 555053929106,
+    'PCSE' = 555053983103,
+    'GP_ADMIN_BSOL' = 555056255109,
+    'SMOKE_GP_CLINICAL_H85686' = 555054036108, // anything prefixed with smoke shouldnt be used for cy.login
+    'SMOKE_GP_ADMIN_H85686' = 555054037109,
 }
 
 export const roleIds = Object.values(Roles) as Array<Roles>;
