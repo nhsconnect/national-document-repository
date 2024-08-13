403c90750b07a9342e345457718e329a170f4ba7
abid-nhs <abdur.abid1@nhs.net>
2024-08-13 10:22:14 +0100

    Prmp 791 - Content change to unauthorised access page  (#408)
    * [PRMP-791] - write tests for the content of the page
    
    * [PRMP-791] - add new content to unautherised error page
    
    * [PRMP-791] - remove unused import

--- app/src/pages/unauthorisedLoginPage/UnauthorisedLoginPage.tsx
+++ app/src/pages/unauthorisedLoginPage/UnauthorisedLoginPage.tsx
@@ -2,7 +2,6 @@ import { routes } from '../../types/generic/routes';
 import { useNavigate } from 'react-router-dom';
 import { ButtonLink } from 'nhsuk-react-components';
 import React from 'react';
-import ServiceDeskLink from '../../components/generic/serviceDeskLink/ServiceDeskLink';
 import useTitle from '../../helpers/hooks/useTitle';
 
 const UnauthorisedLoginPage = () => {
@@ -19,21 +18,23 @@ const UnauthorisedLoginPage = () => {
             <h2>Who can access this service</h2>
             <p>
                 In order to keep patient information safe, only authorised accounts can access this
-                service.
+                service
             </p>
-            <p> This includes:</p>
+            <p>This includes:</p>
             <ul>
-                <li>GP practice staff involved in the pilot scheme of this service</li>
                 <li>
-                    PCSE staff to search or download patient records where there has been an access
-                    request
+                    GP practice staff who work at the practice the patient is registered with who
+                    have one of these roles on their smart cards:
+                    <br />
+                    <br />
+                    <p>GP Admin Role: R8010, R8013, R1790, R8008</p>
+                    <p>GP Clinical Role: R8000</p>
                 </li>
+                <li>PCSE staff where a patient does not have an active registration</li>
             </ul>
             <p>
-                If you think you should have access to this service contact the <ServiceDeskLink />
-                {/*
-                 */}
-                .
+                If you don't have access and feel you should have, please contact your local
+                Registration Authority
             </p>
             <ButtonLink
                 href="#"
