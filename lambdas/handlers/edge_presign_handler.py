9aeed586bec7e8f9a68465c2aee57c0c566c815c
Rio Knightley <rio.knightley2@nhs.net>
2024-08-15 14:53:50 +0100

    Remove auth logging from handler

--- lambdas/handlers/edge_presign_handler.py
+++ lambdas/handlers/edge_presign_handler.py
@@ -4,13 +4,11 @@ from enums.logging_app_interaction import LoggingAppInteraction
 from utils.audit_logging_setup import LoggingService
 from utils.decorators.handle_lambda_exceptions import handle_lambda_exceptions
 from utils.decorators.override_error_check import override_error_check
-from utils.decorators.set_audit_arg import set_request_context_for_logging
 from utils.request_context import request_context
 
 logger = LoggingService(__name__)
 
 
-@set_request_context_for_logging
 @override_error_check
 @handle_lambda_exceptions
 def lambda_handler(event, context):
