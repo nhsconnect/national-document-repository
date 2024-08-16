fef3f907b46cf3e17373a752c1b4d1294b1ad828
Rio Knightley <rio.knightley2@nhs.net>
2024-08-16 15:47:02 +0100

    Add try except clause to lambda layer configuration on default

--- lambdas/scripts/lambda_layer_update.py
+++ lambdas/scripts/lambda_layer_update.py
@@ -32,18 +32,22 @@ class LambdaLayerUpdate:
     def extract_default_layer_arns(self):
         response = self.client.get_function(FunctionName=self.function_name_aws)
         print("JSON RESPONSE: " + json.dumps(response))
-        layer_responses = response["Configuration"]["Layers"]
-        for layer_response in layer_responses:
-            layer_arn = layer_response["Arn"]
-            layer_name_match = re.search(r"layer:([^:]+):", layer_arn)
-            if layer_name_match:
-                layer_name = layer_name_match.group(1)
+        try:
+            layer_responses = response["Configuration"]["Layers"]
+            for layer_response in layer_responses:
+                layer_arn = layer_response["Arn"]
+                layer_name_match = re.search(r"layer:([^:]+):", layer_arn)
+                if layer_name_match:
+                    layer_name = layer_name_match.group(1)
 
-                if layer_name in [
-                    "AWS-AppConfig-Extension",
-                    "LambdaInsightsExtension",
-                ]:
-                    self.updated_lambda_arns.update({layer_name: layer_arn})
+                    if layer_name in [
+                        "AWS-AppConfig-Extension",
+                        "LambdaInsightsExtension",
+                    ]:
+                        self.updated_lambda_arns.update({layer_name: layer_arn})
+        except KeyError:
+            print("Lambda layer default configuration not found")
+            return
 
     def extract_updated_layer_arns(self):
         for updated_layer_name in self.updated_layer_names:
