56d700399439cdabb144eb35a0344d715af6ac82
Rio Knightley <rio.knightley2@nhs.net>
2024-08-19 10:18:44 +0100

    Fix JSON lambda error

--- lambdas/handlers/edge_presign_handler.py
+++ lambdas/handlers/edge_presign_handler.py
@@ -4,6 +4,7 @@ from enums.logging_app_interaction import LoggingAppInteraction
 from utils.audit_logging_setup import LoggingService
 from utils.decorators.handle_lambda_exceptions import handle_lambda_exceptions
 from utils.decorators.override_error_check import override_error_check
+from utils.lambda_response import ApiGatewayResponse
 from utils.request_context import request_context
 
 logger = LoggingService(__name__)
@@ -15,4 +16,6 @@ def lambda_handler(event, context):
     request_context.app_interaction = LoggingAppInteraction.EDGE_PRESIGN.value
     logger.info("Edge Presign handler triggered")
     location_header = {"Location": "https://www.google.co.uk", "Hello": "World"}
-    return json.dumps(location_header)
+    return ApiGatewayResponse(
+        200, json.dumps(location_header), "GET"
+    ).create_api_gateway_response()
