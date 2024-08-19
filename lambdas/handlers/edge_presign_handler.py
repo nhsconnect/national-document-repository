a5d15cd08dc371c07eae311ce35ccfe1840dc2ad
Rio Knightley <rio.knightley2@nhs.net>
2024-08-19 14:43:48 +0100

    Add basic logger for edge lambda

--- lambdas/handlers/edge_presign_handler.py
+++ lambdas/handlers/edge_presign_handler.py
@@ -1,18 +1,17 @@
 import json
+import logging
 
-from enums.logging_app_interaction import LoggingAppInteraction
-from utils.audit_logging_setup import LoggingService
 from utils.decorators.handle_lambda_exceptions import handle_lambda_exceptions
 from utils.decorators.override_error_check import override_error_check
-from utils.request_context import request_context
 
-logger = LoggingService(__name__)
+# Set up logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 @override_error_check
 @handle_lambda_exceptions
 def lambda_handler(event, context):
-    request_context.app_interaction = LoggingAppInteraction.EDGE_PRESIGN.value
     logger.info(json.dumps(event))
     response = {
         "status": "302",  # Redirect status code
