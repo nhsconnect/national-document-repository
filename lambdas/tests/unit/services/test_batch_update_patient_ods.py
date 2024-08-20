98af62ddc33364bca8c0f2eed70bbd866bb471fc
Steph Torres <stephane.torres1@nhs.net>
2024-08-20 16:55:10 +0100

    [PRMP-763] add test cases for changed logic in batchupdate

--- lambdas/tests/unit/services/test_batch_update_patient_ods.py
+++ lambdas/tests/unit/services/test_batch_update_patient_ods.py
@@ -7,3 +7,15 @@ def mock_update_ods_service(mocker, set_env):
     service = BatchUpdate()
     mocker.patch.object(service, "dynamo_service")
     yield service
+
+
+def test_get_updated_gp_ods_returns_DECE_if_patient_is_deceased():
+    pass
+
+
+def test_get_updated_gp_ods_returns_SUSP_if_patient_is_inactive():
+    pass
+
+
+def test_get_updated_gp_ods_returns_patient_ods_code():
+    pass
