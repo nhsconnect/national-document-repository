15c035de42936da61a6bdcd7bff0c7fc1f1e2fe3
Rio Knightley <rio.knightley2@nhs.net>
2024-09-12 16:07:33 +0100

    Add data sorting to ods report methods

--- lambdas/services/bulk_upload_report_service.py
+++ lambdas/services/bulk_upload_report_service.py
@@ -36,11 +36,47 @@ class BulkUploadReportService:
         else:
             logger.info("No data found, no new report file to upload")
 
-    def generate_daily_report(self, report_data, formatted_date):
+    def generate_daily_report(self, report_data, start_time, end_time):
+        formatted_date = end_time.strftime("%Y%m%d")
         file_name = f"daily_statistical_report_bulk_upload_summary_{formatted_date}.csv"
         file_key = f"daily-reports/{file_name}"
 
-        self.write_items_to_csv(report_data, f"/tmp/{file_name}")
+        total_successful = len(
+            [item for item in report_data if item["UploadStatus"] == "complete"]
+        )
+        registered_elsewhere = len(
+            [
+                item
+                for item in report_data
+                if item["PdsOdsCode"] != item["UploaderOdsCode"]
+                and item["UploadStatus"] == "complete"
+            ]
+        )
+        suspended_total = len(
+            [item for item in report_data if item["UploadStatus"] == "suspended"]
+        )
+        failure_reasons = self.group_by_failure_reason(report_data)
+
+        csv_data = [
+            {
+                "Type": "Total",
+                "Delimiter": "Total Successful",
+                "Count": total_successful,
+            },
+            {
+                "Type": "Total",
+                "Delimiter": "Successful - Registered Elsewhere",
+                "Count": registered_elsewhere,
+            },
+            {"Type": "Total", "Delimiter": "Suspended", "Count": suspended_total},
+        ]
+
+        for reason, count in failure_reasons.items():
+            csv_data.append(
+                {"Type": "FailureReason", "Delimiter": reason, "Count": count}
+            )
+
+        self.write_to_csv(file_key, csv_data)
 
         logger.info("Uploading daily report file to S3")
         self.s3_service.upload_file(
@@ -49,13 +85,50 @@ class BulkUploadReportService:
             file_name=f"/tmp/{file_name}",
         )
 
-    def generate_ods_reports(self, report_data, formatted_date):
+    def generate_ods_reports(self, report_data, start_time, end_time):
         ods_reports = self.group_data_by_ods_code(report_data)
-        for ods_code, ods_data in ods_reports.items():
-            file_name = f"daily_statistical_report_bulk_upload_summary_{formatted_date}_{ods_code}.csv"
-            file_key = f"{file_name}"
+        formatted_date = end_time.strftime("%Y%m%d")
 
-            self.write_items_to_csv(ods_data, f"/tmp/{file_name}")
+        for ods_code, ods_data in ods_reports.items():
+            total_successful = len(
+                [item for item in ods_data if item["UploadStatus"] == "complete"]
+            )
+            registered_elsewhere = len(
+                [
+                    item
+                    for item in ods_data
+                    if item["PdsOdsCode"] != item["UploaderOdsCode"]
+                    and item["UploadStatus"] == "complete"
+                ]
+            )
+            suspended_total = len(
+                [item for item in ods_data if item["UploadStatus"] == "suspended"]
+            )
+            failure_reasons = self.group_by_failure_reason(ods_data)
+
+            csv_data = [
+                {
+                    "Type": "Total",
+                    "Delimiter": "Total Successful",
+                    "Count": total_successful,
+                },
+                {
+                    "Type": "Total",
+                    "Delimiter": "Successful - Registered Elsewhere",
+                    "Count": registered_elsewhere,
+                },
+                {"Type": "Total", "Delimiter": "Suspended", "Count": suspended_total},
+            ]
+
+            for reason, count in failure_reasons.items():
+                csv_data.append(
+                    {"Type": "FailureReason", "Delimiter": reason, "Count": count}
+                )
+
+            file_name = f"daily_statistical_report_bulk_upload_summary_{formatted_date}_{ods_code}.csv"
+            file_key = f"daily-reports/{file_name}"
+
+            self.write_to_csv(file_key, csv_data)
 
             logger.info(f"Uploading ODS report file for {ods_code} to S3")
             self.s3_service.upload_file(
@@ -90,6 +163,29 @@ class BulkUploadReportService:
         return items
 
     @staticmethod
+    def group_by_failure_reason(self, report_data):
+        failure_reasons = {}
+        for item in report_data:
+            if item["UploadStatus"] == "failed":
+                reason = item.get("FailureReason", "Unknown")
+                if reason in failure_reasons:
+                    failure_reasons[reason] += 1
+                else:
+                    failure_reasons[reason] = 1
+        return failure_reasons
+
+    @staticmethod
+    def group_data_by_ods_code(self, report_data):
+        ods_reports = {}
+        for item in report_data:
+            ods_code = item["UploaderOdsCode"]
+            if ods_code in ods_reports:
+                ods_reports[ods_code].append(item)
+            else:
+                ods_reports[ods_code] = [item]
+        return ods_reports
+
+    @staticmethod
     def write_items_to_csv(items: list, csv_file_path: str):
         logger.info("Writing scan results to csv file")
         with open(csv_file_path, "w") as output_file:
@@ -116,13 +212,3 @@ class BulkUploadReportService:
         #     end_timestamp -= datetime.timedelta(days=1)
         # start_timestamp = end_timestamp - datetime.timedelta(days=1)
         # return start_timestamp, end_timestamp
-
-    @staticmethod
-    def group_data_by_ods_code(report_data):
-        ods_reports = {}
-        for item in report_data:
-            ods_code = item.get("UploaderOdsCode")
-            if ods_code not in ods_reports:
-                ods_reports[ods_code] = []
-            ods_reports[ods_code].append(item)
-        return ods_reports
