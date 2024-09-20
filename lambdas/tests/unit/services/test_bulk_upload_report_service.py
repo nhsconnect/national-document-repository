d6ed8a5c46c18d3173453df99d70c43cb77b058e
Steph Torres <stephane.torres1@nhs.net>
2024-09-20 13:39:46 +0100

    [PRMP-841] add unit tests for new service functionality

--- lambdas/tests/unit/services/test_bulk_upload_report_service.py
+++ lambdas/tests/unit/services/test_bulk_upload_report_service.py
@@ -7,7 +7,9 @@ from freezegun import freeze_time
 from services.bulk_upload_report_service import BulkUploadReportService, OdsReport
 from tests.unit.conftest import (
     MOCK_BULK_REPORT_TABLE_NAME,
-    MOCK_STATISTICS_REPORT_BUCKET,
+    TEST_CURRENT_GP_ODS,
+    TEST_NHS_NUMBER,
+    TEST_UUID,
 )
 from tests.unit.helpers.data.dynamo_scan_response import (
     EXPECTED_RESPONSE,
@@ -16,8 +18,7 @@ from tests.unit.helpers.data.dynamo_scan_response import (
     MOCK_RESPONSE_WITH_LAST_KEY,
     UNEXPECTED_RESPONSE,
 )
-from unit.conftest import TEST_CURRENT_GP_ODS, TEST_NHS_NUMBER, TEST_UUID
-from unit.models.test_bulk_upload_status import (
+from tests.unit.models.test_bulk_upload_status import (
     MOCK_DATA_COMPLETE_UPLOAD,
     MOCK_DATA_FAILED_UPLOAD,
 )
@@ -28,17 +29,17 @@ MOCK_BULK_REPORT_TABLE_RESPONSE = [
         "Date": "2012-01-13",
         "NhsNumber": TEST_NHS_NUMBER,
         "FilePath": f"/{TEST_NHS_NUMBER}/1of1_Lloyd_George_Record_[NAME]_[{TEST_NHS_NUMBER}_[DOB].pdf",
-        "UploaderOdsCode": TEST_CURRENT_GP_ODS,
+        "UploaderOdsCode": "TEST",
         "UploadStatus": "complete",
         "ID": TEST_UUID,
-        "PdsOdsCode": TEST_CURRENT_GP_ODS,
+        "PdsOdsCode": "TEST",
     },
     {
         "Timestamp": 1688395681,
         "Date": "2012-01-13",
         "NhsNumber": "9000000010",
         "FilePath": "/9000000010/1of1_Lloyd_George_Record_[NAME_2]_[9000000010]_[DOB].pdf",
-        "UploaderOdsCode": TEST_CURRENT_GP_ODS,
+        "UploaderOdsCode": "TEST",
         "FailureReason": "Could not find the given patient on PDS",
         "UploadStatus": "failed",
         "ID": TEST_UUID,
@@ -52,6 +53,7 @@ def bulk_upload_report_service(mocker):
     patched_bulk_upload_report_service = BulkUploadReportService()
     mocker.patch.object(patched_bulk_upload_report_service, "db_service")
     mocker.patch.object(patched_bulk_upload_report_service, "s3_service")
+
     yield patched_bulk_upload_report_service
 
 
@@ -95,21 +97,6 @@ def test_get_time_for_scan_at_7am(bulk_upload_report_service):
 
     assert expected_start_report_time == actual_start_time
     assert expected_end_report_time == actual_end_time
-
-
-# def test_write_items_to_csv(bulk_upload_report_service):
-#     items = [MOCK_DATA_COMPLETE_UPLOAD, MOCK_DATA_FAILED_UPLOAD]
-#     expected = readfile("expected_bulk_upload_report.csv")
-#
-# bulk_upload_report_service.write_items_to_csv(file_name="test_file",
-#                                               total_successful=1, total_registered_elsewhere=0,
-#                                               total_suspended=0, extra_rows=[["FailureReason",
-#                                                   "File name not matching Lloyd George naming convention",1]])
-#
-#     with open("test_file") as test_file:
-#         actual = test_file.read()
-#         assert expected == actual
-#     os.remove("test_file")
 
 
 def test_get_dynamo_data_2_calls(mocker, set_env, bulk_upload_report_service):
@@ -225,14 +212,13 @@ def test_report_handler_no_items_return(
 def test_report_handler_with_items(mocker, set_env, bulk_upload_report_service):
     mock_end_report_time = datetime(2012, 1, 14, 7, 0, 0, 0)
     mock_start_report_time = datetime(2012, 1, 13, 7, 0, 0, 0)
-    mock_file_name = "daily_statistical_report_bulk_upload_summary_20120114.csv"
     mock_get_time = mocker.patch(
         "services.bulk_upload_report_service.BulkUploadReportService.get_times_for_scan",
         return_value=(mock_start_report_time, mock_end_report_time),
     )
     mock_get_db = mocker.patch(
         "services.bulk_upload_report_service.BulkUploadReportService.get_dynamodb_report_items",
-        return_value=[{"test": "dsfsf"}],
+        return_value=[MOCK_DATA_COMPLETE_UPLOAD],
     )
     mock_write_csv = mocker.patch(
         "services.bulk_upload_report_service.BulkUploadReportService.write_items_to_csv"
@@ -248,17 +234,17 @@ def test_report_handler_with_items(mocker, set_env, bulk_upload_report_service):
     )
 
     mock_write_csv.assert_called()
-    mock_write_csv.assert_called_with(
-        filename=mock_file_name,
-    )
-    bulk_upload_report_service.s3_service.upload_file.assert_called_with(
-        s3_bucket_name=MOCK_STATISTICS_REPORT_BUCKET,
-        file_key=f"daily-reports/{mock_file_name}",
-        file_name=f"/tmp/{mock_file_name}",
-    )
+
+    bulk_upload_report_service.s3_service.upload_file.assert_called()
+    # bulk_upload_report_service.s3_service.upload_file.assert_called_with(
+    #     s3_bucket_name=MOCK_STATISTICS_REPORT_BUCKET_NAME,
+    #     file_key=f"daily-reports/{mock_file_name}",
+    #     file_name=f"/tmp/{mock_file_name}",
+    # )
 
 
-def test_ods_report_object_creation(bulk_upload_report_service):
+def test_generate_ods_report_object_creation(bulk_upload_report_service, mocker):
+    mock_end_report_time = datetime(2012, 1, 14, 7, 0, 0, 0)
     mock_ods_report_data = [MOCK_DATA_COMPLETE_UPLOAD, MOCK_DATA_FAILED_UPLOAD]
     expected = OdsReport(
         TEST_CURRENT_GP_ODS,
@@ -268,11 +254,89 @@ def test_ods_report_object_creation(bulk_upload_report_service):
         {"File name not matching Lloyd George naming convention": 1},
     )
     actual = bulk_upload_report_service.generate_individual_ods_report(
-        TEST_CURRENT_GP_ODS, mock_ods_report_data
+        TEST_CURRENT_GP_ODS, mock_ods_report_data, mock_end_report_time
     )
 
     assert actual.__eq__(expected)
 
 
-def test_write_csv_with_ods_report_obj(bulk_upload_report_service):
-    pass
+def test_csv_written_with_correct_items(mocker, bulk_upload_report_service):
+    mock_end_report_time = datetime(2012, 1, 14, 7, 0, 0, 0)
+    mock_ods_report_data = [MOCK_DATA_COMPLETE_UPLOAD, MOCK_DATA_FAILED_UPLOAD]
+    expected = OdsReport(
+        TEST_CURRENT_GP_ODS,
+        1,
+        0,
+        0,
+        {"File name not matching Lloyd George naming convention": 1},
+    )
+    mock_write_csv = mocker.patch(
+        "services.bulk_upload_report_service.BulkUploadReportService.write_items_to_csv"
+    )
+    bulk_upload_report_service.generate_individual_ods_report(
+        TEST_CURRENT_GP_ODS, mock_ods_report_data, mock_end_report_time
+    )
+    extra_rows = [
+        ["FailureReason", "File name not matching Lloyd George naming convention", 1]
+    ]
+    mock_write_csv.assert_called()
+    mock_write_csv.assert_called_with(
+        file_name=f"daily_statistical_report_bulk_upload_summary_{mock_end_report_time}_uploaded_by_{TEST_CURRENT_GP_ODS}.csv",
+        total_successful=expected.total_successful,
+        total_registered_elsewhere=expected.total_registered_elsewhere,
+        total_suspended=expected.total_suspended,
+        extra_rows=extra_rows,
+    )
+
+
+def test_generate_ods_reports_with_multiple_ods(bulk_upload_report_service):
+    mock_end_report_time = datetime(2012, 1, 14, 7, 0, 0, 0)
+    mock_ods_report_data = [
+        MOCK_DATA_COMPLETE_UPLOAD,
+        MOCK_DATA_FAILED_UPLOAD,
+        MOCK_BULK_REPORT_TABLE_RESPONSE[0],
+        MOCK_BULK_REPORT_TABLE_RESPONSE[1],
+    ]
+
+    actual = bulk_upload_report_service.generate_ods_reports(
+        mock_ods_report_data, mock_end_report_time
+    )
+    assert len(actual) == 2
+
+
+def test_generate_summary_report_with_two_ods_reports(
+    mocker, bulk_upload_report_service
+):
+    mock_end_report_time = datetime(2012, 1, 14, 7, 0, 0, 0)
+    mock_ods_report_data = [
+        MOCK_DATA_COMPLETE_UPLOAD,
+        MOCK_DATA_FAILED_UPLOAD,
+        MOCK_BULK_REPORT_TABLE_RESPONSE[0],
+        MOCK_BULK_REPORT_TABLE_RESPONSE[1],
+    ]
+    mock_write_csv = mocker.patch(
+        "services.bulk_upload_report_service.BulkUploadReportService.write_items_to_csv"
+    )
+
+    ods_reports = bulk_upload_report_service.generate_ods_reports(
+        mock_ods_report_data, mock_end_report_time
+    )
+
+    bulk_upload_report_service.generate_summary_report(
+        ods_reports, mock_end_report_time
+    )
+
+    extra_rows = [
+        ["FailureReason", "File name not matching Lloyd George naming convention", 1],
+        ["FailureReason", "Could not find the given patient on PDS", 1],
+        ["Success by ODS", "Y12345", 1],
+        ["Success by ODS", "TEST", 1],
+    ]
+    mock_write_csv.assert_called()
+    mock_write_csv.assert_called_with(
+        file_name=f"daily_statistical_report_bulk_upload_summary_{mock_end_report_time}.csv",
+        total_successful=2,
+        total_registered_elsewhere=0,
+        total_suspended=0,
+        extra_rows=extra_rows,
+    )
