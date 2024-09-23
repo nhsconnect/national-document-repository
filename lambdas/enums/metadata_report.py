972d960d8c031095c903b64a2b86bb4b7df98352
Steph Torres <stephane.torres1@nhs.net>
2024-09-23 10:30:05 +0100

    [PRMP-841] remove auto formatting from metadata_report enum

--- lambdas/enums/metadata_report.py
+++ lambdas/enums/metadata_report.py
@@ -2,12 +2,12 @@ from enum import StrEnum
 
 
 class MetadataReport(StrEnum):
-    NhsNumber = ("NhsNumber",)
-    UploadStatus = ("UploadStatus",)
-    FailureReason = ("FailureReason",)
-    PdsOdsCode = ("PdsOdsCode",)
-    UploaderOdsCode = ("UploaderOdsCode",)
-    FilePath = ("FilePath",)
-    Date = ("Date",)
-    Timestamp = ("Timestamp",)
-    ID = ("ID",)
+    NhsNumber = "NhsNumber"
+    UploadStatus = "UploadStatus"
+    FailureReason = "FailureReason"
+    PdsOdsCode = "PdsOdsCode"
+    UploaderOdsCode = "UploaderOdsCode"
+    FilePath = "FilePath"
+    Date = "Date"
+    Timestamp = "Timestamp"
+    ID = "ID"
