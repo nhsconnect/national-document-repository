958079e4ccc45ea68133b7bd5190e5ab181b5685
Rio Knightley <rio.knightley2@nhs.net>
2024-09-27 12:10:44 +0100

    Fix cloudwatch login query

--- lambdas/utils/cloudwatch_logs_query.py
+++ lambdas/utils/cloudwatch_logs_query.py
@@ -55,13 +55,12 @@ LloydGeorgeRecordsSearched = CloudwatchLogsQueryParams(
 UserLogins = CloudwatchLogsQueryParams(
     lambda_name="TokenRequestHandler",
     query_string="""
-        fields @timestamp, Message, 
+        fields @timestamp, 
         Authorisation.selected_organisation.org_ods_code AS ods_code, 
         Authorisation.nhs_user_id AS user_id, 
         Authorisation.role_id AS role_id
         | filter Message = 'User logged in successfully'
-        | stats count_distinct(user_id) as distinct_user_count, 
-        collect_set(role_id) as role_ids 
+        | stats count_distinct(user_id) as distinct_user_count 
         by ods_code
     """,
 )
