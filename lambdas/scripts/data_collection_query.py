504d4a4912bc9fb9a2f8326e56ad8be9fcef16bc
Rio Knightley <rio.knightley2@nhs.net>
2024-09-27 14:27:43 +0100

    Extend error handling of data collection script

--- lambdas/scripts/data_collection_query.py
+++ lambdas/scripts/data_collection_query.py
@@ -13,45 +13,49 @@ os.environ["LLOYD_GEORGE_BUCKET_NAME"] = ""
 os.environ["DOCUMENT_STORE_BUCKET_NAME"] = ""
 
 
+def check_env_vars(required_vars):
+    missing_vars = [var for var in required_vars if not os.getenv(var)]
+    if missing_vars:
+        raise EnvironmentError(
+            f"Missing required environment variables: {', '.join(missing_vars)}"
+        )
+
+
 def decimal_default(obj):
-    """Helper function to convert Decimals to floats for JSON serialization."""
     if isinstance(obj, Decimal):
         return float(obj)
     elif isinstance(obj, set):
-        # Handle sets by converting them to lists
         return list(obj)
     elif hasattr(obj, "__dict__"):
-        # Handle other custom objects by serializing their dictionary
         return obj.__dict__
     else:
         raise TypeError(f"Object of type {type(obj)} is not serializable")
 
 
 def convert_to_dict(item):
-    """Converts an item to a dictionary if it is a dataclass, else returns the item as is."""
     if is_dataclass(item):
         return asdict(item)
     else:
-        return item  # Handle custom objects if needed
+        return item
 
 
 def main():
+    required_env_vars = [
+        "WORKSPACE",
+        "STATISTICS_TABLE",
+        "DOCUMENT_STORE_DYNAMODB_NAME",
+        "LLOYD_GEORGE_DYNAMODB_NAME",
+        "LLOYD_GEORGE_BUCKET_NAME",
+        "DOCUMENT_STORE_BUCKET_NAME",
+    ]
+    check_env_vars(required_env_vars)
+
     service = DataCollectionService()
-
-    # Collect all data
     statistic_data = service.collect_all_data()
-
-    # Convert all dataclasses to dictionaries if they are dataclass instances
     statistic_data_dict = [convert_to_dict(entry) for entry in statistic_data]
-
-    # Convert the data to a JSON string, handling Decimals
     json_output = json.dumps(statistic_data_dict, default=decimal_default, indent=4)
-
-    # Log or print the JSON output
     print(json_output)
-
-    # If you want to write to a file
-    with open("output.json", "w") as f:
+    with open("data_collection_output.json", "w") as f:
         f.write(json_output)
 
 
