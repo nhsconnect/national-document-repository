384dbd35c33c4199fe2c43559f8043c07080a13c
AndyFlintNHS <andrew.flint2@nhs.net>
2024-10-07 18:23:57 +0100

    [PRMP-844] refactored column rename script to be generic

--- lambdas/scripts/bulk_upload_column_change.py
+++ lambdas/scripts/bulk_upload_column_change.py
@@ -7,32 +7,31 @@ from typing import Dict
 from pydantic import BaseModel, TypeAdapter
 from services.base.dynamo_service import DynamoDBService
 
-COLUMNS_TO_FETCH = "OdsCode,PdsOdsCode,UploaderOdsCode,ID"
-ATTRIBUTE_TO_REMOVE = "OdsCode"
-ATTRIBUTE_TO_ADD = "PdsOdsCode"
-UPLOADER_ODS_CODE = "PlaceHolder"
+TABLE = "ndrb_BulkUploadReport"
+COLUMNS_TO_FETCH = "FailureReason,ID"
+ATTRIBUTE_TO_REMOVE = "FailureReason"
+ATTRIBUTE_TO_ADD = "Reason"
 
 
 class ProgressForDoc(BaseModel):
-    ods_code: str = ""
+    value_stored_in_old_column: str = ""
     update_completed: bool = False
 
 
-class BatchUpdate:
+class RenameColumn:
     def __init__(
         self,
-        table_name: str,
         progress_store_file_path: str = "column_change_progress.json",
     ):
         self.progress_store = progress_store_file_path
-        self.table_name = table_name
+        self.table_name = TABLE
         self.dynamo_service = DynamoDBService()
         self.progress: Dict[str, ProgressForDoc] = {}
 
-        self.logger = logging.getLogger("BatchUpdateOds")
+        self.logger = logging.getLogger(__name__)
 
     def main(self):
-        self.logger.info("Starting batch update script")
+        self.logger.info("Starting rename column script")
         self.logger.info(f"Table to be updated: {self.table_name}")
 
         if self.found_previous_progress():
@@ -62,23 +61,22 @@ class BatchUpdate:
         ]
         if not patients_to_be_updated:
             self.logger.info(
-                "Already updated the ODS codes for all patients in previous run."
+                f"Already renamed the '{ATTRIBUTE_TO_REMOVE}' column to '{ATTRIBUTE_TO_ADD}' for all records in previous run."
             )
             exit(2)
 
-        self.update_patient_ods()
+        self.update_column_name()
 
-        self.logger.info("Finished updating all patient's ODS codes")
+        self.logger.info(
+            f"Finished renaming '{ATTRIBUTE_TO_REMOVE}' column to '{ATTRIBUTE_TO_ADD}' for all records"
+        )
         exit(0)
 
-    def update_patient_ods(self):
+    def update_column_name(self):
         for doc_id, document in self.progress.items():
-            self.logger.info(f"Updated ODS code for {doc_id}")
+            self.logger.info(f"Updated column name for {doc_id}")
 
-            updated_fields = {
-                ATTRIBUTE_TO_ADD: document.ods_code,
-                "UploaderOdsCode": UPLOADER_ODS_CODE,
-            }
+            updated_fields = {ATTRIBUTE_TO_ADD: document.value_stored_in_old_column}
             self.dynamo_service.update_item(
                 table_name=self.table_name,
                 key=doc_id,
@@ -88,7 +86,7 @@ class BatchUpdate:
             self.dynamo_service.remove_attribute_from_item(
                 table_name=self.table_name, key=doc_id, attribute=ATTRIBUTE_TO_REMOVE
             )
-            self.logger.info(f"Updated ODS code for patient: {document}")
+            self.logger.info(f"Updated column name for record: {document}")
 
             self.progress[doc_id].update_completed = True
         self.save_progress()
@@ -97,7 +95,7 @@ class BatchUpdate:
         all_entries = self.list_all_entries()
         if len(all_entries) == 0:
             self.logger.info(
-                f"No records was found in table {self.table_name}. Please check the table name."
+                f"No records were found in table {self.table_name}. Please check the table name."
             )
             exit(1)
 
@@ -149,12 +147,14 @@ class BatchUpdate:
 
         progress_dict = {}
         for entry in dynamodb_records:
-            ods_code = entry.get(ATTRIBUTE_TO_REMOVE)
-            if ods_code is None:
+            value_stored_in_old_column = entry.get(ATTRIBUTE_TO_REMOVE)
+            if value_stored_in_old_column is None:
                 continue
             doc_ref_id = entry["ID"]
 
-            progress_dict[doc_ref_id] = ProgressForDoc(ods_code=ods_code)
+            progress_dict[doc_ref_id] = ProgressForDoc(
+                value_stored_in_old_column=value_stored_in_old_column
+            )
 
         self.logger.info(f"Totally {len(progress_dict)} patients found in record.")
         return progress_dict
@@ -172,15 +172,5 @@ def setup_logging_for_local_script():
 
 
 if __name__ == "__main__":
-    import argparse
-
     setup_logging_for_local_script()
-
-    parser = argparse.ArgumentParser(
-        prog="batch_update_ods_code.py",
-        description="A utility script to update the ODS Codes for all patients in a dynamoDB doc reference table",
-    )
-    parser.add_argument("table_name", type=str, help="The name of dynamodb table")
-    args = parser.parse_args()
-
-    BatchUpdate(table_name=args.table_name).main()
+    RenameColumn().main()
