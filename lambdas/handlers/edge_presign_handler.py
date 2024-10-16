584371b91c6e3d1b2debe1316d4a85fbf1d4a1ce
Rio Knightley <rio.knightley2@nhs.net>
2024-10-16 16:08:27 +0100

    Log edge response

--- lambdas/handlers/edge_presign_handler.py
+++ lambdas/handlers/edge_presign_handler.py
@@ -1,7 +1,6 @@
 import hashlib
 import json
 import logging
-from urllib.parse import parse_qs, urlencode
 
 from enums.lambda_error import LambdaError
 from services.edge_presign_service import EdgePresignService
@@ -9,6 +8,9 @@ from utils.decorators.handle_edge_exceptions import handle_edge_exceptions
 from utils.decorators.override_error_check import override_error_check
 from utils.decorators.set_audit_arg import set_request_context_for_logging
 from utils.lambda_exceptions import CloudFrontEdgeException
+
+# from urllib.parse import parse_qs, urlencode
+
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -33,7 +35,7 @@ def lambda_handler(event, context):
         )
         raise CloudFrontEdgeException(500, LambdaError.EdgeMalformed)
 
-    s3_presign_credentials = parse_qs(presign_query_string)
+    # s3_presign_credentials = parse_qs(presign_query_string)
     presign_string = f"{uri}?{presign_query_string}"
     encoded_presign_string: str = presign_string.encode("utf-8")
     presign_credentials_hash = hashlib.md5(encoded_presign_string).hexdigest()
@@ -46,19 +48,22 @@ def lambda_handler(event, context):
     logger.info(f"Original Query String: {presign_query_string}")
     logger.info(f"Original Headers: {headers}")
 
-    filtered_query_params = {
-        key: value
-        for key, value in s3_presign_credentials.items()
-        if not key.lower().startswith("x-amz-")
-    }
-    filtered_query_string = urlencode(filtered_query_params, doseq=True)
+    # filtered_query_params = {
+    #     key: value
+    #     for key, value in s3_presign_credentials.items()
+    #     if not key.lower().startswith("x-amz-")
+    # }
+    # filtered_query_string = urlencode(filtered_query_params, doseq=True)
 
-    if "authorization" in headers:
-        del headers["authorization"]
+    # if "authorization" in headers:
+    #     del headers["authorization"]
 
-    logger.info(f"Filtered Query String: {filtered_query_string}")
-    logger.info(f"Filtered Headers: {json.dumps(headers)}")
+    # logger.info(f"Filtered Query String: {filtered_query_string}")
+    # logger.info(f"Filtered Headers: {json.dumps(headers)}")
 
-    request["headers"] = headers
-    request["querystring"] = filtered_query_string
+    # request["headers"] = headers
+    # request["querystring"] = filtered_query_string
+
+    logger.info(f"Edge Response: {json.dumps(request)}")
+
     return request
