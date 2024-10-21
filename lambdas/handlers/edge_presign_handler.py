8ad92d3ad52a4366ea249e81b697f04e32a894b3
Rio Knightley <rio.knightley2@nhs.net>
2024-10-21 12:30:17 +0100

    Fix domain name path

--- lambdas/handlers/edge_presign_handler.py
+++ lambdas/handlers/edge_presign_handler.py
@@ -46,9 +46,7 @@ def lambda_handler(event, context):
         del headers["authorization"]
     request["headers"] = headers
     request["querystring"] = ""
-    request["origin"]["s3"][
-        "domainName"
-    ] = "ndra-lloyd-george-store.s3.eu-west-2.amazonaws.com"
+    request["origin"]["s3"]["domainName"]
     logger.info(f"Edge Response: {json.dumps(request)}")
 
     return request
