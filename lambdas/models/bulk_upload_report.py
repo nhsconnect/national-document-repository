9e1bace658117e0fea4171d3de47f5cb3a826a17
Steph Torres <stephane.torres1@nhs.net>
2024-11-05 13:57:06 +0000

    [PRMP-1066] add type casting to new BulkUploadReport model method

--- lambdas/models/bulk_upload_report.py
+++ lambdas/models/bulk_upload_report.py
@@ -29,10 +29,10 @@ class BulkUploadReport(BaseModel):
     uploader_ods_code: str = Field(alias=MetadataReport.UploaderOdsCode)
     reason: Optional[str] = Field(default="", alias=MetadataReport.Reason)
 
-    def get_registered_at_uploader_practice_status(self):
+    def get_registered_at_uploader_practice_status(self) -> str:
         if self.pds_ods_code in PatientOdsInactiveStatus.list():
             return self.pds_ods_code
-        return self.uploader_ods_code == self.pds_ods_code
+        return str(self.uploader_ods_code == self.pds_ods_code)
 
 
 def date_string_yyyymmdd(time_now: datetime) -> str:
