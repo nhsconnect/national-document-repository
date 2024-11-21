74a523f5daf6e6413b2099bb75311482f813dd0a
Jack Sutton <jack.sutton@madetech.com>
2024-11-21 12:01:49 +0000

    adds tests around sending nrl message

--- lambdas/tests/unit/repositories/bulk_upload/test_bulk_upload_sqs_repository.py
+++ lambdas/tests/unit/repositories/bulk_upload/test_bulk_upload_sqs_repository.py
@@ -2,9 +2,11 @@ import copy
 
 import pytest
 from repositories.bulk_upload.bulk_upload_sqs_repository import BulkUploadSqsRepository
-from tests.unit.conftest import MOCK_LG_METADATA_SQS_QUEUE
+from tests.unit.conftest import METADATA_NRL_SQS_URL, MOCK_LG_METADATA_SQS_QUEUE
 from tests.unit.helpers.data.bulk_upload.test_data import (
+    TEST_GROUP_ID,
     TEST_NHS_NUMBER_FOR_BULK_UPLOAD,
+    TEST_NRL_SQS_MESSAGE,
     TEST_SQS_MESSAGE,
     TEST_STAGING_METADATA,
 )
@@ -41,4 +43,17 @@ def test_put_sqs_message_back_to_queue(set_env, repo_under_test):
         queue_url=MOCK_LG_METADATA_SQS_QUEUE,
         message_body=TEST_SQS_MESSAGE["body"],
         nhs_number=TEST_NHS_NUMBER_FOR_BULK_UPLOAD,
+    )
+
+
+def test_send_message_to_nrl_sqs_fifo(set_env, repo_under_test):
+    repo_under_test.send_message_to_nrl_fifo(
+        METADATA_NRL_SQS_URL,
+        TEST_NRL_SQS_MESSAGE,
+        TEST_GROUP_ID,
+        TEST_NHS_NUMBER_FOR_BULK_UPLOAD,
+    )
+    messsage_body = TEST_NRL_SQS_MESSAGE.model_dump()
+    repo_under_test.sqs_repository.send_message_with_nhs_number_attr_fifo.assert_called_with(
+        "https://test-queue.com", messsage_body, "123", "9000000009"
     )
