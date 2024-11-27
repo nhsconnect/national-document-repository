c77497c20e72be87940d5da9c91070052eb558db
Steph Torres <stephane.torres1@nhs.net>
2024-11-27 17:15:55 +0000

    [PRMP-1185] ammend bad request

--- lambdas/scripts/mns_subscription.py
+++ lambdas/scripts/mns_subscription.py
@@ -15,8 +15,6 @@ auth_service = NhsOauthService(ssm_service)
 
 
 headers = {
-    "content-Type": "application/fhir+json",
-    "accept": "application/json",
     "authorization": f"Bearer {auth_service.get_active_access_token()}",
     "x-correlation-id": str(uuid.uuid4()),
 }
@@ -30,6 +28,7 @@ sqs_client = boto3.client("sqs")
 sqs_url = sqs_client.get_queue_url(QueueName=f"{env_prefix}-mns-notification-queue")[
     "QueueUrl"
 ]
+
 sqs_arn = sqs_client.get_queue_attributes(
     QueueUrl=sqs_url, AttributeNames=["QueueArn"]
 )["Attributes"]["QueueArn"]
@@ -48,7 +47,7 @@ def get_subscription_id(event_type):
         },
     }
     try:
-        response = requests.post(url, headers=headers, data=request_body)
+        response = requests.post(url, headers=headers, json=request_body)
         response.raise_for_status()
         subscription_id = response.json().get("id")
         return subscription_id
@@ -58,8 +57,9 @@ def get_subscription_id(event_type):
 
 if __name__ == "__main__":
     for event, parameter in events.items():
+        subscription_id = get_subscription_id(event)
         ssm_service.update_ssm_parameter(
             parameter_key=parameter,
-            parameter_value=get_subscription_id(event),
+            parameter_value=subscription_id,
             parameter_type="SecureString",
         )
