5bcc4fd08bcde597c82c7d59cc0a0a6c69200285
Steph Torres <stephane.torres1@nhs.net>
2024-11-27 12:30:38 +0000

    [PRMP-1188] remove subscription from enum

--- lambdas/enums/mns_notification_types.py
+++ lambdas/enums/mns_notification_types.py
@@ -4,4 +4,3 @@ from enum import StrEnum
 class MNSNotificationTypes(StrEnum):
     CHANGE_OF_GP = "pds-change-of-gp-1"
     DEATH_NOTIFICATION = "pds-death-notification-1"
-    SUBSCRIPTION = "Subscription"
