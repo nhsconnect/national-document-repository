1c46437153b5e20ae22b6ddfa45aa9ba8d9b7000
NogaNHS <noga.sasson1@nhs.net>
2024-12-17 11:53:31 +0000

    [PRMP-1287] add service unit test

--- lambdas/tests/unit/services/test_nrl_get_document_reference_service.py
+++ lambdas/tests/unit/services/test_nrl_get_document_reference_service.py
@@ -1,7 +1,9 @@
 import pytest
+from enums.patient_ods_inactive_status import PatientOdsInactiveStatus
 from services.nrl_get_document_reference_service import NRLGetDocumentReferenceService
 from tests.unit.conftest import FAKE_URL, TEST_CURRENT_GP_ODS, TEST_UUID
 from tests.unit.helpers.data.test_documents import create_test_doc_store_refs
+from utils.lambda_exceptions import NRLGetDocumentReferenceException
 
 MOCK_USER_INFO = {
     "nhsid_useruid": TEST_UUID,
@@ -59,6 +61,7 @@ def mock_fetch_user_info(patched_service, mocker):
     mocker.patch.object(service, "get_user_roles_and_ods_codes")
     mocker.patch.object(service, "fetch_user_info")
     mocker.patch.object(service, "get_patient_current_gp_ods")
+    mocker.patch.object(service, "patient_is_inactive")
     yield service
 
 
@@ -91,11 +94,67 @@ def test_get_document_reference_service(patched_service):
     assert actual == create_test_doc_store_refs()[0]
 
 
+def test_handle_get_document_reference_request(patched_service, mocker):
+    expected = "test_response"
+    mock_document_ref = create_test_doc_store_refs()[0]
+    mocker.patch.object(
+        patched_service, "get_document_references", return_value=mock_document_ref
+    )
+    mocker.patch.object(patched_service, "fetch_user_info", return_value=MOCK_USER_INFO)
+    mocker.patch.object(
+        patched_service, "is_user_allowed_to_see_file", return_value=True
+    )
+    mocker.patch.object(
+        patched_service, "create_document_presigned_url", return_value=FAKE_URL
+    )
+    mocker.patch.object(
+        patched_service,
+        "create_document_reference_fhir_response",
+        return_value=expected,
+    )
+
+    actual = patched_service.handle_get_document_reference_request(
+        "test-id", "bearer token_test"
+    )
+
+    assert expected == actual
+    patched_service.create_document_reference_fhir_response.assert_called_once_with(
+        mock_document_ref, FAKE_URL
+    )
+
+
+def test_handle_get_document_reference_request_when_user_is_not_allowed_access(
+    patched_service, mocker
+):
+    mock_document_ref = create_test_doc_store_refs()[0]
+    mocker.patch.object(
+        patched_service, "get_document_references", return_value=mock_document_ref
+    )
+    mocker.patch.object(patched_service, "fetch_user_info", return_value=MOCK_USER_INFO)
+    mocker.patch.object(
+        patched_service, "is_user_allowed_to_see_file", return_value=False
+    )
+    mocker.patch.object(
+        patched_service, "create_document_presigned_url", return_value=FAKE_URL
+    )
+    mocker.patch.object(patched_service, "create_document_reference_fhir_response")
+
+    with pytest.raises(NRLGetDocumentReferenceException):
+        patched_service.handle_get_document_reference_request(
+            "test-id", "bearer token_test"
+        )
+
+    patched_service.create_document_reference_fhir_response.assert_not_called()
+
+
+def test_create_document_reference_fhir_response(patched_service):
+    actual = patched_service.create_document_reference_fhir_response(
+        create_test_doc_store_refs()[0], FAKE_URL
+    )
+    assert actual["content"][0]["attachment"]["url"] == FAKE_URL
+
+
 def test_user_allowed_to_see_file_happy_path(patched_service, mock_fetch_user_info):
-    # mock_fetch_user_info.fetch_user_info.return_value = MOCK_USER_INFO
-    # patched_service.document_service.fetch_documents_from_table.return_value = (
-    #     MOCK_SINGLE_DOCUMENT_RESPONSE
-    # )
     patched_service.get_user_roles_and_ods_codes.return_value = {
         "TEST_ODS": ["R8000", "R3002", "R8003"],
         "Y12345": ["R8000", "R1234"],
@@ -103,6 +162,8 @@ def test_user_allowed_to_see_file_happy_path(patched_service, mock_fetch_user_in
 
     patched_service.get_ndr_accepted_role_codes.return_value = ["R8000", "R8008"]
     patched_service.get_patient_current_gp_ods.return_value = TEST_CURRENT_GP_ODS
+    patched_service.patient_is_inactive.return_value = False
+
     assert (
         patched_service.is_user_allowed_to_see_file(
             TEST_UUID, create_test_doc_store_refs()[0]
@@ -111,7 +172,41 @@ def test_user_allowed_to_see_file_happy_path(patched_service, mock_fetch_user_in
     )
 
 
-def test_create_document_reference_fhir_response(patched_service):
-    patched_service.create_document_reference_fhir_response(
-        create_test_doc_store_refs()[0], FAKE_URL
+def test_user_allowed_to_see_file_returns_false(patched_service, mock_fetch_user_info):
+    patched_service.get_user_roles_and_ods_codes.return_value = {
+        "TEST_ODS": ["R8001", "R3002", "R8003"],
+        "Y12345": ["R8001", "R1234"],
+    }
+
+    patched_service.get_ndr_accepted_role_codes.return_value = ["R8000", "R8008"]
+    patched_service.get_patient_current_gp_ods.return_value = TEST_CURRENT_GP_ODS
+    patched_service.patient_is_inactive.return_value = False
+
+    assert (
+        patched_service.is_user_allowed_to_see_file(
+            TEST_UUID, create_test_doc_store_refs()[0]
+        )
+        is False
+    )
+
+
+def test_user_allowed_to_see_file_inactive_patient(
+    patched_service, mock_fetch_user_info
+):
+    patched_service.get_user_roles_and_ods_codes.return_value = {
+        "TEST_ODS": ["R8000", "R3002", "R8003"],
+        "Y12345": ["R8000", "R1234"],
+    }
+
+    patched_service.get_ndr_accepted_role_codes.return_value = ["R8000", "R8008"]
+    patched_service.get_patient_current_gp_ods.return_value = (
+        PatientOdsInactiveStatus.DECEASED
+    )
+    patched_service.patient_is_inactive.return_value = True
+
+    assert (
+        patched_service.is_user_allowed_to_see_file(
+            TEST_UUID, create_test_doc_store_refs()[0]
+        )
+        is False
     )
