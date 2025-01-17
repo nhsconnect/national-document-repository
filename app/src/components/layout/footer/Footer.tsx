8f95e4bc0a8d4e0d32673bc8d53168b69a834ca2
Jack Sutton <jack.sutton@madetech.com>
2025-01-17 12:45:14 +0000

    adds new link to footer

--- app/src/components/layout/footer/Footer.tsx
+++ app/src/components/layout/footer/Footer.tsx
@@ -5,10 +5,31 @@ import { routes } from '../../../types/generic/routes';
 const serviceUpdatesLink =
     'https://digital.nhs.uk/services/access-and-store-digital-patient-documents/service-updates';
 
+const helpandGuidanceLink =
+    'https://digital.nhs.uk/services/access-and-store-digital-patient-documents/help-and-guidance';
+
 function Footer() {
     return (
         <NHSFooter>
             <NHSFooter.List>
+                <NHSFooter.ListItem
+                    href={helpandGuidanceLink}
+                    data-testid="help-and-guidance-link"
+                    rel="noopener"
+                    target="_blank"
+                    aria-label="Help and Guidance - Opens in a new tab"
+                >
+                    Help and Guidance
+                </NHSFooter.ListItem>
+                <NHSFooter.ListItem
+                    href={serviceUpdatesLink}
+                    data-testid="service-updates-link"
+                    rel="noopener"
+                    target="_blank"
+                    aria-label="Service updates - Opens in a new tab"
+                >
+                    Service updates
+                </NHSFooter.ListItem>
                 <NHSFooter.ListItem
                     href={routes.PRIVACY_POLICY}
                     data-testid="privacy-link"
