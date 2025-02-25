9a457da2a82f0b839ab234f7e72814e3e2bc4bdb
Ollie Beumkes <oliver.beumkes2@nhs.net>
2025-02-25 09:21:21 +0000

    Started to implement AWS RUM on frontend

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
+import { AwsRum } from 'aws-rum-web';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
