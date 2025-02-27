e4cf3679de20d8688f35093e89e0234d4fbc1eca
Ollie Beumkes <oliver.beumkes2@nhs.net>
2025-02-27 15:30:34 +0000

    Updated tsx to use Default script code from AWS

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,27 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
-import { AwsRum } from 'aws-rum-web';
+import { AwsRum, AwsRumConfig } from 'aws-rum-web';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 if (process.env.REACT_APP_ENVIRONMENT === 'development') {
-    new AwsRum(
-        process.env.REACT_APP_RUM_APP_ID || '',
-        process.env.REACT_APP_AWS_REGION || 'eu-west-2',
-        '1.0.0',
-        {
-            sessionSampleRate: 1.0, // Capture all sessions
-            guestRoleArn: `arn:aws:iam::${process.env.REACT_APP_AWS_ACCOUNT_ID}:role/RumServiceRole`,
+    try {
+        const config: AwsRumConfig = {
+            sessionSampleRate: 1,
             identityPoolId: process.env.REACT_APP_RUM_IDENTITY_POOL_ID || '',
             endpoint: `https://dataplane.rum.${
                 process.env.REACT_APP_AWS_REGION || 'eu-west-2'
             }.amazonaws.com`,
-            telemetries: ['performance', 'errors', 'http'],
+            telemetries: ['http', 'errors', 'performance'],
             allowCookies: true,
             enableXRay: true,
-        },
-    );
+        };
+
+        const APPLICATION_ID: string = process.env.REACT_APP_AWS_ACCOUNT_ID || '';
+        const APPLICATION_VERSION: string = '1.0.0';
+        const APPLICATION_REGION: string = process.env.REACT_APP_AWS_REGION || 'eu-west-2';
+
+        new AwsRum(APPLICATION_ID, APPLICATION_VERSION, APPLICATION_REGION, config);
+    } catch (error) {
+        // Ignore errors thrown during CloudWatch RUM web client initialization
+    }
 }
 
 root.render(
