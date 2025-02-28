93a78dbc573d7b3592c29e74d3deaf2826c4efbc
Ollie Beumkes <oliver.beumkes2@nhs.net>
2025-02-28 12:58:36 +0000

    Refactored for initial PR

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -10,17 +10,14 @@ if (process.env.REACT_APP_ENVIRONMENT === 'development') {
     try {
         const config: AwsRumConfig = {
             sessionSampleRate: 1,
-            identityPoolId:
-                process.env.REACT_APP_RUM_IDENTITY_POOL_ID ||
-                'eu-west-2:5d286c6d-f8bd-49eb-930a-eb650a56427c',
+            identityPoolId: process.env.REACT_APP_RUM_IDENTITY_POOL_ID || '',
             endpoint: `https://dataplane.rum.eu-west-2.amazonaws.com`,
             telemetries: ['http', 'errors', 'performance'],
             allowCookies: true,
             enableXRay: false,
         };
 
-        const APPLICATION_ID: string =
-            process.env.REACT_APP_MONITOR_ACCOUNT_ID || '45c1bd72-49c2-4d95-b25c-7f8f28e62153';
+        const APPLICATION_ID: string = process.env.REACT_APP_MONITOR_ACCOUNT_ID || '';
         const APPLICATION_VERSION: string = '1.0.0';
         const APPLICATION_REGION: string = process.env.REACT_APP_AWS_REGION || 'eu-west-2';
 
@@ -29,28 +26,10 @@ if (process.env.REACT_APP_ENVIRONMENT === 'development') {
             APPLICATION_VERSION,
             APPLICATION_REGION,
             config,
-        ); // eslint-disable-line
-        // eslint-disable-next-line no-console
-        console.log('RUM client initialized');
-        // eslint-disable-next-line no-console
-        console.log(process.env.REACT_APP_ENVIRONMENT);
-        // eslint-disable-next-line no-console
-        console.log(process.env.REACT_APP_MONITOR_ACCOUNT_ID);
-        // eslint-disable-next-line no-console
-        console.log(process.env.REACT_APP_RUM_IDENTITY_POOL_ID);
+        );
     } catch (error) {
-        // eslint-disable-next-line no-console
-        console.log(error);
+        console.log(error); // eslint-disable-line
     }
-} else {
-    // eslint-disable-next-line no-console
-    console.log('RUM client not initialized');
-    // eslint-disable-next-line no-console
-    console.log(process.env.REACT_APP_ENVIRONMENT);
-    // eslint-disable-next-line no-console
-    console.log(process.env.REACT_APP_MONITOR_ACCOUNT_ID);
-    // eslint-disable-next-line no-console
-    console.log(process.env.REACT_APP_RUM_IDENTITY_POOL_ID);
 }
 
 function App() {
