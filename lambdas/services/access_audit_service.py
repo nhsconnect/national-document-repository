1f0e940e4df43f62ac73283291a905bb27fe3c01
NogaNHS <noga.sasson1@nhs.net>
2025-03-06 17:06:50 +0000

    [PRMP-1601] add methods in access_audit_service.py

--- lambdas/services/access_audit_service.py
+++ lambdas/services/access_audit_service.py
@@ -1,7 +1,7 @@
 import os
 
-from services.authoriser_service import AuthoriserService
 from services.base.dynamo_service import DynamoDBService
+from services.manage_user_session_access import ManageUserSessionAccess
 from utils.audit_logging_setup import LoggingService
 
 logger = LoggingService(__name__)
@@ -9,13 +9,19 @@ logger = LoggingService(__name__)
 
 class AccessAuditService:
     def __init__(self):
-        self.dynamodb_service = DynamoDBService()
-        self.auth_service = AuthoriserService()
-        self.audit_table = self.dynamodb_service.get_table("access_audit")
-        self.session_table_name = os.getenv("AUTH_SESSION_TABLE_NAME")
+        self.audit_table = os.getenv("ACCESS_AUDIT_TABLE_NAME")
+        self.manage_user_session_service = ManageUserSessionAccess()
+        self.db_service = DynamoDBService()
 
-    def write_to_access_audit_table(self):
-        pass
+    def write_to_access_audit_table(self, ndr_session_id, updated_fields):
+        self.db_service.update_item(
+            table_name=self.audit_table,
+            key_pair={"NDRSessionId": ndr_session_id},
+            updated_fields=updated_fields,
+        )
 
-    def update_auth_audit_table(self):
-        pass
+    def update_auth_session_table(self, nhs_number):
+        self.manage_user_session_service.update_auth_session_with_permitted_search(
+            nhs_number=nhs_number,
+            write_to_deceased_column=False,
+        )
