454f58a4ddff8efe8a28ef9d05b6aff8b9378f0c
Ollie Beumkes <oliver.beumkes2@nhs.net>
2025-03-06 11:13:45 +0000

    Attempt to use context to get user role and add it to data in RUM

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './styles/App.scss';
 import PatientDetailsProvider from './providers/patientProvider/PatientProvider';
-import SessionProvider from './providers/sessionProvider/SessionProvider';
+import SessionProvider, { useSessionContext } from './providers/sessionProvider/SessionProvider';
 import AppRouter from './router/AppRouter';
 import ConfigProvider from './providers/configProvider/ConfigProvider';
 import { AwsRum, AwsRumConfig } from 'aws-rum-web';
+let awsRumInstance: AwsRum | null = null;
 
 if (process.env.REACT_APP_ENVIRONMENT === 'development') {
     try {
@@ -20,8 +21,7 @@ if (process.env.REACT_APP_ENVIRONMENT === 'development') {
         const APPLICATION_ID: string = process.env.REACT_APP_MONITOR_ACCOUNT_ID || '';
         const APPLICATION_VERSION: string = '1.0.0';
         const APPLICATION_REGION: string = process.env.REACT_APP_AWS_REGION || 'eu-west-2';
-
-        const awsRum: AwsRum = new AwsRum( // eslint-disable-line
+        awsRumInstance = new AwsRum(
             APPLICATION_ID,
             APPLICATION_VERSION,
             APPLICATION_REGION,
@@ -33,6 +33,19 @@ if (process.env.REACT_APP_ENVIRONMENT === 'development') {
 }
 
 function App() {
+    const [session] = useSessionContext();
+    const roleRef = useRef<string | null>(null);
+
+    useEffect(() => {
+        if (awsRumInstance && session?.auth?.role && session.auth.role !== roleRef.current) {
+            awsRumInstance.addSessionAttributes({
+                userRole: session.auth.role,
+            });
+            roleRef.current = session.auth.role;
+            // eslint-disable-next-line no-use-before-define
+            console.log('User role added to AWS RUM:', session.auth.role);
+        }
+    }, [session]);
     return (
         <ConfigProvider>
             <SessionProvider>
