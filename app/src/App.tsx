12acfc8abb89dc1c89a8be893569e58f846173f7
Ollie Beumkes <oliver.beumkes2@nhs.net>
2025-03-07 10:35:03 +0000

    Added role to null check

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,6 @@
 import './styles/App.scss';
 import PatientDetailsProvider from './providers/patientProvider/PatientProvider';
-import SessionProvider, { useSessionContext } from './providers/sessionProvider/SessionProvider';
+import SessionProvider from './providers/sessionProvider/SessionProvider';
 import AppRouter from './router/AppRouter';
 import ConfigProvider from './providers/configProvider/ConfigProvider';
 import { AwsRum, AwsRumConfig } from 'aws-rum-web';
@@ -17,12 +17,10 @@ if (process.env.REACT_APP_ENVIRONMENT === 'development') {
             allowCookies: true,
             enableXRay: false,
         };
-
         const APPLICATION_ID: string = process.env.REACT_APP_MONITOR_ACCOUNT_ID || '';
         const APPLICATION_VERSION: string = '1.0.0';
         const APPLICATION_REGION: string = process.env.REACT_APP_AWS_REGION || 'eu-west-2';
         const session = sessionStorage.getItem('UserSession');
-
         awsRumInstance = new AwsRum( // eslint-disable-line
             APPLICATION_ID,
             APPLICATION_VERSION,
@@ -32,7 +30,7 @@ if (process.env.REACT_APP_ENVIRONMENT === 'development') {
         if (session != null) {
             const data = JSON.parse(session);
             console.log(data); // eslint-disable-line
-            if (data.auth !== null && awsRumInstance !== null) {
+            if (data.auth.role !== null && awsRumInstance !== null) {
                 awsRumInstance.addSessionAttributes({
                     userRole: data.auth.role,
                 });
