569b91b0c858f47d35de3dfa44929a62ec437b43
Steph Torres <stephane.torres1@nhs.net>
2025-04-22 13:49:59 +0100

    change template of message card

--- lambdas/handlers/teams_alerting_handler.py
+++ lambdas/handlers/teams_alerting_handler.py
@@ -2,14 +2,12 @@ import json
 
 import requests
 from utils.audit_logging_setup import LoggingService
-from utils.decorators import handle_lambda_exceptions
 from utils.decorators.ensure_env_var import ensure_environment_variables
 from utils.decorators.set_audit_arg import set_request_context_for_logging
 
 logger = LoggingService(__name__)
 
 
-@handle_lambda_exceptions
 @set_request_context_for_logging
 @ensure_environment_variables(
     names=[
@@ -19,33 +17,54 @@ logger = LoggingService(__name__)
 )
 def lambda_handler(event, context):
 
-    logger.info(f"Received event: {json.dumps(event)}")
-
     url = "<HTTP POST URL>"
 
-    payload = json.dumps(
-        {
-            "type": "message",
-            "attachments": [
-                {
-                    "contentType": "application/vnd.microsoft.card.adaptive",
-                    "contentUrl": None,
-                    "content": {
-                        "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-                        "type": "AdaptiveCard",
-                        "version": "1.2",
-                        "body": [
-                            {
-                                "type": "TextBlock",
-                                "text": "For Samples and Templates, see [https://adaptivecards.io/samples](https://adaptivecards.io/samples)",
-                            }
-                        ],
-                    },
-                }
-            ],
-        }
-    )
-    headers = {"Content-Type": "application/json"}
+    alarm_notifications = event.get("Records", [])
 
-    response = requests.request("POST", url, headers=headers, data=payload)
-    logger.info(response.text)
+    for sns_message in alarm_notifications:
+        card_title = sns_message["Sns"]["Message"]["AlarmName"]
+        card_description = sns_message["Sns"]["Message"]["AlarmDescription"]
+        alarm_state = sns_message["Sns"]["Message"]["NewStateValue"]
+        alarm_time = sns_message["Sns"]["Message"]["StateChangeTime "]
+
+        colour = "red" if alarm_state == "ALARM" else "green"
+
+        payload = json.dumps(
+            {
+                "type": "message",
+                "attachments": [
+                    {
+                        "contentType": "application/vnd.microsoft.card.adaptive",
+                        "contentUrl": None,
+                        "content": {
+                            "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+                            "type": "AdaptiveCard",
+                            "version": "1.2",
+                            "body": [
+                                {
+                                    "type": "TextBlock",
+                                    "size": "medium",
+                                    "weight": "bold",
+                                    "text": card_title,
+                                    "color": colour,
+                                },
+                                {
+                                    "type": "TextBlock",
+                                    "text": card_description,
+                                    "wrap": True,
+                                },
+                                {
+                                    "type": "TextBlock",
+                                    "text": f"This state change happened at {alarm_time}",
+                                },
+                            ],
+                        },
+                    }
+                ],
+            }
+        )
+
+        headers = {"Content-Type": "application/json"}
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+        logger.info(response.text)
