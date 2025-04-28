bc7b07ed24319467cd8e6d645a2fc7a3cc000ac6
NogaNHS <noga.sasson1@nhs.net>
2025-04-28 15:20:53 +0100

    [NDR-47] Updating update cloudfront script to remove unnecessary logic

--- lambdas/scripts/update_cloudfront.py
+++ lambdas/scripts/update_cloudfront.py
@@ -9,6 +9,7 @@ distribution_id = os.getenv("DISTRIBUTION_ID")
 lambda_name = os.getenv("LAMBDA_NAME")
 aws_region = os.getenv("AWS_REGION")
 aws_account_id = os.getenv("AWS_ACCOUNT_ID")
+lambda_arn = os.getenv("LAMBDA_ARN")
 
 cloudfront_client = boto3.client("cloudfront", region_name=aws_region)
 lambda_client = boto3.client("lambda", region_name=aws_region)
@@ -32,7 +33,7 @@ def propagate_lambda_update(retry_count=3, seconds_delay=5):
 
     for attempt in range(1, retry_count + 1):
         print(f"Attempt {attempt}: Propagating lambda update...")
-        response = lambda_client.get_function_configuration(FunctionName=lambda_name)
+        response = lambda_client.get_function_configuration(FunctionName=lambda_arn)
         if (
             response.get("State") == "Active"
             and response.get("LastUpdateStatus") == "Successful"
@@ -46,58 +47,6 @@ def propagate_lambda_update(retry_count=3, seconds_delay=5):
 
     print("Exceeded retries. Failed to verify Edge Lambda state.")
     sys.exit(1)
-
-
-def get_latest_lambda_version(function_name):
-    """Get the latest version of the Lambda function."""
-    try:
-        response = lambda_client.get_function_configuration(FunctionName=function_name)
-    except ClientError as ex:
-        print(f"Client error: {ex.response['Error']['Message']}")
-        sys.exit(2)
-    try:
-        version = response["Version"]
-        if not version:
-            print(f"No published versions found for Lambda function: {function_name}")
-            sys.exit(3)
-        return version
-    except ValueError as ex:
-        print(f"Error while processing versions: {ex}")
-        sys.exit(4)
-    except KeyError:
-        print("Unexpected response format: missing 'Version' key.")
-        sys.exit(5)
-
-
-def check_lambda_status(
-    function_name, version, retry_limit_seconds=60, retry_interval=15
-):
-    """Check the status of a Lambda function, retrying for up to 1 minute."""
-    total_time = 0
-    while total_time < retry_limit_seconds:
-        try:
-            response = lambda_client.get_function(
-                FunctionName=function_name, Qualifier=version
-            )
-            status = response["Configuration"]["State"]
-            print(f"Lambda function status: {status}")
-            if status == "Active":
-                print("Lambda function is active.")
-                return
-        except ClientError as e:
-            print(f"Error checking Lambda status: {e.response['Error']['Message']}")
-            sys.exit(6)
-
-        print(
-            f"Lambda function not yet active. Retrying in {retry_interval} seconds..."
-        )
-        time.sleep(retry_interval)
-        total_time += retry_interval
-
-    print(
-        f"Exceeded retry limit of {retry_limit_seconds} seconds. Lambda function is still not active."
-    )
-    sys.exit(7)
 
 
 def invalidate_cloudfront_cache(distribution_id):
@@ -172,11 +121,7 @@ if __name__ == "__main__":
             aws_account_id = get_aws_account_id()
 
         propagate_lambda_update()
-        lambda_version = get_latest_lambda_version(lambda_name)
-        lambda_arn = f"arn:aws:lambda:{aws_region}:{aws_account_id}:function:{lambda_name}:{lambda_version}"
         print(f"Lambda ARN: {lambda_arn}")
-
-        check_lambda_status(lambda_name, lambda_version)
         update_cloudfront_lambda_association(distribution_id, lambda_arn)
         invalidate_cloudfront_cache(distribution_id)
         print("Lambda@Edge update process completed successfully.")
