a9d5e22f18ce0d6fe2a9792eebaaa8f66d7e8439
PedroSoaresNHS <pedro.soares1@nhs.net>
2025-05-08 09:34:06 +0100

    [PRMT-69] - added extra validation to extracting document path

--- lambdas/tests/unit/services/test_metadata_preprocessor_service.py
+++ lambdas/tests/unit/services/test_metadata_preprocessor_service.py
@@ -100,11 +100,24 @@ def test_validate_and_update_file_name_returns_a_valid_file_path(test_service):
                 "10of10_Lloyd_George_Record_[Carol Hughes]_[1234567890]_[14-11-2000].pdf",
             ),
         ),
+        (
+            "/2020 Prince of Whales 2/10of10_Lloyd_George_Record_[Carol Hughes]_[1234567890]_[14/11/2000].pdf",
+            (
+                "/2020 Prince of Whales 2/",
+                "10of10_Lloyd_George_Record_[Carol Hughes]_[1234567890]_[14/11/2000].pdf",
+            ),
+        ),
+        (
+            "/2020of2024 Prince of Whales 2/2020 Prince of Whales 2/"
+            "10of10_Lloyd_George_Record_[Carol Hughes]_[1234567890]_[14/11/2000].pdf",
+            (
+                "/2020of2024 Prince of Whales 2/2020 Prince of Whales 2/",
+                "10of10_Lloyd_George_Record_[Carol Hughes]_[1234567890]_[14/11/2000].pdf",
+            ),
+        ),
     ],
 )
 def test_extract_document_path(test_service, value, expected):
-    # paths, expected_results
-    # for input_str, expected in test_cases:
     actual = test_service.extract_document_path(value)
     assert actual == expected
 
