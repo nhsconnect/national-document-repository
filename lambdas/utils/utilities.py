336f8ee23dd5bd3c307a718a551ea86fa28813d7
Mohammad Iqbal <mohammad.iqbal27@nhs.net>
2025-05-23 09:27:54 +0100

    [PRMT-131] - Remove unnecessary comments

--- lambdas/utils/utilities.py
+++ lambdas/utils/utilities.py
@@ -19,31 +19,24 @@ DATE_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
 def validate_nhs_number(nhs_number: str) -> bool:
     """
     Validate an NHS number using the Modulus 11 algorithm.
-    The NHS number must be 10 digits, with the last digit being the check digit.
-    Accepts NHS numbers with spaces or dashes.
+    https://www.datadictionary.nhs.uk/attributes/nhs_number.html
     """
-    # Remove any non-digit characters (e.g. spaces, dashes)
     nhs_number = re.sub(r"\D", "", nhs_number)
 
-    # Ensure the cleaned string is exactly 10 digits
     if not re.fullmatch(r"\d{10}", nhs_number):
         raise InvalidNhsNumberException("Invalid NHS number length")
 
-    # Extract the main digits and the check digit
     digits = [int(digit) for digit in nhs_number]
     check_digit = digits.pop()
 
-    # Calculate the modulus 11 checksum
     weights = list(range(10, 1, -1))
     total = sum(w * d for w, d in zip(weights, digits))
     remainder = total % 11
     calculated_check_digit = 11 - remainder
 
-    # Adjust the check digit if necessary
     if calculated_check_digit == 11:
         calculated_check_digit = 0
 
-    # If the check digit does not match calculated check digit, the number is invalid
     if check_digit != calculated_check_digit:
         raise InvalidNhsNumberException("Invalid NHS number format")
 
