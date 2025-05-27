9c3a338171055c9e7b27d6fa842370bd8778cc1b
jack-nhs <jack.sutton6@nhs.net>
2025-05-27 12:20:48 +0100

    Edits error checking to use error type

--- lambdas/handlers/token_handler.py
+++ lambdas/handlers/token_handler.py
@@ -54,7 +54,7 @@ def lambda_handler(event, context):
             "GET",
         ).create_api_gateway_response()
     except LoginException as e:
-        if e.status_code == 401 and e.err_code == "LIN_4006":
+        if e.error == LambdaError.LoginNoRole:
             allowed_roles = (
                 login_service.token_handler_ssm_service.get_smartcard_role_codes()
             )
