b8d68e347df684b722db7a1cf39a66d3a8b68f24
Steph Torres <stephane.torres1@nhs.net>
2025-05-30 10:18:18 +0100

    [PRM-134] ammend headers used on updating original slack message

--- lambdas/services/im_alerting_service.py
+++ lambdas/services/im_alerting_service.py
@@ -438,17 +438,12 @@ class IMAlertingService:
         slack_message["ts"] = alarm_entry.slack_timestamp
         slack_message["blocks"] = self.compose_slack_message(alarm_entry)
 
-        headers = {
-            "Authorization": "Bearer " + self.slack_bot_token,
-            "Content-type": "application/json; charset=utf-8",
-        }
-
         slack_update_chat_api = "https://slack.com/api/chat.update"
         requests.request(
             "POST",
             slack_update_chat_api,
             data=json.dumps(slack_message),
-            headers=headers,
+            headers=self.slack_headers,
         )
 
     def compose_slack_message(self, alarm_entry: AlarmEntry):
