0145452a8d9c60872015f2aad1e9c5788168cacf
Robert Gaskin <rob.gaskin1@nhs.net>
2025-06-04 11:59:00 +0100

    [NDR-43] Only track the completion

--- lambdas/services/pds_api_service.py
+++ lambdas/services/pds_api_service.py
@@ -43,11 +43,11 @@ class PdsApiService(PatientSearch):
 
             url_endpoint = endpoint + "Patient/" + nhs_number
 
-            logger.info("PDS Call Initiated", { "Event": "NDR-TR1" })
+            logger.info("PDS Call Initiated")
             pds_response = self.session.get(
                 url=url_endpoint, headers=authorization_header
             )
-            logger.info("PDS Call Completed", { "Event": "NDR-TR1" })
+            logger.info("PDS Call Completed",{ "Event": "NDR-TR1" })
 
             if pds_response.status_code == 401 and retry_on_expired:
                 return self.pds_request(nhs_number, retry_on_expired=False)
