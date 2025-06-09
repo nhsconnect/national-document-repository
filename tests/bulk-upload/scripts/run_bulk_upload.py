f6bcc518c01d3405dd2f36d91c33e6906ba1f421
PedroSoaresNHS <pedro.soares1@nhs.net>
2025-06-09 11:54:19 +0100

    [PRMT-229] - testing run bulk upload

--- tests/bulk-upload/scripts/run_bulk_upload.py
+++ tests/bulk-upload/scripts/run_bulk_upload.py
@@ -1,6 +1,8 @@
-import boto3
-import json
 import argparse
+import json
+import sys
+
+import boto3
 
 
 def invoke_lambda(lambda_name, payload={}):
@@ -53,7 +55,10 @@ if __name__ == "__main__":
 
     bulk_upload_lambda_name = f"{args.environment}_BulkUploadLambda"
     search_lambda_name = f"{args.environment}_SearchPatientDetailsLambda"
-    if args.disable_pds_stub or input("Would you like to disable the FHIR Stub: "):
+    if args.disable_pds_stub or (
+        sys.stdin.isatty()
+        and input("Would you like to disable the FHIR Stub: ").lower() == "y"
+    ):
         new_variables = {"PDS_FHIR_IS_STUBBED": "false"}
         update_lambda_environment_variables(bulk_upload_lambda_name, new_variables)
         update_lambda_environment_variables(search_lambda_name, new_variables)
