76046c9da46be793e3d82c3ec26fc9d4ea5b68d4
PedroSoaresNHS <pedro.soares1@nhs.net>
2025-06-09 12:12:16 +0100

    [PRMT-229] - testing run bulk upload

--- tests/bulk-upload/scripts/updated_setup_bulk_upload.py
+++ tests/bulk-upload/scripts/updated_setup_bulk_upload.py
@@ -8,7 +8,7 @@ import boto3
 SOURCE_PDF_FILE_NAME = "source_to_copy_from.pdf"
 SOURCE_PDF_FILE = "../source_to_copy_from.pdf"
 UPDATED_SOURCE_PDF_FILE = "../updated_source_to_copy_from.pdf"
-NHS_NUMBER = "0000000000"
+NHS_NUMBER = "1000000000"
 
 CSV_HEADER_ROW = (
     "FILEPATH,PAGE COUNT,GP-PRACTICE-CODE,NHS-NO,"
