4c43bd1f3ff0b25c3b02c853d46ebc1e0c30b1d2
NogaNHS <noga.sasson1@nhs.net>
2025-06-11 14:38:16 +0100

    [NDR-100] MNS changes

--- lambdas/services/process_mns_message_service.py
+++ lambdas/services/process_mns_message_service.py
@@ -3,12 +3,12 @@ from datetime import datetime
 
 from botocore.exceptions import ClientError
 from enums.death_notification_status import DeathNotificationStatus
-from enums.metadata_field_names import DocumentReferenceMetadataFields
 from enums.mns_notification_types import MNSNotificationTypes
 from enums.patient_ods_inactive_status import PatientOdsInactiveStatus
+from models.document_reference import DocumentReference
 from models.sqs.mns_sqs_message import MNSSQSMessage
-from services.base.dynamo_service import DynamoDBService
 from services.base.sqs_service import SQSService
+from services.document_service import DocumentService
 from utils.audit_logging_setup import LoggingService
 from utils.exceptions import PdsErrorException
 from utils.utilities import get_pds_service
@@ -18,11 +18,13 @@ logger = LoggingService(__name__)
 
 class MNSNotificationService:
     def __init__(self):
-        self.dynamo_service = DynamoDBService()
+        self.document_service = DocumentService()
         self.table = os.getenv("LLOYD_GEORGE_DYNAMODB_NAME")
         self.pds_service = get_pds_service()
         self.sqs_service = SQSService()
         self.queue = os.getenv("MNS_NOTIFICATION_QUEUE_URL")
+        self.DOCUMENT_UPDATE_FIELDS = {"current_gp_ods", "custodian", "last_updated"}
+        self.PCSE_ODS = "X4S4L"
 
     def handle_mns_notification(self, message: MNSSQSMessage):
         try:
@@ -40,106 +42,92 @@ class MNSNotificationService:
                 f"Unable to process message: {message.id}, of type: {message.type}"
             )
             logger.info(f"{e}")
-            raise (e)
+            raise e
 
         except ClientError as e:
             logger.info(
                 f"Unable to process message: {message.id}, of type: {message.type}"
             )
             logger.info(f"{e}")
-            raise (e)
+            raise e
 
-    def handle_gp_change_notification(self, message: MNSSQSMessage):
+    def handle_gp_change_notification(self, message: MNSSQSMessage) -> None:
         patient_document_references = self.get_patient_documents(
             message.subject.nhs_number
         )
 
-        if not self.patient_is_present_in_ndr(patient_document_references):
+        if not patient_document_references:
             return
 
         updated_ods_code = self.get_updated_gp_ods(message.subject.nhs_number)
-
-        for reference in patient_document_references:
-            if reference["CurrentGpOds"] is not updated_ods_code:
-                self.dynamo_service.update_item(
-                    table_name=self.table,
-                    key_pair={"ID": reference["ID"]},
-                    updated_fields={
-                        DocumentReferenceMetadataFields.CURRENT_GP_ODS.value: updated_ods_code,
-                        DocumentReferenceMetadataFields.LAST_UPDATED.value: int(
-                            datetime.now().timestamp()
-                        ),
-                    },
-                )
-
+        self.update_patient_ods_code(patient_document_references, updated_ods_code)
         logger.info("Update complete for change of GP")
 
-    def handle_death_notification(self, message: MNSSQSMessage):
+    def handle_death_notification(self, message: MNSSQSMessage) -> None:
         death_notification_type = message.data["deathNotificationStatus"]
+        nhs_number = message.subject.nhs_number
+
         match death_notification_type:
             case DeathNotificationStatus.INFORMAL:
                 logger.info(
                     "Patient is deceased - INFORMAL, moving on to the next message."
                 )
-                return
 
             case DeathNotificationStatus.REMOVED:
-                patient_documents = self.get_patient_documents(
-                    message.subject.nhs_number
-                )
-                if not self.patient_is_present_in_ndr(patient_documents):
-                    return
-
-                updated_ods_code = self.get_updated_gp_ods(message.subject.nhs_number)
-                self.update_patient_ods_code(patient_documents, updated_ods_code)
-                logger.info("Update complete for death notification change.")
+                patient_document_references = self.get_patient_documents(nhs_number)
+                if patient_document_references:
+                    updated_ods_code = self.get_updated_gp_ods(nhs_number)
+                    self.update_patient_ods_code(
+                        patient_document_references, updated_ods_code
+                    )
+                    logger.info("Update complete for death notification change.")
 
             case DeathNotificationStatus.FORMAL:
-                patient_documents = self.get_patient_documents(
-                    message.subject.nhs_number
-                )
-                if not self.patient_is_present_in_ndr(patient_documents):
-                    return
+                patient_document_references = self.get_patient_documents(nhs_number)
+                if patient_document_references:
+                    self.update_patient_ods_code(
+                        patient_document_references, PatientOdsInactiveStatus.DECEASED
+                    )
+                    logger.info(
+                        f"Update complete, patient marked {PatientOdsInactiveStatus.DECEASED}."
+                    )
 
-                self.update_patient_ods_code(
-                    patient_documents, PatientOdsInactiveStatus.DECEASED
-                )
-                logger.info(
-                    f"Update complete, patient marked {PatientOdsInactiveStatus.DECEASED}."
-                )
-
-    def get_patient_documents(self, nhs_number: str):
-        logger.info("Getting patient document references...")
-        response = self.dynamo_service.query_table_by_index(
-            table_name=self.table,
-            index_name="NhsNumberIndex",
-            search_key="NhsNumber",
-            search_condition=nhs_number,
-        )
-        return response["Items"]
-
-    def update_patient_ods_code(self, patient_documents: list[dict], code: str) -> None:
-        for document in patient_documents:
+    def update_patient_ods_code(
+        self, patient_documents: list[DocumentReference], updated_ods_code: str
+    ) -> None:
+        if not patient_documents:
+            return
+        update_needed = False
+        for reference in patient_documents:
             logger.info("Updating patient document reference...")
-            self.dynamo_service.update_item(
-                table_name=self.table,
-                key_pair={"ID": document["ID"]},
-                updated_fields={
-                    DocumentReferenceMetadataFields.CURRENT_GP_ODS.value: code,
-                    DocumentReferenceMetadataFields.LAST_UPDATED.value: int(
-                        datetime.now().timestamp()
-                    ),
-                },
+
+            if (
+                reference.current_gp_ods != updated_ods_code
+                or reference.custodian != updated_ods_code
+            ):
+                update_needed = True
+                updated_custodian = updated_ods_code
+                if updated_ods_code in [
+                    PatientOdsInactiveStatus.DECEASED,
+                    PatientOdsInactiveStatus.SUSPENDED,
+                ]:
+                    updated_custodian = self.PCSE_ODS
+                reference.current_gp_ods = updated_ods_code
+                reference.custodian = updated_custodian
+                reference.last_updated = int(datetime.now().timestamp())
+        if update_needed:
+            self.document_service.update_documents(
+                self.table,
+                patient_documents,
+                self.DOCUMENT_UPDATE_FIELDS,
             )
 
     def get_updated_gp_ods(self, nhs_number: str) -> str:
         patient_details = self.pds_service.fetch_patient_details(nhs_number)
         return patient_details.general_practice_ods
 
-    def patient_is_present_in_ndr(self, dynamo_response):
-        if len(dynamo_response) < 1:
-            logger.info("Patient is not held in the National Document Repository.")
-            logger.info("Moving on to the next message.")
-            return False
-        else:
-            return True
+    def get_patient_documents(self, nhs_number: str) -> list[DocumentReference]:
+        """Fetch patient documents and return them if they exist."""
+        return self.document_service.fetch_documents_from_table_with_nhs_number(
+            nhs_number, self.table
+        )
