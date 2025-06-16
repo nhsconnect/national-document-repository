75f72b6fbd1d3be792f272fe63a5e3ea8b151baf
“samFallowfieldNHSgit config user.email “sam.fallowfield1@nhs.net <sam.fallowfield@madetech.com>
2025-06-16 13:48:05 +0100

    [PRMT-329] updated mock login page

--- app/src/pages/mockCis2LoginPage/MockCis2LoginPage.tsx
+++ app/src/pages/mockCis2LoginPage/MockCis2LoginPage.tsx
@@ -1,27 +1,49 @@
-import { FormGroup } from 'nhsuk-react-components';
+import { FormGroup, Form } from 'nhsuk-react-components';
+import { useState } from 'react';
 
 const MockCis2LoginPage = () => {
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    };
+
     return (
         <div className="nhsuk-main-wrapper--s">
             <div className="nhsuk-grid-row">
                 <div className="nhsuk-grid-column-two-thirds">
-                    <FormGroup>
-                        <div className="form-label-group">
-                            <div className="nhsuk-form-group position-relative">
-                                <label>User Name</label>
-                                <input className="nhsuk-input"></input>
+                    <Form onSubmit={handleSubmit}>
+                        <FormGroup>
+                            <div className="form-label-group">
+                                <div className="nhsuk-form-group position-relative">
+                                    <label htmlFor="username">Username</label>
+                                    <input
+                                        className="nhsuk-input"
+                                        type="text"
+                                        id="username"
+                                        onChange={(e) => setUsername(e.target.value)}
+                                    ></input>
+                                </div>
                             </div>
-                        </div>
-                        <div className="form-label-group">
-                            <div className="nhsuk-form-group position-relative">
-                                <label>Password</label>
-                                <input className="nhsuk-input"></input>
+                        </FormGroup>
+                        <FormGroup>
+                            <div className="form-label-group">
+                                <div className="nhsuk-form-group position-relative">
+                                    <label htmlFor="password">Password</label>
+                                    <input
+                                        className="nhsuk-input"
+                                        type="text"
+                                        id="password"
+                                        onChange={(e) => setPassword(e.target.value)}
+                                    ></input>
+                                </div>
                             </div>
-                        </div>
-                        <div className="form-label-group">
-                            <button className="nhsuk-button">Continue</button>
-                        </div>
-                    </FormGroup>
+                            <div className="form-label-group">
+                                <button className="nhsuk-button">Continue</button>
+                            </div>
+                        </FormGroup>
+                    </Form>
                 </div>
             </div>
         </div>
