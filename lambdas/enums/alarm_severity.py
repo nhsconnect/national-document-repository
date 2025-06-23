1f388b2bffe8aca755aa0ed9a5958f7b51364c9c
AndyFlintNHS <andrew.flint2@nhs.net>
2025-06-23 14:05:49 +0100

    [PRM-134] Added insufficient_data slack emoji

--- lambdas/enums/alarm_severity.py
+++ lambdas/enums/alarm_severity.py
@@ -6,4 +6,4 @@ class AlarmSeverity(MultiValueEnum):
     MEDIUM = "\U0001F7E0", "large_orange_circle"
     LOW = "\U0001F7E1", "large_yellow_circle"
     OK = "\U0001F7E2", "large_green_circle"
-    # TODO PRM-134 add 'insufficient data' grey circle
+    INSUFFICIENT_DATA = "\U000026AB", "black_circle"
