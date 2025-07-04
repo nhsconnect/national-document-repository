be5e257ea4bab5a4ad39b1ab48901cd1b92964e8
PedroSoaresNHS <pedro.soares1@nhs.net>
2025-07-04 09:00:04 +0100

    [PRMT-70]- updated last_updated to use int

--- lambdas/services/pdf_stitching_service.py
+++ lambdas/services/pdf_stitching_service.py
@@ -3,7 +3,6 @@ import re
 import sys
 import uuid
 from datetime import datetime, timezone
-from decimal import Decimal
 from io import BytesIO
 
 from botocore.exceptions import ClientError
@@ -120,7 +119,7 @@ class PdfStitchingService:
                 "file_location": f"s3://{self.target_bucket}/{document_reference.nhs_number}/{reference_id}",
                 "file_name": f"1of1_{stripped_filename}",
                 "file_size": stitch_file_size,
-                "last_updated": Decimal(str(date_now.timestamp())),
+                "last_updated": int(datetime.now(timezone.utc).timestamp()),
             },
             deep=True,
         )
