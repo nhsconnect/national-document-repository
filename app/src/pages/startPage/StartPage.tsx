dca9aefaf58aa392a0435235f244ebe70b6f7572
Kamen Bachvarov <kamen.bachvaror1@nhs.net>
2025-07-22 14:28:57 +0100

    remove test commit lines

--- app/src/pages/startPage/StartPage.tsx
+++ app/src/pages/startPage/StartPage.tsx
@@ -60,7 +60,6 @@ function StartPage(props: Props) {
             <ul>
                 <li>your NHS smartcard</li>
                 <li>patient details including their name, date of birth and NHS number</li>
-                <li>test commit from vscode</li>
             </ul>
             <ButtonLink data-testid="start-btn" onClick={handleLogin} href="#">
                 Start now
