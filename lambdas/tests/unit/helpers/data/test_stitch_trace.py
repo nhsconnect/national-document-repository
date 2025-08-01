ae7cfcf8f98cbf2521eef26e9238b0922218913a
Ollie Beumkes <oliver.beumkes2@nhs.net>
2025-08-01 17:05:42 +0100

    Updated NHS Number to correct format

--- lambdas/tests/unit/helpers/data/test_stitch_trace.py
+++ lambdas/tests/unit/helpers/data/test_stitch_trace.py
@@ -1,12 +1,13 @@
 from typing import List
 from lambdas.enums.trace_status import TraceStatus
 from lambdas.models.stitch_trace import StitchTrace
+from lambdas.tests.unit.conftest import TEST_NHS_NUMBER
 
 
 def create_test_stitch_trace() -> StitchTrace:
     """Create a test StitchTrace object with optional overrides."""
     stitch_trace = StitchTrace(
-        nhs_number=9999999999,
+        nhs_number=TEST_NHS_NUMBER,
         expire_at=9999999, 
         job_status=TraceStatus.PENDING
     )
@@ -16,17 +17,17 @@ def get_list_test_stitch_trace() -> List[StitchTrace]:
     """Create a list of test StitchTrace objects."""
     return [
         StitchTrace(
-            nhs_number=9999999999,
+            nhs_number=TEST_NHS_NUMBER,
             expire_at=9999999,
             job_status=TraceStatus.PENDING
         ),
         StitchTrace(
-            nhs_number=9999999999,
+            nhs_number=TEST_NHS_NUMBER,
             expire_at=8888888,
             job_status=TraceStatus.PENDING
         ),
         StitchTrace(
-            nhs_number=9999999999,
+            nhs_number=TEST_NHS_NUMBER,
             expire_at=7777777,
             job_status=TraceStatus.PENDING
         )
