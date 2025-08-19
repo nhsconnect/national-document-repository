77217ceb3c5a713b779d701b0b756175abb0b20b
SWhyteAnswer-NHS <sam.whyte3@nhs.net>
2025-08-19 10:28:55 +0100

    [NDR-193] Removing Splunk (#731)
    * [NDR-193] removing splunk
    
    * [NDR-193] removing more
    
    * [NDR-193] removing moc
    
    ---------
    
    Co-authored-by: Sam Whyte <sam.whyte@answerdigital.com>
    Co-authored-by: robg-nhs <rob.gaskin@nhs.net>

--- lambdas/utils/audit_logging_setup.py
+++ lambdas/utils/audit_logging_setup.py
@@ -1,8 +1,6 @@
 import logging
 
-from services.sensitive_audit_service import SensitiveAuditService
 from utils.logging_formatter import LoggingFormatter
-
 
 class LoggingService:
     audit_logger = None
@@ -11,24 +9,8 @@ class LoggingService:
         self.name = name
         self.logger = logging.getLogger(name)
         self.logger.setLevel(logging.INFO)
-        self.configure_audit_logger()
         self.formatter = LoggingFormatter()
         logging.Formatter.format = self.formatter.format
-
-    def configure_audit_logger(self):
-        if self.__class__.audit_logger is None:
-            self.__class__.audit_logger = logging.getLogger("audit")
-            audit_handler = SensitiveAuditService()
-            self.__class__.audit_logger.addHandler(audit_handler)
-            self.__class__.audit_logger.setLevel(logging.INFO)
-
-    def audit_splunk_info(self, msg, args: dict = None):
-        logging.getLogger("audit.{}".format(self.name))
-        self.audit_logger.info(msg, extra={"custom_args": args})
-
-    def audit_splunk_error(self, msg, custom_args: dict = None, **kwargs):
-        logging.getLogger("audit.{}".format(self.name))
-        self.audit_logger.error(msg, extra={"custom_args": custom_args})
 
     def info(self, message, custom_args: dict = None, *args, **kwargs):
         self.logger.info(message, extra={"custom_args": custom_args}, *args, **kwargs)
