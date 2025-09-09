da92042401cc62614bc81ff5e55ef5743788bbf0
kamenbachvarov-nhs <kamen.bachvaror1@nhs.net>
2025-09-09 16:58:14 +0100

    [PRME-163] Update feedback page errors & include error box (#758)
    * initial implementation of error component in feedback page
    
    * refactoring ErrorBox component to be generic
    
    * refactor generic errorBoxErrorMessage and groupErrorsByType
    
    * move the generic page error types to a separate file
    
    * format code
    
    * fix and extend e2e tests for feedback page (errorbox)
    
    * refactor errorMessageList on feedback page
    
    * extend UT to cover for error box component
    
    * fix ESlint warnings
    
    * address PR comments
    
    * fix scroll to errorBox onSubmit
    
    * add timeout to the scroll to ensure the ref exists
    
    * increase timeout to 50
    
    * remove timeout and fix tests
    
    * add happy-dom as vitest env

---
+++ app/src/helpers/utils/errorMessages.ts
@@ -0,0 +1,28 @@
+import { ErrorMessageListItem, ErrorMessageMap } from '../../types/pages/genericPageErrors';
+
+export function getMappedErrorMessage<T extends string>(
+    error: ErrorMessageListItem<T>,
+    messageMap: ErrorMessageMap<T>,
+): string {
+    return messageMap[error.error].errorBox;
+}
+
+export function groupErrorsByType<T extends string>(
+    errors: ErrorMessageListItem<T>[],
+    getMessage: (error: ErrorMessageListItem<T>) => string,
+): Partial<Record<T, { linkIds: string[]; errorMessage: string }>> {
+    const result: Partial<Record<T, { linkIds: string[]; errorMessage: string }>> = {};
+
+    errors.forEach((errorItem) => {
+        const { error, linkId = '' } = errorItem;
+        const errorMessage = getMessage(errorItem);
+
+        if (!result[error]) {
+            result[error] = { linkIds: [linkId], errorMessage };
+        } else {
+            result[error]!.linkIds.push(linkId);
+        }
+    });
+
+    return result;
+}
