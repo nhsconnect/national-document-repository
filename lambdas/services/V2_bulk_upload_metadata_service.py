fa774275458e4fbf54796ca71560d0cbf4b70678
PedroSoaresNHS <pedro.soares1@nhs.net>
2025-09-15 16:31:24 +0100

    [PRMT-575]- allowed the use of the preprocessor service instead of duplicating code

--- lambdas/services/V2_bulk_upload_metadata_service.py
+++ lambdas/services/V2_bulk_upload_metadata_service.py
@@ -7,7 +7,6 @@ from datetime import datetime
 from typing import Iterable
 
 import pydantic
-import regex
 from botocore.exceptions import ClientError
 from enums.upload_status import UploadStatus
 from models.staging_metadata import (
@@ -22,6 +21,9 @@ from repositories.bulk_upload.bulk_upload_dynamo_repository import (
 )
 from services.base.s3_service import S3Service
 from services.base.sqs_service import SQSService
+from services.bulk_upload_metadata_preprocessor_service import (
+    MetadataPreprocessorService,
+)
 from utils.audit_logging_setup import LoggingService
 from utils.exceptions import BulkUploadMetadataException, InvalidFileNameException
 
@@ -43,9 +45,12 @@ class V2BulkUploadMetadataService:
         self.corrections = {}
         self.practice_directory = practice_directory
         self.file_key = f"{self.practice_directory}/{METADATA_FILENAME}"
+        self.metadata_preprocessor_service = MetadataPreprocessorService(
+            practice_directory
+        )
 
     def process_metadata(self):
-        if self.practice_directory is "":
+        if self.practice_directory == "":
             self.file_key = METADATA_FILENAME
         try:
             metadata_file = self.download_metadata_from_s3()
@@ -161,19 +166,25 @@ class V2BulkUploadMetadataService:
     def validate_record_filename(self, file_name) -> str:
         logger.info(f"Processing file name {file_name}")
 
-        file_path_prefix, current_file_name = self.extract_document_path(file_name)
+        file_path_prefix, current_file_name = (
+            self.metadata_preprocessor_service.extract_document_path(file_name)
+        )
 
         first_document_number, second_document_number, current_file_name = (
-            self.extract_document_number_bulk_upload_file_name(current_file_name)
+            self.metadata_preprocessor_service.extract_document_number_bulk_upload_file_name(
+                current_file_name
+            )
         )
 
         lloyd_george_record, current_file_name = (
-            self.extract_lloyd_george_record_from_bulk_upload_file_name(
+            self.metadata_preprocessor_service.extract_lloyd_george_record_from_bulk_upload_file_name(
                 current_file_name
             )
         )
         patient_name, current_file_name = (
-            self.extract_patient_name_from_bulk_upload_file_name(current_file_name)
+            self.metadata_preprocessor_service.extract_patient_name_from_bulk_upload_file_name(
+                current_file_name
+            )
         )
 
         if sum(c.isdigit() for c in current_file_name) != 18:
@@ -181,177 +192,34 @@ class V2BulkUploadMetadataService:
             raise InvalidFileNameException("Incorrect NHS number or date format")
 
         nhs_number, current_file_name = (
-            self.extract_nhs_number_from_bulk_upload_file_name(current_file_name)
+            self.metadata_preprocessor_service.extract_nhs_number_from_bulk_upload_file_name(
+                current_file_name
+            )
         )
         day, month, year, current_file_name = (
-            self.extract_date_from_bulk_upload_file_name(current_file_name)
+            self.metadata_preprocessor_service.extract_date_from_bulk_upload_file_name(
+                current_file_name
+            )
         )
-        file_extension = self.extract_file_extension_from_bulk_upload_file_name(
+        file_extension = self.metadata_preprocessor_service.extract_file_extension_from_bulk_upload_file_name(
             current_file_name
         )
-        file_name_correction = self.assemble_valid_file_name(
-            file_path_prefix,
-            first_document_number,
-            second_document_number,
-            lloyd_george_record,
-            patient_name,
-            nhs_number,
-            day,
-            month,
-            year,
-            file_extension,
+        file_name_correction = (
+            self.metadata_preprocessor_service.assemble_valid_file_name(
+                file_path_prefix,
+                first_document_number,
+                second_document_number,
+                lloyd_george_record,
+                patient_name,
+                nhs_number,
+                day,
+                month,
+                year,
+                file_extension,
+            )
         )
         if file_name_correction:
             logger.info(f"Finished processing, new file name is: {file_name}")
             return file_name_correction
         else:
             return ""
-
-    @staticmethod
-    def extract_document_path(
-        file_path: str,
-    ) -> tuple[str, str]:
-        document_number_expression = r"(.*[/])*((\d+)[^0-9]*of[^0-9]*(\d+)(.*))"
-
-        expression_result = regex.search(rf"{document_number_expression}", file_path)
-
-        if expression_result is None:
-            logger.info("Failed to find the document path in file name")
-            raise InvalidFileNameException("Incorrect document path format")
-
-        current_file_path = expression_result.group(2)
-        if expression_result.group(1) is None:
-            file_path = file_path.replace(current_file_path, "")
-            file_path = file_path[: file_path.rfind("/") + 1]
-        else:
-            file_path = expression_result.group(1)
-        return file_path, current_file_path
-
-    @staticmethod
-    def extract_document_number_bulk_upload_file_name(
-        file_path: str,
-    ) -> tuple[int, int, str]:
-        document_number_expression = r"[^0-9]*(\d+)[^0-9]*of[^0-9]*(\d+)(.*)"
-        expression_result = regex.search(rf"{document_number_expression}", file_path)
-
-        if expression_result is None:
-            logger.info("Failed to find the document number in file name")
-            raise InvalidFileNameException("Incorrect document number format")
-
-        first_document_number = int(expression_result.group(1))
-        second_document_number = int(expression_result.group(2))
-        current_file_path = expression_result.group(3)
-
-        return first_document_number, second_document_number, current_file_path
-
-    @staticmethod
-    def extract_lloyd_george_record_from_bulk_upload_file_name(
-        file_path: str,
-    ) -> tuple[str, str]:
-        _expression = r".*?ll[oO0οՕ〇]yd.*?ge[oO0οՕ〇]rge.*?rec[oO0οՕ〇]rd(.*)"
-        lloyd_george_record = regex.search(
-            rf"{_expression}", file_path, regex.IGNORECASE
-        )
-        if lloyd_george_record is None:
-            logger.info("Failed to extract Lloyd George Record from file name")
-            raise InvalidFileNameException("Invalid Lloyd_George_Record separator")
-
-        current_file_path = lloyd_george_record.group(1)
-
-        lloyd_george_record_text = "Lloyd_George_Record"
-
-        return lloyd_george_record_text, current_file_path
-
-    @staticmethod
-    def extract_patient_name_from_bulk_upload_file_name(
-        file_path: str,
-    ) -> tuple[str, str]:
-        document_number_expression = r".*?([\p{L}][^\d]*[\p{L}])(.*)"
-        expression_result = regex.search(
-            rf"{document_number_expression}", file_path, regex.IGNORECASE
-        )
-
-        if expression_result is None:
-            logger.info("Failed to find the patient name in the file name")
-            raise InvalidFileNameException("Invalid patient name")
-
-        patient_name = expression_result.group(1)
-        current_file_path = expression_result.group(2)
-
-        return patient_name, current_file_path
-
-    @staticmethod
-    def extract_nhs_number_from_bulk_upload_file_name(
-        file_path: str,
-    ) -> tuple[str, str]:
-        nhs_number_expression = r"((?:[^_]*?\d){10})(.*)"
-        expression_result = regex.search(rf"{nhs_number_expression}", file_path)
-
-        if expression_result is None:
-            logger.info("Failed to find NHS number in file name")
-            raise InvalidFileNameException("Invalid NHS number")
-
-        nhs_number = "".join(regex.findall(r"\d", expression_result.group(1)))
-        current_file_path = expression_result.group(2)
-
-        return nhs_number, current_file_path
-
-    @staticmethod
-    def extract_date_from_bulk_upload_file_name(file_path):
-        date_number_expression = r"(\D+\d{2})(\D*\d{2})(\D*(\d{4}))(.*)"
-        expression_result = regex.search(rf"{date_number_expression}", file_path)
-
-        if expression_result is None:
-            logger.info("Failed to find date in file name")
-            raise InvalidFileNameException("Invalid date format")
-
-        day = "".join(regex.findall(r"\d", expression_result.group(1))).zfill(2)
-        month = "".join(regex.findall(r"\d", expression_result.group(2))).zfill(2)
-        year = "".join(regex.findall(r"\d", expression_result.group(3)))
-        current_file_path = expression_result.group(5)
-
-        try:
-            datetime(day=int(day), month=int(month), year=int(year))
-        except ValueError as e:
-            logger.info(f"Failed to parse date from filename: {e}")
-            raise InvalidFileNameException("Invalid date format")
-
-        return day, month, year, current_file_path
-
-    @staticmethod
-    def extract_file_extension_from_bulk_upload_file_name(
-        file_path: str,
-    ) -> str:
-        file_extension_expression = r"(\.([^.]*))$"
-        expression_result = regex.search(rf"{file_extension_expression}", file_path)
-
-        if expression_result is None:
-            logger.info("Failed to find a file extension")
-            raise InvalidFileNameException("Invalid file extension")
-
-        file_extension = expression_result.group(1)
-
-        return file_extension
-
-    @staticmethod
-    def assemble_valid_file_name(
-        file_path_prefix: str,
-        first_document_number: int,
-        second_document_number: int,
-        lloyd_george_record: str,
-        patient_name: str,
-        nhs_number: str,
-        day: str,
-        month: str,
-        year: str,
-        file_extension: str,
-    ) -> str:
-        return (
-            f"{file_path_prefix}"
-            f"{first_document_number}of{second_document_number}"
-            f"_{lloyd_george_record}_"
-            f"[{patient_name}]_"
-            f"[{nhs_number}]_"
-            f"[{day}-{month}-{year}]"
-            f"{file_extension}"
-        )
