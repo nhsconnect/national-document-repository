6ccf4d69c798f04e2cd6bd5639830017c862588a
PedroSoaresNHS <pedro.soares1@nhs.net>
2025-09-19 08:29:17 +0100

    [PRMT-609]- fixed spaces

--- lambdas/services/statistical_report_service.py
+++ lambdas/services/statistical_report_service.py
@@ -22,6 +22,7 @@ from utils.utilities import generate_date_folder_name
 
 logger = LoggingService(__name__)
 
+
 class StatisticalReportService:
     def __init__(self):
         self.dynamo_service = DynamoDBService()
@@ -159,7 +160,9 @@ class StatisticalReportService:
                 pl.concat_list("active_user_ids_hashed")
                 .flatten()
                 .unique()
-                .map_elements(lambda col: str(col.sort().to_list()), return_dtype=pl.Utf8)
+                .map_elements(
+                    lambda col: str(col.sort().to_list()), return_dtype=pl.Utf8
+                )
                 .alias("unique_active_user_ids_hashed"),
                 pl.concat_list("active_user_ids_hashed")
                 .flatten()
@@ -179,7 +182,7 @@ class StatisticalReportService:
 
         for other_dataframe in data_to_report[1:]:
             joined_dataframe = joined_dataframe.join(
-                other_dataframe, on="ods_code", how='full', coalesce=True
+                other_dataframe, on="ods_code", how="full", coalesce=True
             )
 
         return joined_dataframe
